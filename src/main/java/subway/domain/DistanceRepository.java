e6b9496fc2255d12dd42635e78fc7ae8f004a747
taelee <taelee@student.42seoul.kr>
2020-12-19 16:30:06 +0900

    feat(FastestPathFinder): 구간별 시간을 주어진 값으로 초기화

--- src/main/java/subway/domain/DistanceRepository.java
+++ src/main/java/subway/domain/DistanceRepository.java
@@ -7,23 +7,24 @@ import org.jgrapht.graph.WeightedMultigraph;
 import java.util.List;
 
 public class DistanceRepository {
-    private static WeightedMultigraph<String, DefaultWeightedEdge> graph;
+    private static WeightedMultigraph<String, DefaultWeightedEdge> distanceGraph;
     private static DijkstraShortestPath dijkstraShortestPath;
+    private static WeightedMultigraph<String, DefaultWeightedEdge> timeGraph;
+    private static DijkstraShortestPath dijkstraFastestPath;
 
     public static void initializePathByDistance() {
-        graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        distanceGraph = new WeightedMultigraph(DefaultWeightedEdge.class);
         for (Station station : StationRepository.stations()) {
-            graph.addVertex(station.getName());
+            distanceGraph.addVertex(station.getName());
         }
-        graph.setEdgeWeight(graph.addEdge("교대역", "강남역"), 2);
-        graph.setEdgeWeight(graph.addEdge("강남역", "역삼역"), 2);
-        graph.setEdgeWeight(graph.addEdge("교대역", "남부터미널역"), 3);
-        graph.setEdgeWeight(graph.addEdge("남부터미널역", "양재역"), 6);
-        graph.setEdgeWeight(graph.addEdge("양재역", "매봉역"), 1);
-        graph.setEdgeWeight(graph.addEdge("강남역", "양재역"), 2);
-        graph.setEdgeWeight(graph.addEdge("양재역", "양재시민의숲역"), 10);
-
-        dijkstraShortestPath = new DijkstraShortestPath(graph);
+        distanceGraph.setEdgeWeight(distanceGraph.addEdge("교대역", "강남역"), 2);
+        distanceGraph.setEdgeWeight(distanceGraph.addEdge("강남역", "역삼역"), 2);
+        distanceGraph.setEdgeWeight(distanceGraph.addEdge("교대역", "남부터미널역"), 3);
+        distanceGraph.setEdgeWeight(distanceGraph.addEdge("남부터미널역", "양재역"), 6);
+        distanceGraph.setEdgeWeight(distanceGraph.addEdge("양재역", "매봉역"), 1);
+        distanceGraph.setEdgeWeight(distanceGraph.addEdge("강남역", "양재역"), 2);
+        distanceGraph.setEdgeWeight(distanceGraph.addEdge("양재역", "양재시민의숲역"), 10);
+        dijkstraShortestPath = new DijkstraShortestPath(distanceGraph);
     }
 
 
@@ -32,8 +33,31 @@ public class DistanceRepository {
     }
 
     public static int getShortestPathLength(String startStation, String endStation) {
-        dijkstraShortestPath = new DijkstraShortestPath(graph);
         return (int)dijkstraShortestPath.getPathWeight(startStation, endStation);
+    }
+
+
+    public static void initializePathByTime() {
+        timeGraph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        for (Station station : StationRepository.stations()) {
+            timeGraph.addVertex(station.getName());
+        }
+        timeGraph.setEdgeWeight(timeGraph.addEdge("교대역", "강남역"), 3);
+        timeGraph.setEdgeWeight(timeGraph.addEdge("강남역", "역삼역"), 3);
+        timeGraph.setEdgeWeight(timeGraph.addEdge("교대역", "남부터미널역"), 2);
+        timeGraph.setEdgeWeight(timeGraph.addEdge("남부터미널역", "양재역"), 5);
+        timeGraph.setEdgeWeight(timeGraph.addEdge("양재역", "매봉역"), 1);
+        timeGraph.setEdgeWeight(timeGraph.addEdge("강남역", "양재역"), 8);
+        timeGraph.setEdgeWeight(timeGraph.addEdge("양재역", "양재시민의숲역"), 3);
+        dijkstraFastestPath = new DijkstraShortestPath(timeGraph);
+    }
+
+    public static List<String> getFastestPath(String startStation, String endStation) {
+        return dijkstraFastestPath.getPath(startStation, endStation).getVertexList();
+    }
+
+    public static int getFastestPathLength(String startStation, String endStation) {
+        return (int)dijkstraFastestPath.getPathWeight(startStation, endStation);
     }
 
 }
