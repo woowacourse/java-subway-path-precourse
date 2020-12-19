715bd43add92cb061a36af144c6b8be5c912a9b5
Seonho Kim <zxzx235@naver.com>
2020-12-19 14:02:08 +0900

    추가: 메인 메뉴 항목 리스트 추가

--- src/main/java/subway/domain/Constant.java
+++ src/main/java/subway/domain/Constant.java
@@ -1,13 +1,24 @@
 package subway.domain;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Constant {
+    static final String[] mainMenuItems = {"1. 경로 조회", "Q. 종료"};
+    private static final List<String> mainMenuItemList = Arrays.asList(mainMenuItems);
+
+    public static List<String> mainMenuItemList() {
+        return Collections.unmodifiableList(mainMenuItemList);
+    }
+
     static final String HEAD_HASH = "## ";
     static final String HEAD_INFO = "[INFO] ";
     static final String HEAD_ERROR = "[ERROR] ";
     static final String DIVIDER = "---";
 
-    public static final String CHOOSE_FUNCTION = "원하는 기능을 선택하세요.";
-    static final String MAIN_MENU_TITLE = "메인 화면";
+    static final String CHOOSE_FUNCTION = "원하는 기능을 선택하세요.";
+    public static final String MAIN_MENU_TITLE = "메인 화면";
 
     static final String IS_IN_MENU = "존재하지 않는 항목입니다.";
 }
