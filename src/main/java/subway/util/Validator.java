d2cab49fb8cbc5aa5791c32ca1e082fa4d1aea68
JinseongHwang <eddy5360@naver.com>
2020-12-19 16:22:34 +0900

    feat: 역을 입력받고 존재하는 역인지, 같은 역을 입력했는지 검증

--- src/main/java/subway/util/Validator.java
+++ src/main/java/subway/util/Validator.java
@@ -1,5 +1,6 @@
 package subway.util;
 
+import subway.domain.StationRepository;
 import subway.view.OutputView;
 
 import java.util.List;
@@ -11,6 +12,21 @@ public class Validator {
             OutputView.printError(OutputView.ERROR_INVALID_SELECT);
             throw new IllegalArgumentException();
         }
+    }
+
+    public static void existStation(String stationName) {
+        if (!StationRepository.isExist(stationName)){
+            OutputView.printError(OutputView.ERROR_INVALID_STATION);
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static boolean sameStation(String source, String destination) {
+        if (source.equals(destination)) {
+            OutputView.printError(OutputView.ERROR_SAME_STATION);
+            return true;
+        }
+        return false;
     }
 
 }
