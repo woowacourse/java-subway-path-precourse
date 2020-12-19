17bec9d9ac953a7edd75d12060880bc62765bc98
Hyeon9mak <jinha3507@gmail.com>
2020-12-19 16:53:40 +0900

    feat: add Station exception

--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -4,24 +4,37 @@ public class Station {
 
     private static final String WHITE_SPACE = " ";
     private static final String RULE_STATION_NAME = "역";
-    private static final int LAST_INDEX = 1;
     private static final int MINIMUM_NAME_LENGTH = 3;
 
-    private String name;
+    private final String name;
 
     private Station(String name) {
         validateWhiteSpace(name);
+        validateLastCharacter(name);
+        validateLength(name);
         this.name = name;
-    }
-
-    private void validateWhiteSpace(String name) {
-        if(name.contains(WHITE_SPACE)){
-            throw new IllegalArgumentException("지하철 역 이름에 공백이 포함될 수 없습니다.");
-        }
     }
 
     public static Station newStationWithName(String name) {
         return new Station(name);
+    }
+
+    private void validateLength(String name) {
+        if (name.length() < MINIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException("지하철 역 이름은 2글자 이상 + \"역\"으로 이루어져야 합니다.");
+        }
+    }
+
+    private void validateLastCharacter(String name) {
+        if (!name.endsWith(RULE_STATION_NAME)) {
+            throw new IllegalArgumentException("지하철 역 이름은 \"역\"으로 끝나야 합니다.");
+        }
+    }
+
+    private void validateWhiteSpace(String name) {
+        if (name.contains(WHITE_SPACE)) {
+            throw new IllegalArgumentException("지하철 역 이름에 공백이 포함될 수 없습니다.");
+        }
     }
 
     public String getName() {
