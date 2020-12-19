6c449403fdddef6459abbe7cd4c1198b1f7f0324
Seokchae Park <seokcpark@smilegate.com>
2020-12-19 15:17:40 +0900

    feat(StationRepository): 이름으로 Station 객체 찾는 기능 추가

--- src/main/java/subway/domain/StationRepository.java
+++ src/main/java/subway/domain/StationRepository.java
@@ -6,11 +6,21 @@ import java.util.List;
 import java.util.Objects;
 
 public class StationRepository {
+	private static final String NOT_FOUND_STATION_MESSAGE = "입력한 역을 찾을 수 없습니다.";
     private static final List<Station> stations = new ArrayList<>();
 
     public static List<Station> stations() {
         return Collections.unmodifiableList(stations);
     }
+    
+    public static Station getStationByName(String name) {
+		for(Station station : stations) {
+			if(station.getName().equals(name)) {
+				return station;
+			}
+		}
+		throw new IllegalArgumentException(NOT_FOUND_STATION_MESSAGE);
+	}
 
     public static void addStation(Station station) {
         stations.add(station);
@@ -18,9 +28,5 @@ public class StationRepository {
 
     public static boolean deleteStation(String name) {
         return stations.removeIf(station -> Objects.equals(station.getName(), name));
-    }
-
-    public static void deleteAll() {
-        stations.clear();
     }
 }
