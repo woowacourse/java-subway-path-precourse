7fdbd6af9248a67f9239c8bf7384e494641e8fe3
naru <hsik0225@gmail.com>
2020-12-19 14:40:59 +0900

    feat(distance): 이웃하는 역 사이의 거리를 담당하는 클래스 생성

---
+++ src/main/java/subway/domain/Distance.java
@@ -0,0 +1,22 @@
+package subway.domain;
+
+public class Distance {
+
+    private static final int MINIMUM_DISTANCE = 1;
+
+    public static final String NOT_POSITIVE_ERROR = "역 사이의 거리는 양의 정수이어야 합니다.";
+
+    private final int distance;
+
+    public Distance(int distance) {
+        if (distance < MINIMUM_DISTANCE) {
+            throw new IllegalArgumentException(NOT_POSITIVE_ERROR);
+        }
+
+        this.distance = distance;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
