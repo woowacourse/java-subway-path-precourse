efa93874dbcb67d60e59d3ab75aff09747ca388c
doorisopen <audauddl2@gmail.com>
2020-12-19 13:41:29 +0900

    feat: 원하는 기능 입력 요청 메시지 출력

---
+++ src/main/java/subway/view/InputView.java
@@ -0,0 +1,9 @@
+package subway.view;
+
+public class InputView {
+    private static final char NEW_LINE = '\n';
+
+    public void inputSelectMenuMessage() {
+        System.out.println(NEW_LINE+"## 원하는 기능을 선택하세요.");
+    }
+}
