a9cf102cdb0a0dfb0c63cbc8d59b2bbd1a923463
Sung Si Hyung <fjzjqhdl@gmail.com>
2020-12-19 15:14:10 +0900

    test : 구간의 거리와 걸리는 시간을 구하는 getter에 대한 테스트 작성

--- src/test/java/subway/domain/SectionTest.java
+++ src/test/java/subway/domain/SectionTest.java
@@ -1,5 +1,6 @@
 package subway.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,12 +8,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SectionTest {
+    Station from;
+    Station to;
+
+    @BeforeEach
+    void setUp() {
+        this.from = new Station("FROM");
+        this.to = new Station("TO");
+    }
 
     @DisplayName("구간의 이전역을 구한다")
     @Test
     void from() {
-        Station from = new Station("FROM");
-        Station to = new Station("TO");
         Section section = SectionFactory.makeSection(from, to, 1, 1);
         assertThat(section.from()).isEqualTo(new Station("FROM"));
     }
@@ -20,9 +27,21 @@ class SectionTest {
     @DisplayName("구간의 이후역을 구한다")
     @Test
     void to() {
-        Station from = new Station("FROM");
-        Station to = new Station("TO");
         Section section = SectionFactory.makeSection(from, to, 1, 1);
         assertThat(section.to()).isEqualTo(new Station("TO"));
+    }
+
+    @DisplayName("구간의 걸리는 시간을 구한다")
+    @Test
+    void getDistance() {
+        Section section = SectionFactory.makeSection(from, to, 1, 1);
+        assertThat(section.getTakenTime()).isEqualTo(1);
+    }
+
+    @DisplayName("구간의 거리를 구한다.")
+    @Test
+    void getTakenTime() {
+        Section section = SectionFactory.makeSection(from, to, 1, 1);
+        assertThat(section.getDistance()).isEqualTo(1);
     }
 }
