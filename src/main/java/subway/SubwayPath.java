655e918c2c5a9f6f62e50138fac08fdbfda7d5c1
KOREAparksh <45682868+KOREAparksh@users.noreply.github.com>
2020-12-19 14:44:45 +0900

    feat : station 사전데이터 등록 기능 구현

--- src/main/java/subway/SubwayPath.java
+++ src/main/java/subway/SubwayPath.java
@@ -1,5 +1,7 @@
 package subway;
 
+import subway.domain.data.Station;
+import subway.domain.data.StationRepository;
 import subway.view.InputView;
 import subway.view.OutputView;
 import java.util.Scanner;
@@ -17,7 +19,6 @@ public class SubwayPath {
     private void run(){
         OutputView.printMainView();
         String order = selectMenu();
-
     }
 
     private String selectMenu(){
@@ -27,6 +28,21 @@ public class SubwayPath {
         } catch (IllegalArgumentException e){
             OutputView.printError(e.getMessage());
             return selectMenu();
+        }
+    }
+
+    private void initData(){
+        initStation();
+        //initLine();
+        //initPath();
+    }
+
+    private void initStation() {
+        String[] stations = {"교대역", "강남역", "역삼역"
+                , "남부터미널역", "양재역", "양재시민의숲역", "매봉역"};
+
+        for(String station : stations) {
+            StationRepository.addStation(new Station(station));
         }
     }
 
