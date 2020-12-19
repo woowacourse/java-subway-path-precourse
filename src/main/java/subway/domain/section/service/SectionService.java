3554b94d3c901b68c3f4e523be17d8cbccac61fb
김경록 <goodboy3421@gmail.com>
2020-12-19 16:01:07 +0900

    style: 들여쓰기 code convention 규격에 맞춰 변경

--- src/main/java/subway/domain/section/service/SectionService.java
+++ src/main/java/subway/domain/section/service/SectionService.java
@@ -24,12 +24,13 @@ public class SectionService {
         WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
         List<Section> sections = SectionRepository.sections();
         sections.stream()
-                        .forEach(section -> section.updateRunTimeGraph(graph));
+                .forEach(section -> section.updateRunTimeGraph(graph));
 
-        return findShortestPath(startStation,arrivalStation,graph);
+        return findShortestPath(startStation, arrivalStation, graph);
     }
 
-    private static List findShortestPath(Station startStation, Station arrivalStation, WeightedMultigraph<Station, DefaultWeightedEdge> graph) {
+    private static List findShortestPath(Station startStation, Station arrivalStation,
+                                         WeightedMultigraph<Station, DefaultWeightedEdge> graph) {
         DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
         return dijkstraShortestPath.getPath(startStation, arrivalStation).getVertexList();
     }
