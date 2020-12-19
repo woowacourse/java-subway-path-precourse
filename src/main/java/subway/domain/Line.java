368794bb82a08455772adacf4fe1bed5ec9061a4
Hyeon9mak <jinha3507@gmail.com>
2020-12-19 16:55:38 +0900

    feat: add Line exception

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,10 +1,36 @@
 package subway.domain;
 
 public class Line {
+
+    private static final String WHITE_SPACE = " ";
+    private static final String RULE_STATION_NAME = "선";
+    private static final int MINIMUM_NAME_LENGTH = 3;
+
     private String name;
 
     private Line(String name) {
+        validateLastCharacter(name);
+        validateWhitepace(name);
+        validateLength(name);
         this.name = name;
+    }
+
+    private void validateLastCharacter(String name) {
+        if(!name.endsWith(RULE_STATION_NAME)){
+            throw new IllegalArgumentException("지하철 노선 이름은 \"선\"으로 끝나야 합니다.");
+        }
+    }
+
+    private void validateWhitepace(String name) {
+        if (name.contains(WHITE_SPACE)) {
+            throw new IllegalArgumentException("지하철 노선 이름에 공백이 포함될 수 없습니다.");
+        }
+    }
+
+    private void validateLength(String name) {
+        if (name.length() < MINIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException("지하철 노선 이름은 2글자 이상 + \"선\"으로 이루어져야 합니다.");
+        }
     }
 
     public static Line newLineWithName(String name) {
