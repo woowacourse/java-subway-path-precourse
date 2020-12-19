3bb0c7663d2433b09997f22b5e18298ff0448188
xrabcde <joara124@ewhain.net>
2020-12-19 13:48:20 +0900

    feat(Application): 메인화면 기능 목록과 입력 안내 문구 출력, 메인 기능 입력 구현

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,7 +1,7 @@
 package subway;
 
 import subway.domain.*;
-//import subway.view.*;
+import subway.view.*;
 
 import java.util.Scanner;
 
@@ -9,6 +9,13 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         Init.initialize();
-        //startProgram(scanner);
+        startProgram(scanner);
+    }
+
+    public static void startProgram(Scanner kbd) {
+        View.showMainMenu();
+        String mainInput = InputView.inputFunction(kbd, Constants.MAIN_FUNCTIONS);
+        System.out.println(mainInput);
+        //goSubMenu(mainInput, kbd);
     }
 }
