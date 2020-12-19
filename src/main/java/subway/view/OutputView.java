3b00cf0858fba09da0d6acbcbafc9c2da73d59bc
jaeseongDev <qkrwotjd1445@naver.com>
2020-12-19 16:12:51 +0900

    feat(OutputView) : 조회 결과 출력하는 메소드 추가

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -1,5 +1,8 @@
 package subway.view;
 
+import java.util.List;
+import subway.domain.station.Station;
+
 public class OutputView {
     public static void printMainMenu() { 
         System.out.println("## 메인 화면");
@@ -13,6 +16,18 @@ public class OutputView {
         System.out.println("1. 최단 거리");
         System.out.println("2. 최소 시간");
         System.out.println("B. 돌아가기");
+        printEmptyLine();
+    }
+    
+    public static void printFindRouteResult(int distance, int time, List<String> namesOfStations) {
+        System.out.println("## 조회 결과");
+        System.out.println("[INFO] ---");
+        System.out.println("[INFO] 총 거리: " + distance + "km");
+        System.out.println("[INFO] 총 소요 시간: " + time + "분");
+        System.out.println("[INFO] ---");
+        for (String name : namesOfStations) {
+            System.out.println("[INFO] " + name);
+        }
         printEmptyLine();
     }
 
