3f42377ae1efbb81faeb13a94686f9671eae9dba
Hansaem Woo <feliscatus@hanyang.ac.kr>
2020-12-19 16:28:50 +0900

    feat(Initializer): add methods
    노선 저장소를 초기화하는 메소드 추가
    저장소를 모두 초기화 요청할 때 사용할 public 메소드 추가

--- src/main/java/subway/util/Initializer.java
+++ src/main/java/subway/util/Initializer.java
@@ -3,6 +3,8 @@ package subway.util;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import subway.domain.Line;
+import subway.domain.LineRepository;
 import subway.domain.Station;
 import subway.domain.StationRepository;
 
@@ -11,12 +13,22 @@ public class Initializer {
     private Initializer() {
     }
 
-    private static void InitializeStationRepository() {
+    private static void initializeStationRepository() {
         final Set<String> stationNameSet = new HashSet<>();
         for (String[] stationNames : Constants.INITIAL_STATION_NAMES_IN_LINES) {
             stationNameSet.addAll(Arrays.asList(stationNames));
         }
         stationNameSet
             .forEach(stationName -> StationRepository.addStation(new Station(stationName)));
+    }
+
+    private static void initializeLineRepository() {
+        Arrays.asList(Constants.INITIAL_LINE_NAMES)
+            .forEach(lineName -> LineRepository.addLine(new Line(lineName)));
+    }
+
+    public static void initializeRepositories() {
+        initializeStationRepository();
+        initializeLineRepository();
     }
 }
