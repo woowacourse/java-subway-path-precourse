d05f10b21da9399bc3860d0b96c0f16ccfde2fe7
이동건 <tnpdlzi@naver.com>
2020-12-19 15:16:57 +0900

    feat(Application): 기능 실행 구현

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,41 @@
 package subway;
 
+import subway.domain.*;
+import subway.validator.Validator;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
+
+        LineRepository.lineInitialization();
+
+
+
+        selectUtils(scanner);
+    }
+
+    public static void selectUtils(Scanner scanner) {
+        final int MAIN_QUIT = 0;
+
+        while (true) {
+            String chosen;
+            int chosenNumber;
+            Route route = new Route(scanner);
+
+            System.out.println("## 메인 화면\n" +
+                    "1. 경로 조회\n" +
+                    "Q. 종료\n" +
+                    "\n" +
+                    "## 원하는 기능을 선택하세요.");
+            chosen = scanner.next();
+            chosenNumber = Validator.isMainInputRight(chosen);
+            if (chosenNumber == MAIN_QUIT) {
+                break;
+            }
+            route.play();
+        }
     }
 }
--- src/main/java/subway/domain/Route.java
+++ src/main/java/subway/domain/Route.java
@@ -1,22 +1,50 @@
 package subway.domain;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Route {
     private Scanner scanner;
     private Station startStation;
     private Station endStation;
+    private SubwayGraph subwayGraph = new SubwayGraph();
 
     public Route(Scanner scanner) {
         this.scanner = scanner;
     }
 
-    public void findDistanceByDistance() {
+    public void play() {
+        String number;
+        System.out.println("## 경로 기준\n" +
+                "1. 최단 거리\n" +
+                "2. 최소 시간\n" +
+                "B. 돌아가기\n" +
+                "\n" +
+                "## 원하는 기능을 선택하세요.");
+        number = scanner.next();
 
+        getStationName();
+
+        if (number.equals("1")) {
+            for (String station : findDistanceByDistance()) {
+                System.out.println("[INFO] " + station);
+            }
+        }
+        if (number.equals("2")) {
+            for (String station : findDistanceByTime()) {
+                System.out.println("[INFO] " + station);
+            }
+        }
     }
 
-    public void findDistanceByTime() {
+    public List<String> findDistanceByDistance() {
+        subwayGraph.setSubwayGraphByDistance();
+        return subwayGraph.getShortestPath(startStation, endStation);
+    }
 
+    public List<String> findDistanceByTime() {
+        subwayGraph.setSubwayGraphByTime();
+        return subwayGraph.getShortestPath(startStation, endStation);
     }
 
     public void getStationName() {
@@ -26,5 +54,4 @@ public class Route {
         System.out.println("## 도착역을 입력하세요.");
         this.endStation = StationRepository.getStationByName(scanner.next());
     }
-
 }
