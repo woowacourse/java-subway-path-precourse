7e64d91f432af977a18ae4863e873393767dce24
amy choi <cgc8016@ewhain.net>
2020-12-19 16:59:33 +0900

    feat(RouteLookUpManage): add findPathByDistanceContol and error handling except UNCONNECTED ERROR

--- src/main/java/subway/RouteLookUpManage.java
+++ src/main/java/subway/RouteLookUpManage.java
@@ -1,16 +1,30 @@
 package subway;
 
-import static subway.common.errorCase.FUNCTION_INPUT_ERROR;
-import static subway.common.logger.errorPrint;
-import static subway.view.inputView.inputFunctionNumber;
-import static subway.view.outputView.printPathTypes;
+import static subway.common.ErrorCase.FUNCTION_INPUT_ERROR;
+import static subway.common.ErrorCase.NO_SUCH_STATION_ERROR;
+import static subway.common.ErrorCase.SAME_DEPARTURE_ARRIVAL_ERROR;
+import static subway.common.ErrorCase.UNCONNECTED_DEPARTURE_ARRIVAL_ERROR;
+import static subway.common.Logger.errorPrint;
+import static subway.domain.Graph.DISTANCE_GRAPH;
+import static subway.domain.Graph.getTimeByVertexList;
+import static subway.domain.StationRepository.getStationByName;
+import static subway.view.InputView.inputArrivalStation;
+import static subway.view.InputView.inputDepartureStation;
+import static subway.view.InputView.inputFunctionNumber;
+import static subway.view.OutputView.printLookUpResult;
+import static subway.view.OutputView.printPathTypes;
 
+import java.util.List;
 import java.util.Scanner;
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
 
 public class RouteLookUpManage {
 	private final static String SHORTEST_DISTANCE = "1";
 	private final static String MINIMUM_TIME = "2";
 	private final static String BACK = "B";
+	static boolean ERROR_FLAG = false;
 
 	public static void routeLookUpManage(Scanner scanner) {
 		boolean exitFlag = false;
@@ -26,8 +40,7 @@ public class RouteLookUpManage {
 			return true;
 		}
 		if (input.equals(SHORTEST_DISTANCE)) {
-			//findPathByDistance(scanner);
-			return false;
+			return findPathByDistanceControl(scanner);
 		}
 		if (input.equals(MINIMUM_TIME)) {
 			//findPathByTime(scanner);
@@ -35,5 +48,54 @@ public class RouteLookUpManage {
 		}
 		errorPrint(FUNCTION_INPUT_ERROR);
 		return false;
+	}
+
+	private static boolean findPathByDistanceControl(Scanner scanner) {
+		String departureStation = inputDepartureStation(scanner);
+		if (!verifyExistStation(departureStation)) {
+			return false;
+		}
+
+		String arrivalStation = inputArrivalStation(scanner);
+		if (!verifyExistStation(arrivalStation)) {
+			return false;
+		}
+		if (!verifyDepartArrivalStation(departureStation, arrivalStation)) {
+			return false;
+		}
+
+		findPathByDistance(departureStation, arrivalStation);
+		return true;
+	}
+
+	private static boolean verifyExistStation(String stationName) {
+		if (getStationByName(stationName) == null) {
+			errorPrint(NO_SUCH_STATION_ERROR);
+			return false;
+		}
+		return true;
+	}
+
+	private static boolean verifyDepartArrivalStation(String departureStation,
+		String arrivalStation) {
+		if (departureStation.equals(arrivalStation)) {
+			errorPrint(SAME_DEPARTURE_ARRIVAL_ERROR);
+			return false;
+		}
+		// TODO : UNCONNECTED ERROR handling
+		return true;
+	}
+
+	private static void findPathByDistance(String departureStation, String arrivalStation) {
+		DijkstraShortestPath<String, DefaultWeightedEdge> shortestDistancePath = new DijkstraShortestPath<>(
+			DISTANCE_GRAPH);
+		GraphPath<String, DefaultWeightedEdge> path = shortestDistancePath
+			.getPath(departureStation, arrivalStation);
+
+		List<String> pathStations = path.getVertexList();
+		int totalDistance = (int) path.getWeight();
+		int totalTime = getTimeByVertexList(pathStations);
+
+		printLookUpResult(totalDistance, totalTime, pathStations);
 	}
 }
