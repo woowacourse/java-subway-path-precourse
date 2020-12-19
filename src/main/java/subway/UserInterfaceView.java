767a29986eb12712ababdd87e226ffdc05fe05b7
mungto <daum7766@naver.com>
2020-12-19 13:29:25 +0900

    feat(UserInterfaceView): 시작함수 생성

--- src/main/java/subway/UserInterfaceView.java
+++ src/main/java/subway/UserInterfaceView.java
@@ -3,9 +3,23 @@ package subway;
 import java.util.Scanner;
 
 public class UserInterfaceView {
+
     private static Scanner scanner;
 
     public UserInterfaceView(Scanner scanner) {
         this.scanner = scanner;
+    }
+
+    public void start() {
+        while (true) {
+            if (printViewAndReturnIsEnd()) {
+                break;
+            }
+        }
+    }
+
+    private boolean printViewAndReturnIsEnd() {
+        // TODO - 기능구현
+        return false;
     }
 }
