8d98e9ec13aba6aad8abe89e2540fd8a82046c9c
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-12-19 17:40:01 +0900

    refactor: OutputView 메시지 포맷 누락된 부분 추가

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -8,8 +8,8 @@ public class OutputView {
     private static final String MAIN_DISPLAY = "\n## 메인 화면\n1. 경로 조회\nQ. 종료";
     private static final String PATH_SEARCH_DISPLAY = "\n## 경로 기준\n1. 최단 거리\n2. 최소 시간\nB. 돌아가기";
     private static final String RESULT_PRINT_HEADER = "\n## 조회 결과";
-    private static final String TOTAL_DISTANCE_FORMAT = "\n[INFO] 총 거리 : %d";
-    private static final String TOTAL_TIME_FORMAT = "\n[INFO] 총 소요 시간 : %d";
+    private static final String TOTAL_DISTANCE_FORMAT = "\n[INFO] 총 거리 : %dkm";
+    private static final String TOTAL_TIME_FORMAT = "\n[INFO] 총 소요 시간 : %d분";
     private static final String RESULT_PRINT_DELIMITER = "\n[INFO] ---";
     private static final String INFORMATION_MESSAGE_FORMAT = "\n[INFO] %s";
     private static final String ERROR_MESSAGE_FORMAT = "\n[ERROR] %s\n";
@@ -30,7 +30,8 @@ public class OutputView {
     }
 
     public static void printPathResult(PathResponseDto pathResponseDto) {
-        System.out.println(RESULT_PRINT_HEADER);
+        System.out.print(RESULT_PRINT_HEADER);
+        System.out.print(RESULT_PRINT_DELIMITER);
         System.out.printf(TOTAL_DISTANCE_FORMAT, pathResponseDto.getDistanceTotal());
         System.out.printf(TOTAL_TIME_FORMAT, pathResponseDto.getTimeTotal());
         System.out.print(RESULT_PRINT_DELIMITER);
