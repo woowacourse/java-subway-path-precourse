e049c2ae57511c221353fce3816bec03d69d39ff
bepoz <kangsy763@naver.com>
2020-12-19 17:48:57 +0900

    refactor: DistanceAndTimeDto 필드값 final 키워드 추가

--- src/main/java/subway/service/dto/DistanceAndTimeDto.java
+++ src/main/java/subway/service/dto/DistanceAndTimeDto.java
@@ -1,8 +1,8 @@
 package subway.service.dto;
 
 public class DistanceAndTimeDto {
-    private int distance;
-    private int time;
+    private final int distance;
+    private final int time;
 
     private DistanceAndTimeDto(int distance, int time) {
         this.distance = distance;
