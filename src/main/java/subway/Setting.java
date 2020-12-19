d7a5ce0d3090691d43b0b7fb8a645aa15b969da5
amy choi <cgc8016@ewhain.net>
2020-12-19 16:07:04 +0900

    fix(Setting): fix initSetting for fix lookup only in one line bug

--- src/main/java/subway/Setting.java
+++ src/main/java/subway/Setting.java
@@ -1,5 +1,6 @@
 package subway;
 
+import static subway.domain.Graph.*;
 import static subway.domain.LineRepository.addLine;
 import static subway.domain.StationRepository.addStation;
 
@@ -12,6 +13,12 @@ import subway.domain.Station;
 public class Setting {
 	private final static List<String> INIT_STATIONS_NAME = new ArrayList<>(
 		Arrays.asList("교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역"));
+	private final static List<String> INIT_2ND_LINE_STATIONS = new ArrayList<>(
+		Arrays.asList("교대역", "강남역", "역삼역"));
+	private final static List<String> INIT_3RD_LINE_STATIONS = new ArrayList<>(
+		Arrays.asList("교대역", "남부터미널역", "양재역", "매봉역"));
+	private final static List<String> INIT_SINBUNDANG_LINE_STATIONS = new ArrayList<>(
+		Arrays.asList("강남역", "양재역", "양재시민의숲역"));
 
 	public static void initSetting() {
 		setInitStations();
@@ -27,27 +34,52 @@ public class Setting {
 	}
 
 	private static void setInit2ndLine() {
-		Line line = new Line("2호선", "교대역");
+		Line line = new Line("2호선");
 		addLine(line);
 
-		line.addNextStation("교대역", "강남역", 2, 3);
-		line.addNextStation("강남역", "역삼역", 2, 3);
+		for (String stationName : INIT_2ND_LINE_STATIONS) {
+			line.addStation(stationName);
+			addVertex(stationName);
+		}
+
+		addTimeEdge("교대역", "강남역", 3);
+		addTimeEdge("강남역", "역삼역", 3);
+
+		addDistanceEdge("교대역", "강남역", 2);
+		addDistanceEdge("강남역", "역삼역", 2);
 	}
 
 	private static void setInit3rdLine() {
-		Line line = new Line("3호선", "교대역");
+		Line line = new Line("3호선");
 		addLine(line);
 
-		line.addNextStation("교대역", "남부터미널역", 3, 2);
-		line.addNextStation("남부터미널역", "양재역", 6, 5);
-		line.addNextStation("양재역", "매봉역", 1, 1);
+		for (String stationName : INIT_3RD_LINE_STATIONS) {
+			line.addStation(stationName);
+			addVertex(stationName);
+		}
+
+		addTimeEdge("교대역", "남부터미널역", 2);
+		addTimeEdge("남부터미널역", "양재역", 5);
+		addTimeEdge("양재역", "매봉역", 1);
+
+		addDistanceEdge("교대역", "남부터미널역", 3);
+		addDistanceEdge("남부터미널역", "양재역", 6);
+		addDistanceEdge("양재역", "매봉역", 1);
 	}
 
 	private static void setInitSinbundangLine() {
-		Line line = new Line("신분당", "강남역");
+		Line line = new Line("신분당");
 		addLine(line);
 
-		line.addNextStation("강남역", "양재역", 2, 8);
-		line.addNextStation("양재역", "양재시민의숲역", 10, 3);
+		for (String stationName : INIT_SINBUNDANG_LINE_STATIONS) {
+			line.addStation(stationName);
+			addVertex(stationName);
+		}
+
+		addTimeEdge("강남역", "양재역", 8);
+		addTimeEdge("양재역", "양재시민의숲역", 3);
+
+		addDistanceEdge("강남역", "양재역", 2);
+		addDistanceEdge("양재역", "양재시민의숲역", 10);
 	}
 }
