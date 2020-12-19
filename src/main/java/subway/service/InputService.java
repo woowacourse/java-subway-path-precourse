241ae3a63ff46b29a140d0f2b742ee3d005e5a64
doorisopen <audauddl2@gmail.com>
2020-12-19 13:48:18 +0900

    feat: 출발역과 도착역을 입력한다

--- src/main/java/subway/service/InputService.java
+++ src/main/java/subway/service/InputService.java
@@ -11,4 +11,16 @@ public class InputService extends InputView {
         String menu = scanner.nextLine();
         return menu;
     }
+
+    public String inputStartStationName(Scanner scanner) {
+        inputStartStationNameMessage();
+        String stationName = scanner.nextLine();
+        return stationName;
+    }
+
+    public String inputEndStationName(Scanner scanner) {
+        inputEndStationNameMessage();
+        String stationName = scanner.nextLine();
+        return stationName;
+    }
 }
