e9000b6abe59e243ed281056be27b4f2b3296e5c
binghe819 <binghe819@naver.com>
2020-12-19 17:50:26 +0900

    test: 역, 구간 저장소 테스트 추가

--- src/test/java/subway/domain/SectionRepositoryTest.java
+++ src/test/java/subway/domain/SectionRepositoryTest.java
@@ -1,4 +1,24 @@
+package subway.domain;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 class SectionRepositoryTest {
-  
+
+    @Test
+    public void 구간_등록() {
+        // given
+        Station station1 = new Station("교대역");
+        Station station2 = new Station("강남역");
+        Station station3 = new Station("역삼역");
+        SectionRepository.addSection(station1, station2, 2, 3);
+        SectionRepository.addSection(station2, station3, 2, 3);
+
+        // when
+
+
+        // then
+    }
+
 }
--- src/test/java/subway/domain/StationRepositoryTest.java
+++ src/test/java/subway/domain/StationRepositoryTest.java
@@ -1,4 +1,23 @@
+package subway.domain;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 class StationRepositoryTest {
-  
+
+    @Test
+    public void findBy테스트() {
+        // given
+        Station station1 = new Station("강남역");
+        Station station2 = new Station("교대역");
+
+        // when
+        StationRepository.addStation(station1);
+        StationRepository.addStation(station2);
+
+        // then
+        assertEquals(station1, StationRepository.findByName(station1.getName()));
+    }
+
 }
