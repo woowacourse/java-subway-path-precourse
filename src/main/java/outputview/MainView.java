1e245bfd1b6b2e192dcc352514505bc88ebc4b2e
Ji-Ha <xodhkd36@gmail.com>
2020-12-19 13:45:19 +0900

    feat(MainView.java): 메인 메뉴의 출력 부분 추가

---
+++ src/main/java/outputview/MainView.java
@@ -0,0 +1,14 @@
+package outputview;
+
+public class MainView implements Output{
+    public static void printMain(){
+        System.out.println(HEAD + MAIN);
+        System.out.println(PATH_FINDER);
+        System.out.println(QUIT);
+        System.out.println();
+    }
+
+    public static void printFunctionSelect(){
+        System.out.println(HEAD + FUNCTION);
+    }
+}
---
+++ src/main/java/outputview/Output.java
@@ -0,0 +1,9 @@
+package outputview;
+
+public interface Output {
+    public final static String HEAD = "## ";
+    public final static String MAIN = "메인 화면";
+    public final static String PATH_FINDER = "1. 경로 조회";
+    public final static String QUIT = "Q. 종료";
+    public final static String FUNCTION = "원하는 기능을 입력하세요.";
+}
