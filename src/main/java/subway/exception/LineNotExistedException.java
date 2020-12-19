5640deeb55a38a621ea7176455f86d156f0f381d
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2020-12-19 17:02:24 +0900

    style(Exception): 메세지 개행 추가

--- src/main/java/subway/exception/LineNotExistedException.java
+++ src/main/java/subway/exception/LineNotExistedException.java
@@ -1,7 +1,7 @@
 package subway.exception;
 
 public class LineNotExistedException extends IllegalArgumentException {
-    private static final String message = "[ERROR] 해당 노선은 존재하지 않습니다";
+    private static final String message = "[ERROR] 해당 노선은 존재하지 않습니다.\n";
 
     @Override
     public String getMessage() {
--- src/main/java/subway/exception/StationNotExistedException.java
+++ src/main/java/subway/exception/StationNotExistedException.java
@@ -1,7 +1,7 @@
 package subway.exception;
 
 public class StationNotExistedException extends IllegalArgumentException {
-    private static final String message = "[ERROR] 해당 역이 존재하지 않습니다";
+    private static final String message = "[ERROR] 해당 역은 존재하지 않습니다.\n";
 
     @Override
     public String getMessage() {
