47614e8e8c260596dd45a4ef1390b7d139f5da64
Sung Si Hyung <fjzjqhdl@gmail.com>
2020-12-19 16:12:22 +0900

    feat : 이동한 구간의 총 거리, 총 시간을 구하는 기능 추가

--- src/main/java/subway/domain/SectionRepository.java
+++ src/main/java/subway/domain/SectionRepository.java
@@ -3,6 +3,9 @@ package subway.domain;
 import java.util.List;
 
 public class SectionRepository {
+
+    private static final int ONE = 1;
+
     public static void addSection(Line line, Section section) {
         Line target = LineRepository.findByName(line.getName());
         target.addSection(section);
@@ -15,5 +18,29 @@ public class SectionRepository {
             allSections.addAll(line.getSections().getUnmodifiableList());
         }
         return allSections;
+    }
+
+    public static Section findByName(String from, String to) {
+        return allSections().findByName(from, to);
+    }
+
+    public static int calculateTotalDistance(List<Station> stations) {
+        int totalDistance = 0;
+        for (int i = ONE; i < stations.size(); i++) {
+            String from = stations.get(i - ONE).getName();
+            String to = stations.get(i).getName();
+            totalDistance += SectionRepository.findByName(from, to).getDistance();
+        }
+        return totalDistance;
+    }
+
+    public static int calculateTakenTime(List<Station> stations) {
+        int totaltime = 0;
+        for (int i = ONE; i < stations.size(); i++) {
+            String from = stations.get(i - ONE).getName();
+            String to = stations.get(i).getName();
+            totaltime += SectionRepository.findByName(from, to).getTakenTime();
+        }
+        return totaltime;
     }
 }
--- src/main/java/subway/domain/Sections.java
+++ src/main/java/subway/domain/Sections.java
@@ -1,5 +1,7 @@
 package subway.domain;
 
+import subway.exception.SubwayException;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -7,19 +9,26 @@ import java.util.List;
 public class Sections {
     private List<Section> sections;
 
-    public Sections(){
+    public Sections() {
         sections = new ArrayList<>();
     }
 
-    public void addSection(Section section){
+    public void addSection(Section section) {
         sections.add(section);
     }
 
-    public List<Section> getUnmodifiableList(){
+    public List<Section> getUnmodifiableList() {
         return Collections.unmodifiableList(sections);
     }
 
-    public void addAll(List<Section> others){
+    public void addAll(List<Section> others) {
         sections.addAll(others);
+    }
+
+    public Section findByName(String from, String to) {
+        return sections.stream()
+                .filter(section -> section.from().getName().equals(from) && section.to().getName().equals(to))
+                .findAny()
+                .orElseThrow(() -> new SubwayException("해당 구간이 없습니다."));
     }
 }
