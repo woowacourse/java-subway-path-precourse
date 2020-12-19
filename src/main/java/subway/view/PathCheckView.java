d80776e47dd6aeaebc59adef2bb413eede047a9c
Seokchae Park <seokcpark@smilegate.com>
2020-12-19 13:52:13 +0900

    feat(PathCheckView): 최단 거리, 최소 시간 중 선택할 수 있는 경로 조회 화면 추가

---
+++ src/main/java/subway/view/PathCheckView.java
@@ -0,0 +1,38 @@
+package subway.view;
+
+import java.util.Scanner;
+
+public class PathCheckView extends SubwayPathManagerView {
+	private static final String TOP_MENU_MESSAGE = "경로 기준";
+	private static final String FIRST_MENU_MESSAGE = "최단 거리";
+	private static final String SECOND_MENU_MESSAGE = "최소 시간";
+	private static final String BACK_MENU_MESSAGE = "돌아가기";
+
+	public PathCheckView(Scanner scanner) {
+		print();
+		selectMenu(scanner);
+	}
+
+	private void selectMenu(Scanner scanner) {
+		String menuSelected = scanner.nextLine();
+		if (menuSelected.equals(FIRST_MENU)) {
+			new MinimumDistancePathCheckView(scanner);
+			return;
+		}
+		if (menuSelected.equals(SECOND_MENU)) {
+			new MinimumTimePathCheckView(scanner);
+			return;
+		}
+		if (menuSelected.equals(BACK_MENU)) {
+			new MainView(scanner);
+		}
+	}
+
+	@Override
+	public void print() {
+		printTopMenu(TOP_MENU_MESSAGE);
+		printFirstMenu(FIRST_MENU_MESSAGE);
+		printSecondMenu(SECOND_MENU_MESSAGE);
+		printBackMenu(BACK_MENU_MESSAGE);
+	}
+}
