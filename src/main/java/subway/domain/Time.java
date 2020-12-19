9ccd964fcc8ec45e7df99dea95b01fb62964f96e
yangfriendship <yangfriendship@naver.com>
2020-12-19 13:45:43 +0900

    feat (Time) : Duration을 사용하여 시간을 저장

--- src/main/java/subway/domain/Time.java
+++ src/main/java/subway/domain/Time.java
@@ -1,2 +1,56 @@
-package subway.domain;public class Time {
+package subway.domain;
+
+import java.time.Duration;
+import java.util.Objects;
+import subway.exception.InvalidTimeException;
+
+public class Time {
+
+    private static final long MINUTE_PER = 60;
+    private static final String TIME_UNIT = "분";
+    private static final long MIN_TIME_MINUTE = 1;
+
+    private Duration time;
+
+    private Time(Duration time) {
+        this.time = time;
+    }
+
+    public static Time of(long minute) {
+
+        if (minute < MIN_TIME_MINUTE) {
+            throw new InvalidTimeException(minute);
+        }
+        return new Time(Duration.ofMinutes(minute));
+    }
+
+    public Time add(Time time) {
+        return Time.of(getMinute() + time.getMinute());
+    }
+
+    public long getMinute() {
+        return time.getSeconds() / MINUTE_PER;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Time time1 = (Time) o;
+        return Objects.equals(time, time1.time);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(time);
+    }
+
+    @Override
+    public String toString() {
+        return getMinute() + TIME_UNIT;
+    }
 }
--- src/test/java/subway/domain/TimeTest.java
+++ src/test/java/subway/domain/TimeTest.java
@@ -1,4 +1,49 @@
+package subway.domain;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import jdk.jfr.Description;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class TimeTest {
-  
+
+    @Test
+    @Description("Time 생성 테스트")
+    public void TimeCreateTest(){
+
+        long minute = 60L;
+
+        Time time = Time.of(minute);
+        System.out.println(time.getMinute());
+        Assertions.assertTrue(time.getMinute() == minute);
+
+    }
+
+    @Test
+    @Description("Time 더하기 테스ㅌ")
+    public void timeAddTest(){
+
+        long secondsA = 60L;
+        long secondsB = 40L;
+
+        Time timeA = Time.of(secondsA);
+        Time timeB = Time.of(secondsB);
+
+        Time result = timeA.add(timeB);
+
+        Assertions.assertTrue(result.equals(secondsA+secondsB));
+
+    }
+
+    @Test
+    @Description("toString 확인")
+    public void TimeToStringTest(){
+
+        Time of = Time.of(10);
+
+        System.out.println(of.toString());
+
+    }
+
 }
