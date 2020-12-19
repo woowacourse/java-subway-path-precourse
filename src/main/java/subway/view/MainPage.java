58bae594bc20f4926aa2d67309bc21572e3c8017
perenok <perenoke@gmail.com>
2020-12-19 14:05:16 +0900

    feat(MainPage): add printWrongItemError
    printWrongItemError 메소드를 통해 입력값 예외 문구 출력

--- src/main/java/subway/view/MainPage.java
+++ src/main/java/subway/view/MainPage.java
@@ -1,9 +1,15 @@
 package subway.view;
 
+import utils.ErrorUtils;
+
 public class MainPage {
     public void printMainPage(){
         System.out.println("## 메인 화면");
         System.out.println("1. 경로 조회");
         System.out.println("Q. 종료");
+    }
+
+    public void printWrongItemError(){
+        ErrorUtils.printError("잘못된 입력입니다.");
     }
 }
