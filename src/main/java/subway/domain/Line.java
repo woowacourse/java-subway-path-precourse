d267189428c7e0a9d340f6ad02f36cbfeb654eaf
이동건 <tnpdlzi@naver.com>
2020-12-19 13:44:19 +0900

    feat(LineRepository): 노선의 역 및 거리와 시간 초기화

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,7 +1,13 @@
 package subway.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Line {
     private String name;
+    private ArrayList<Station> lineStations = new ArrayList<>();
+    private ArrayList<Distance> lineDistances = new ArrayList<>();
 
     public Line(String name) {
         this.name = name;
@@ -12,4 +18,29 @@ public class Line {
     }
 
     // 추가 기능 구현
+    public void setLineStations(ArrayList<Station> lineStations) {
+        this.lineStations = lineStations;
+    }
+
+    public void setTerminal(Station upTerminal, Station downTerninal) {
+        this.lineStations.add(upTerminal);
+        this.lineStations.add(downTerninal);
+    }
+
+    public void addStation(int index, Station station) {
+        lineStations.add(index - 1, station);
+    }
+
+    public void deleteStation(String name) {
+        lineStations.removeIf(station -> Objects.equals(station.getName(), name));
+    }
+
+    public List<Station> showStations() {
+        return lineStations;
+    }
+
+    public void setLineDistances(ArrayList<Distance> lineDistances) {
+        this.lineDistances = lineDistances;
+    }
+
 }
--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -1,11 +1,24 @@
 package subway.domain;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class LineRepository {
+    private static final int 교대에서_강남_거리 = 2;
+    private static final int 교대에서_강남_시간 = 3;
+    private static final int 강남에서_역삼_거리 = 2;
+    private static final int 강남에서_역삼_시간 = 3;
+    private static final int 교대에서_남부터미널_거리 = 3;
+    private static final int 교대에서_남부터미널_시간 = 2;
+    private static final int 남부터미널에서_양재_거리 = 6;
+    private static final int 남부터미널에서_양재_시간 = 5;
+    private static final int 양재에서_매봉_거리 = 1;
+    private static final int 양재에서_매봉_시간 = 1;
+    private static final int 강남에서_양재_거리 = 2;
+    private static final int 강남에서_양재_시간 = 8;
+    private static final int 양재에서_양재시민의숲_거리 = 10;
+    private static final int 양재에서_양재시민의숲_시간 = 3;
+
+
     private static final List<Line> lines = new ArrayList<>();
 
     public static List<Line> lines() {
@@ -22,5 +35,23 @@ public class LineRepository {
 
     public static void deleteAll() {
         lines.clear();
+    }
+
+    public void lineInitialization() {
+        Line secondLine = new Line("2호선");
+        secondLine.setLineStations(new ArrayList<Station>(Arrays.asList(StationRepository.getStationByName("교대역"), StationRepository.getStationByName("강남역"), StationRepository.getStationByName("역삼역"))));
+        secondLine.setLineDistances(new ArrayList<Distance>(Arrays.asList(new Distance(교대에서_강남_거리, 교대에서_강남_시간), new Distance(강남에서_역삼_거리, 강남에서_역삼_시간))));
+
+        Line thirdLine = new Line("3호선");
+        thirdLine.setLineStations(new ArrayList<Station>(Arrays.asList(StationRepository.getStationByName("교대역"), StationRepository.getStationByName("남부터미널역"), StationRepository.getStationByName("양재역"), StationRepository.getStationByName("매봉역"))));
+        thirdLine.setLineDistances(new ArrayList<Distance>(Arrays.asList(new Distance(교대에서_남부터미널_거리, 교대에서_남부터미널_시간), new Distance(남부터미널에서_양재_거리, 남부터미널에서_양재_시간), new Distance(양재에서_매봉_거리, 양재에서_매봉_시간))));
+        
+        Line newBundangLine = new Line("신분당선");
+        newBundangLine.setLineStations(new ArrayList<Station>(Arrays.asList(StationRepository.getStationByName("강남역"), StationRepository.getStationByName("양재역"), StationRepository.getStationByName("양재시민의숲역"))));
+        newBundangLine.setLineDistances(new ArrayList<Distance>(Arrays.asList(new Distance(강남에서_양재_거리, 강남에서_양재_시간), new Distance(양재에서_양재시민의숲_거리, 양재에서_양재시민의숲_시간))));
+
+        LineRepository.addLine(secondLine);
+        LineRepository.addLine(thirdLine);
+        LineRepository.addLine(newBundangLine);
     }
 }
