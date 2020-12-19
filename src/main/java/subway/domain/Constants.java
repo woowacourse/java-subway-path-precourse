965a1b542cf8cb5e14d64966c8d8b18a54ff963f
xrabcde <joara124@ewhain.net>
2020-12-19 13:46:23 +0900

    feat(Constants): 메인 메뉴와 서브메뉴에 해당하는 상수 추가

--- src/main/java/subway/domain/Constants.java
+++ src/main/java/subway/domain/Constants.java
@@ -13,4 +13,14 @@ public class Constants {
     public static final List<String> lineSinbundangStations = Arrays.asList("강남역", "양재역", "양재시민의숲역");
     public static final List<Integer> lineSinbundangDistances = Arrays.asList(2, 10);
     public static final List<Integer> lineSinbundangTimes = Arrays.asList(8, 3);
+
+    public static final int FUNCTION_INPUT_ERROR = 0;
+
+    public static final String FIND_PATH = "1";
+    public static final String FINISH_PROGRAM = "Q";
+    public static final List<String> MAIN_FUNCTIONS = Arrays.asList(FIND_PATH, FINISH_PROGRAM);
+    public static final String MIN_DISTANCE = "1";
+    public static final String MIN_TIME = "2";
+    public static final String GO_BACK_MENU = "B";
+    public static final List<String> SUB_FUNCTIONS = Arrays.asList(MIN_DISTANCE, MIN_TIME, GO_BACK_MENU);
 }
