6fc1e5112a50acb402b08d82e2947e8803e793f3
LeeJuHyuk <dlwngur1535@gmail.com>
2020-12-19 13:50:01 +0900

    feat: 노선 초기 데이터 설정 기능 구현

--- src/main/java/subway/SubwayMap.java
+++ src/main/java/subway/SubwayMap.java
@@ -1,6 +1,9 @@
 package subway;
 
+import subway.Initialization.LineInitialization;
 import subway.Initialization.StationInitialization;
+import subway.domain.Line;
+import subway.domain.LineRepository;
 import subway.domain.Station;
 import subway.domain.StationRepository;
 import java.util.Arrays;
@@ -13,11 +16,17 @@ public class SubwayMap {
 
     private void init() {
         initStations();
+        initLines();
     }
 
     private void initStations() {
         Arrays.stream(StationInitialization.values())
                 .forEach(station -> StationRepository.addStation(new Station(station.getName())));
+    }
+
+    private void initLines() {
+        Arrays.stream(LineInitialization.values())
+                .forEach(line -> LineRepository.addLine(new Line(line.getName())));
     }
 
     public static SubwayMap getInstance() {
