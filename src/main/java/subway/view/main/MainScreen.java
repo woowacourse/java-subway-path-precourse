e07af1d094ced9fdc04e50131824900501777934
operasystem6 <operasystem6@naver.com>
2020-12-19 17:35:00 +0900

    style: scanner 제거

--- src/main/java/subway/view/main/MainScreen.java
+++ src/main/java/subway/view/main/MainScreen.java
@@ -6,11 +6,10 @@ import subway.view.OutputView;
 import subway.view.TextCollection;
 
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class MainScreen {
 
-    public static MainMenu selectMenu(Scanner scanner) {
+    public static MainMenu selectMenu() {
         String input = InputView.inputFunction();
         if (!MainMenu.isValidInput(input)) {
             throw new SubwayException(TextCollection.WRONG_MENU_INPUT_MESSAGE);
