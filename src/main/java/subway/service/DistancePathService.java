6f431e2068f73ded73a8cb38d99164fb17a89111
bepoz <kangsy763@naver.com>
2020-12-19 15:53:51 +0900

    refactor: InputView 변경으로 인한 DistancePathService, TimePathService 리팩토링

--- src/main/java/subway/service/DistancePathService.java
+++ src/main/java/subway/service/DistancePathService.java
@@ -2,11 +2,13 @@ package subway.service;
 
 import subway.view.InputView;
 
+import static subway.view.InputView.*;
+
 public class DistancePathService {
 
     public static void serviceStart() {
-        String startStationName = inputView.inputStartStation();
-        inputView.inputEndStation(startStationName);
+        String startStationName = inputStartStation();
+        String arrivedStationName = inputEndStation(startStationName);
         calculateShortestDistancePath(startStationName, endStationName);
     }
 }
--- src/main/java/subway/service/TimePathService.java
+++ src/main/java/subway/service/TimePathService.java
@@ -1,10 +1,15 @@
 package subway.service;
 
+import subway.view.InputView;
+
+import static subway.util.PathCalculator.calculateShortestTimePath;
+import static subway.view.InputView.*;
+
 public class TimePathService {
 
     public static void serviceStart() {
-        String startStationName = inputView.inputStartStation();
-        inputView.inputEndStation(startStationName);
+        String startStationName = inputStartStation();
+        String endStationName = inputEndStation(startStationName);
         calculateShortestTimePath(startStationName, endStationName);
     }
 }
