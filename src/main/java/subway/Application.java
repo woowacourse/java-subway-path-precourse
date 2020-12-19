3dc003c95e2fcbeafe826de2ba352c61500bf98f
Wannte <T.wannte@gmail.com>
2020-12-19 16:47:12 +0900

    chore: 서비스 검증용 코드 삭제

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,23 +1,8 @@
 package subway;
 
-import subway.domain.EdgeRepository;
-import subway.domain.Station;
-import subway.domain.StationRepository;
-import subway.domain.SubwayGraph;
-
-import java.util.List;
-
 public class Application {
     public static void main(String[] args) {
         Settings.init();
-        Station s1 = StationRepository.getStation("교대역");
-        Station s2 = StationRepository.getStation("양재시민의숲역");
-        List<Station> stations = SubwayGraph.getDistanceShortestPath(s1,s2);
-        for (Station station: stations) {
-            System.out.println(station.getName());
-        }
-        System.out.println("Time" + EdgeRepository.getElapsedTime(stations));
-        System.out.println("Distance" + EdgeRepository.getRouteLength(stations));
         MainService.view();
     }
 }
