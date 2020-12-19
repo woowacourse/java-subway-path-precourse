b09fd13061cf6d193c555184a2a62add602d3356
xrabcde <joara124@ewhain.net>
2020-12-19 14:31:10 +0900

    feat(View): 최단경로 조회 결과를 출력해주는 displayPath 구현

--- src/main/java/subway/view/View.java
+++ src/main/java/subway/view/View.java
@@ -1,5 +1,12 @@
 package subway.view;
 
+import subway.domain.Line;
+import subway.domain.LineRepository;
+
+import java.util.Scanner;
+
+import static subway.Application.startProgram;
+
 public class View {
     public static void showMainMenu() {
         System.out.println("\n## 메인 화면");
@@ -12,6 +19,16 @@ public class View {
         System.out.println("1. 최단 거리");
         System.out.println("2. 최소 시간");
         System.out.println("B. 돌아가기");
+    }
+
+    public static void displayPath(Scanner kbd, int distance, int time, Line line) {
+        System.out.println("## 조회 결과");
+        System.out.println("[INFO] ---");
+        System.out.println("[INFO] 총 거리: "+ distance + "km");
+        System.out.println("[INFO] 총 소요 시간: "+ time + "분");
+        System.out.println("[INFO] ---");
+        line.displayLine();
+        startProgram(kbd);
     }
 
     public static void finishProgram() {
