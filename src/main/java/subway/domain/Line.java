acbe5dffefec3546b73f92afe3d983a261aeac97
pika96 <52442949+pika96@users.noreply.github.com>
2020-12-19 15:32:53 +0900

    feat: 노선의 구간 추가

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,7 +1,11 @@
 package subway.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Line {
     private String name;
+    private List<Station> sections = new ArrayList<>();
 
     public Line(String name) {
         this.name = name;
@@ -12,4 +16,7 @@ public class Line {
     }
 
     // 추가 기능 구현
+    public void addStationInSection(String name) {
+        sections.add(new Station(name));
+    }
 }
