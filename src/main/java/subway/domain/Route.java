513e282900c9b04e1a29ca23fcc305e43ee558c1
이동건 <tnpdlzi@naver.com>
2020-12-19 13:52:00 +0900

    feat(Route): 거리 찾기를 위한 Route 추가

---
+++ src/main/java/subway/domain/Route.java
@@ -0,0 +1,20 @@
+package subway.domain;
+
+import java.util.Scanner;
+
+public class Route {
+    private Scanner scanner;
+
+    public Route(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void findDistanceByDistance() {
+
+    }
+
+    public void findDistanceByTime() {
+
+    }
+
+}
