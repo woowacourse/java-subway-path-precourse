7afe7bd3715b85641733843e3917cd639481acba
Solji Choi <bssj9307@gmail.com>
2020-12-19 13:57:55 +0900

    feat(view): 시스템 출력 기능 - 메인 화면, 경로 조회의 기본 진행 메시지를 출력한다. - 사용자 입력 받을 때 메시지를 출력한다.

---
+++ src/main/java/subway/view/OutputView.java
@@ -0,0 +1,37 @@
+package subway.view;
+
+public class OutputView {
+    OutputMessages outputMessages;
+
+    public void OutputView() {
+        this.outputMessages = new OutputMessages();
+    }
+
+    public void mainView() {
+        System.out.println(outputMessages.MAIN_VIEW);
+        String[] messages = outputMessages.MAIN_OPTIONS;
+        for (String message : messages) {
+            System.out.println(message);
+        }
+    }
+
+    public void getUserOption() {
+        System.out.println(outputMessages.SELECT_OPTION);
+    }
+
+    public void mapView() {
+        System.out.println(outputMessages.MAP_VIEW);
+        String[] messages = outputMessages.MAP_OPTIONS;
+        for (String message : messages) {
+            System.out.println(message);
+        }
+    }
+
+    public void getUserStart() {
+        System.out.println(outputMessages.INPUT_START);
+    }
+
+    public void getUserArrive() {
+        System.out.println(outputMessages.INPUT_ARRIVE);
+    }
+}
