a2d5d842b19abb51bd14e6e58ffc817846bfa617
tributetothemoon <kkm97351@gmail.com>
2020-12-19 13:45:30 +0900

    feat(Initialization): 기초 역 정보 입력

--- src/main/java/subway/domain/StationRepository.java
+++ src/main/java/subway/domain/StationRepository.java
@@ -8,6 +8,16 @@ import java.util.Objects;
 public class StationRepository {
     private static final List<Station> stations = new ArrayList<>();
 
+    static {
+        addStation(new Station("교대역"));
+        addStation(new Station("강남역"));
+        addStation(new Station("역삼역"));
+        addStation(new Station("남부터미널역"));
+        addStation(new Station("매봉역"));
+        addStation(new Station("양재역"));
+        addStation(new Station("양재시민의 숲 역"));
+    }
+
     public static List<Station> stations() {
         return Collections.unmodifiableList(stations);
     }
