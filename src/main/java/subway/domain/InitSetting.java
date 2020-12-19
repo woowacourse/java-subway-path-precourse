84302c7bf4afcc8809c0762b12dc16fed2806c3a
dusdn1702 <dusdn1702@naver.com>
2020-12-19 17:41:59 +0900

    refactor: 초기 설정 함수 하나의 일만 하도록 나눔

--- src/main/java/subway/domain/InitSetting.java
+++ src/main/java/subway/domain/InitSetting.java
@@ -1,14 +1,25 @@
 package subway.domain;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class InitSetting {
-    static {
 
-    }
     public static void initSetting() {
-        Line secondLine = new Line("2호선");
-        Line thirdLine = new Line("3호선");
-        Line sinLine = new Line("신분당선");
+        List<Line> lines = makeLines();
 
+        List<Station> stations = makeStations();
+
+        addLines(lines);
+        addStations(stations);
+
+        addInitSecondLine(stations);
+        addInitThirdLine(stations);
+        addInitSinLine(stations);
+    }
+
+    private static List<Station> makeStations() {
         Station gyoDaeStation = new Station("교대역");
         Station kangnamStation = new Station("강남역");
         Station yeoksamStation = new Station("역삼역");
@@ -17,72 +28,56 @@ public class InitSetting {
         Station maebongStation = new Station("매봉역");
         Station yangjaeForestStation = new Station("양재시민의숲역");
 
-        LineRepository.addLine(secondLine);
-        LineRepository.addLine(thirdLine);
-        LineRepository.addLine(sinLine);
-
-        StationRepository.addStation(gyoDaeStation);
-        StationRepository.addStation(kangnamStation);
-        StationRepository.addStation(yeoksamStation);
-        StationRepository.addStation(nambuTerminalStation);
-        StationRepository.addStation(yangjaeStation);
-        StationRepository.addStation(maebongStation);
-        StationRepository.addStation(yangjaeForestStation);
-
-        SubwayLengthRepository.addStation(gyoDaeStation);
-        SubwayLengthRepository.addStation(kangnamStation);
-        SubwayLengthRepository.addStation(yeoksamStation);
-
-        SubwayLengthRepository.addStation(gyoDaeStation);
-        SubwayLengthRepository.addStation(nambuTerminalStation);
-        SubwayLengthRepository.addStation(yangjaeStation);
-        SubwayLengthRepository.addStation(maebongStation);
-
-        SubwayLengthRepository.addStation(kangnamStation);
-        SubwayLengthRepository.addStation(yangjaeStation);
-        SubwayLengthRepository.addStation(yangjaeForestStation);
-
-        SubwayTimeRepository.addStation(gyoDaeStation);
-        SubwayTimeRepository.addStation(kangnamStation);
-        SubwayTimeRepository.addStation(yeoksamStation);
-
-        SubwayTimeRepository.addStation(gyoDaeStation);
-        SubwayTimeRepository.addStation(nambuTerminalStation);
-        SubwayTimeRepository.addStation(yangjaeStation);
-        SubwayTimeRepository.addStation(maebongStation);
-
-        SubwayTimeRepository.addStation(kangnamStation);
-        SubwayTimeRepository.addStation(yangjaeStation);
-        SubwayTimeRepository.addStation(yangjaeForestStation);
-
-        addInitSecondLine(gyoDaeStation, kangnamStation, yeoksamStation);
-        addInitThirdLine(gyoDaeStation, nambuTerminalStation, yangjaeStation, maebongStation);
-        addInitSinLine(kangnamStation, yangjaeStation, yangjaeForestStation);
+        List<Station> stations = Arrays.asList(gyoDaeStation, kangnamStation, yeoksamStation, nambuTerminalStation, yangjaeStation, maebongStation, yangjaeForestStation);
+        return stations;
     }
 
-    private static void addInitSecondLine(Station index0, Station index1, Station index2) {
-        SubwayLengthRepository.setPathWeightWithTwoStation(index0, index1, 2);
-        SubwayLengthRepository.setPathWeightWithTwoStation(index1, index2, 2);
-
-        SubwayTimeRepository.setPathWeight(index0, index1, 3);
-        SubwayTimeRepository.setPathWeight(index1, index2, 3);
+    private static List<Line> makeLines() {
+        Line secondLine = new Line("2호선");
+        Line thirdLine = new Line("3호선");
+        Line sinLine = new Line("신분당선");
+        List<Line> lines = Arrays.asList(secondLine, thirdLine, sinLine);
+        return lines;
     }
 
-    private static void addInitThirdLine(Station index0, Station index1, Station index2, Station index3) {
-        SubwayLengthRepository.setPathWeightWithTwoStation(index0, index1, 3);
-        SubwayLengthRepository.setPathWeightWithTwoStation(index1, index2, 6);
-        SubwayLengthRepository.setPathWeightWithTwoStation(index2, index3, 1);
-
-        SubwayTimeRepository.setPathWeight(index0, index1, 2);
-        SubwayTimeRepository.setPathWeight(index1, index2, 5);
-        SubwayTimeRepository.setPathWeight(index2, index3, 1);
+    private static void addStations(List<Station> stations) {
+        for(Station station: stations){
+            StationRepository.addStation(station);
+            SubwayLengthRepository.addStation(station);
+            SubwayTimeRepository.addStation(station);
+        }
     }
 
-    private static void addInitSinLine(Station index0, Station index1, Station index2) {
-        SubwayLengthRepository.setPathWeightWithTwoStation(index0, index1, 2);
-        SubwayLengthRepository.setPathWeightWithTwoStation(index1, index2, 10);
+    private static void addLines(List<Line> lines) {
+        for(Line line: lines){
+            LineRepository.addLine(line);
+        }
+    }
 
-        SubwayTimeRepository.setPathWeight(index0, index1, 8);
-        SubwayTimeRepository.setPathWeight(index1, index2, 3);
+    private static void addInitSecondLine(List<Station> stations) {
+
+        SubwayLengthRepository.setPathWeightWithTwoStation(stations.get(0), stations.get(1), 2);
+        SubwayLengthRepository.setPathWeightWithTwoStation(stations.get(1), stations.get(2), 2);
+
+        SubwayTimeRepository.setPathWeightWithTwoStation(stations.get(0), stations.get(1), 3);
+        SubwayTimeRepository.setPathWeightWithTwoStation(stations.get(1), stations.get(2), 3);
+    }
+
+    private static void addInitThirdLine(List<Station> stations) {
+        SubwayLengthRepository.setPathWeightWithTwoStation(stations.get(0), stations.get(3), 3);
+        SubwayLengthRepository.setPathWeightWithTwoStation(stations.get(3), stations.get(4), 6);
+        SubwayLengthRepository.setPathWeightWithTwoStation(stations.get(4), stations.get(5), 1);
+
+        SubwayTimeRepository.setPathWeightWithTwoStation(stations.get(0), stations.get(3), 2);
+        SubwayTimeRepository.setPathWeightWithTwoStation(stations.get(3), stations.get(4), 5);
+        SubwayTimeRepository.setPathWeightWithTwoStation(stations.get(4), stations.get(5), 1);
+    }
+
+    private static void addInitSinLine(List<Station> stations) {
+        SubwayLengthRepository.setPathWeightWithTwoStation(stations.get(1), stations.get(4), 2);
+        SubwayLengthRepository.setPathWeightWithTwoStation(stations.get(4), stations.get(6), 10);
+
+        SubwayTimeRepository.setPathWeightWithTwoStation(stations.get(1), stations.get(4), 8);
+        SubwayTimeRepository.setPathWeightWithTwoStation(stations.get(4), stations.get(6), 3);
     }
 }
