46d8d07937d8d139b55488b3e0c986046e82489c
binghe819 <binghe819@naver.com>
2020-12-19 16:49:12 +0900

    feat: 노선 등록 기능 구현

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,15 +1,27 @@
 package subway.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Line {
     private String name;
+    private List<Station> stations = new ArrayList<>();
 
     public Line(String name) {
         this.name = name;
     }
 
+    public static Line of(String name, List<Station> stations) {
+        Line line = new Line(name);
+        line.addAll(stations);
+        return line;
+    }
+
+    public void addAll(List<Station> stations) {
+        this.stations.addAll(stations);
+    }
+
     public String getName() {
         return name;
     }
-
-    // 추가 기능 구현
 }
