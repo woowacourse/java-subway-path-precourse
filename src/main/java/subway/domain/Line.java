0bef40341f5e0f8be3a3c4ac00628d7327ff1515
perenok <perenoke@gmail.com>
2020-12-19 17:19:01 +0900

    feat(Line): add List<Station> stations
    stations를 통해 각 노선에 속한 역들을 알 수 있게 필드를 추가했다

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,7 +1,12 @@
 package subway.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Line {
     private String name;
+    private List<Station> stations = new ArrayList<>();
+
 
     public Line(String name) {
         this.name = name;
@@ -11,5 +16,8 @@ public class Line {
         return name;
     }
 
-    // 추가 기능 구현
+    public void addStation(String stationName){
+        Station station = new Station(stationName);
+        stations.add(station);
+    }
 }
