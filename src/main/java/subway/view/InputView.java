6961c087054161339d6ef70e0d432972748f3625
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-19 16:43:09 +0900

    feat: 기능 선택 예외 발생 시 재실행 되도록 추가

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -1,6 +1,7 @@
 package subway.view;
 
 import subway.domain.SceneState;
+import subway.exception.NoSuchChoiceException;
 
 import java.util.Scanner;
 
@@ -8,15 +9,28 @@ public class InputView {
     private static final String NEW_LINE = "\n";
     private static final String MAIN_SCENE = "main_scene_";
     private static final String PATH_SCENE = "path_scene_";
+    private static final String CHOICE_ERROR_MESSAGE = "[ERROR] 선택할 수 없는 기능입니다.";
 
     public static SceneState inputMainSceneChoice(Scanner scanner) {
-        System.out.println(NEW_LINE + "## 원하는 기능을 선택하세요.");
-        return SceneState.findSceneStateByInput(MAIN_SCENE + scanner.nextLine().toLowerCase());
+        try {
+            System.out.println(NEW_LINE + "## 원하는 기능을 선택하세요.");
+            return SceneState.findSceneStateByInput(MAIN_SCENE + scanner.nextLine().toLowerCase());
+        }
+        catch (NoSuchChoiceException e) {
+            System.out.println(CHOICE_ERROR_MESSAGE);
+            return inputMainSceneChoice(scanner);
+        }
     }
 
     public static SceneState inputPathSceneChoice(Scanner scanner) {
-        System.out.println(NEW_LINE + "## 원하는 기능을 선택하세요.");
-        return SceneState.findSceneStateByInput(PATH_SCENE + scanner.nextLine().toLowerCase());
+        try {
+            System.out.println(NEW_LINE + "## 원하는 기능을 선택하세요.");
+            return SceneState.findSceneStateByInput(PATH_SCENE + scanner.nextLine().toLowerCase());
+        }
+        catch (NoSuchChoiceException e) {
+            System.out.println(CHOICE_ERROR_MESSAGE);
+            return inputMainSceneChoice(scanner);
+        }
     }
 
     public static String inputDepartureStation(Scanner scanner) {
