a59327ea3717a7012e27461b9cdbe11ba420cef3
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-19 17:06:50 +0900

    feat: inputPathSceneChoice 에서 예외 발생 시 해당 메소드가 재실행되도록 변경

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -29,7 +29,7 @@ public class InputView {
         }
         catch (NoSuchChoiceException e) {
             System.out.println(CHOICE_ERROR_MESSAGE);
-            return inputMainSceneChoice(scanner);
+            return inputPathSceneChoice(scanner);
         }
     }
 
