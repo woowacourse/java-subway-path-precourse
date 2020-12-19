c5d65a3d663f375df755f8bf2d618bf96864038c
mungto <daum7766@naver.com>
2020-12-19 15:23:33 +0900

    feat(LineService): 노선에 역을 등록하는 함수

--- src/main/java/subway/LineService.java
+++ src/main/java/subway/LineService.java
@@ -2,11 +2,19 @@ package subway;
 
 import subway.domain.Line;
 import subway.domain.LineRepository;
+import subway.domain.Station;
+import subway.domain.StationRepository;
 
 public class LineService {
 
     public static void addLine(String name) {
         Line line = new Line(name);
         LineRepository.addLine(line);
+    }
+
+    public static void lineInAddStation(String lineName, String stationName) {
+        Line line = LineRepository.getLine(lineName);
+        Station station = StationRepository.getStation(stationName);
+        line.lineInAddStation(station);
     }
 }
