db3a9b2239f2f2b871774b63d50a8672c04eee2b
doorisopen <audauddl2@gmail.com>
2020-12-19 17:43:33 +0900

    feat: 초기 정보를 저장한다

--- src/main/java/subway/domain/SubwayPathFactory.java
+++ src/main/java/subway/domain/SubwayPathFactory.java
@@ -1,10 +1,32 @@
 package subway.domain;
 
+import static subway.domain.LineStationRepository.*;
+import static subway.domain.StationRepository.findStationByName;
+
 public class SubwayPathFactory {
+    private static String[] startStations = {"교대역", "강남역", "강남역", "역삼역", "교대역", "남부터미널역", "남부터미널역", "양재역",
+            "양재역", "매봉역", "강남역", "양재역", "양재역", "양재시민의숲역"};
+    private static String[] endStations = {"강남역", "교대역", "역삼역", "강남역", "남부터미널역", "교대역", "양재역", "남부터미널역",
+            "매봉역", "양재역", "양재역", "강남역", "양재시민의숲역", "양재역"};
+    private static int[] distances = {2, 2, 2, 2, 3, 3, 6, 6, 1, 1, 1, 1, 10, 10};
+    private static int[] times = {3, 3, 3, 3, 2, 2, 5, 5, 1, 1, 8, 8, 3, 3};
 
     public static void init() {
         initStation();
         initLine();
+        initGraphVertex();
+    }
+
+    public static void creatPathFirstGraph() {
+        for (int i = 0; i < startStations.length; i++) {
+            addLineStationOfSection(startStations[i], endStations[i], distances[i]);
+        }
+    }
+
+    public static void creatTimeFirstGraph() {
+        for (int i = 0; i < startStations.length; i++) {
+            addLineStationOfSection(startStations[i], endStations[i], times[i]);
+        }
     }
 
     private static void initStation() {
@@ -21,5 +43,15 @@ public class SubwayPathFactory {
         LineRepository.addLine(new Line("2호선"));
         LineRepository.addLine(new Line("3호선"));
         LineRepository.addLine(new Line("신분당선"));
+    }
+
+    private static void initGraphVertex() {
+        addVertex("교대역");
+        addVertex("강남역");
+        addVertex("역삼역");
+        addVertex("남부터미널역");
+        addVertex("양재역");
+        addVertex("양재시민의숲역");
+        addVertex("매봉역");
     }
 }
