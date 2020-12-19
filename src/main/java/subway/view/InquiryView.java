b3b1210523784100ef19948ee9589c02b3fb8119
platanus <platanus@canxan.com>
2020-12-19 16:41:22 +0900

    feat(inquiryview): Make show shortest distance feature

--- src/main/java/subway/view/InquiryView.java
+++ src/main/java/subway/view/InquiryView.java
@@ -1,5 +1,7 @@
 package subway.view;
 
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import subway.Subway;
@@ -9,6 +11,7 @@ import subway.util.Constants;
 import subway.util.DialogUtils;
 import subway.util.MessageUtils;
 
+@SuppressWarnings("checkstyle:Indentation")
 public class InquiryView {
 
     private boolean isRunning = true;
@@ -47,10 +50,45 @@ public class InquiryView {
         String endStationName = DialogUtils.ask(scanner, Constants.INQUIRY_END_STATION_ASK);
         MessageUtils.printInfoEntry(endStationName);
         MessageUtils.printAnnouncement(Constants.INQUIRY_RESULT);
-//        MessageUtils.printInfo(String.valueOf(getShortestDistance(startStationName, endStationName)));
-//        MessageUtils.printInfo(String.valueOf(getShortestTime(startStationName, endStationName)));
+        MessageUtils
+            .printInfo(String.valueOf(getShortestDistance(startStationName, endStationName)));
+        List wholePath = getShortestDistanceWholePath(startStationName, endStationName);
+        wholePath.stream().forEach(name -> MessageUtils.printInfoEntry((String) name));
+        MessageUtils.printInfoEntry(String.valueOf(getShortestDistancePathTime(wholePath)));
     }
 
+    private int getShortestDistance(String startStationName, String endStationName) {
+        Station startStation = subway.getStationRepository().findByName(startStationName);
+        Station endStation = subway.getStationRepository().findByName(endStationName);
+        return subway.getDistancePathRepository().findValue(startStation, endStation);
+    }
+
+
+    private List getShortestDistanceWholePath(String startStationName, String endStationName) {
+        return subway.getDistancePathRepository()
+            .findStationToStation(
+                subway.getStationRepository().findByName(startStationName),
+                subway.getStationRepository().findByName(endStationName));
+    }
+
+    private int getShortestDistancePathTime(List<String> stations) {
+        Iterator stationIter = stations.listIterator();
+        Integer cost = 0;
+        String previous = (String) stationIter.next();
+        while (stationIter.hasNext()) {
+            String current = (String) stationIter.next();
+            System.out.println(previous + current);
+            cost += subway.getTimePathRepository()
+                .findValue(
+                    subway.getStationRepository().findByName((String) previous),
+                    subway.getStationRepository().findByName((String) current));
+            previous = current;
+        }
+        return cost;
+    }
+
+    //////////
+    
     private void showMinimumTime() {
         String startStationName = DialogUtils.ask(scanner, Constants.INQUIRY_START_STATION_ASK);
         MessageUtils.printInfoEntry(startStationName);
@@ -59,17 +97,21 @@ public class InquiryView {
         MessageUtils.printAnnouncement(Constants.INQUIRY_RESULT);
     }
 
-    private int getShortestTime(String startStationName, String endStationName) {
+    private int getMinimumShortestTime(String startStationName, String endStationName) {
         Station startStation = subway.getStationRepository().findByName(startStationName);
         Station endStation = subway.getStationRepository().findByName(endStationName);
         return subway.getTimePathRepository().findValue(startStation, endStation);
     }
 
-    private int getShortestDistance(String startStationName, String endStationName) {
-        Station startStation = subway.getStationRepository().findByName(startStationName);
-        Station endStation = subway.getStationRepository().findByName(endStationName);
-        return subway.getDistancePathRepository().findValue(startStation, endStation);
+
+    private List getMinimumShortestTimeWholePath(String startStationName, String endStationName) {
+        List stations = subway.getTimePathRepository()
+            .findStationToStation(
+                subway.getStationRepository().findByName(startStationName),
+                subway.getStationRepository().findByName(endStationName));
+        return stations;
     }
+
 
     private int getStationToStationDistance() {
         return 0;
