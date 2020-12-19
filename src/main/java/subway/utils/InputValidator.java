347fa83bf6b9634b18cba0e3bcecdbfb73b469dc
Injun Son <ison@sfu.ca>
2020-12-19 14:44:00 +0900

    feat: 메인화면 사용자 입력 유효성 검사 기능

--- src/main/java/subway/utils/InputValidator.java
+++ src/main/java/subway/utils/InputValidator.java
@@ -2,4 +2,17 @@ package subway.utils;
 
 public class InputValidator {
 
+    private static final String QUIT_CODE = "Q";
+    private static final String ONE = "1";
+    private static final String MAIN_OPTION_ERROR = "1 또는 Q를 입력하세요";
+
+    public static void validateMainUserOption(String userOption) {
+        if (userOption.equals(QUIT_CODE)) {
+            return;
+        }
+        if (userOption.equals(ONE)) {
+            return;
+        }
+        throw new IllegalArgumentException(MAIN_OPTION_ERROR);
+    }
 }
--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -1,10 +1,10 @@
 package subway.view;
 
 import java.util.Scanner;
+import subway.utils.InputValidator;
 
 public class InputView {
 
-    private static final String MAIN_OPTION_ERROR = "1 또는 Q를 입력하세요";
     private static final String USER_OPTION_MESSAGE = "## 원하는 기능을 선택하세요";
     private static final String ERROR_PREFIX = "[ERROR]: ";
     private static Scanner scanner;
@@ -17,10 +17,10 @@ public class InputView {
         System.out.println(USER_OPTION_MESSAGE);
         String userOption = scanner.nextLine().trim();
         try {
-            validateMainUserOption(userOption);
+            InputValidator.validateMainUserOption(userOption);
             return userOption;
         } catch (Exception e) {
-            System.out.println(ERROR_PREFIX + MAIN_OPTION_ERROR);
+            System.out.println(ERROR_PREFIX + e.getMessage());
             return inputMainUserOption();
         }
     }
