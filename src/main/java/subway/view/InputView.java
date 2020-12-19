0baef33ac6900a0d5b84af65ed46b51689ed4db9
doorisopen <audauddl2@gmail.com>
2020-12-19 13:42:36 +0900

    feat: 출발역과 도착역 입력 요청 메시지 출력

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -6,4 +6,12 @@ public class InputView {
     public void inputSelectMenuMessage() {
         System.out.println(NEW_LINE+"## 원하는 기능을 선택하세요.");
     }
+
+    public void inputStartStationNameMessage() {
+        System.out.println(NEW_LINE+"## 출발역을 입력하세요.");
+    }
+
+    public void inputEndStationNameMessage() {
+        System.out.println(NEW_LINE+"## 도착역을 입력하세요.");
+    }
 }
