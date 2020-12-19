30e00cf3c4e761b982a1d1f6844b16ffd66f29be
Gomding <backchida@gmail.com>
2020-12-19 14:09:58 +0900

    feat : 구간들을 저장하는 SectionRepository 추가

---
+++ src/main/java/subway/domain/SectionRepository.java
@@ -0,0 +1,26 @@
+package subway.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class SectionRepository {
+    private static final List<Section> sections = new ArrayList<>();
+
+    public static List<Section> sections() {
+        return Collections.unmodifiableList(sections);
+    }
+
+    public static void addSection(Section section) {
+        sections.add(section);
+    }
+
+    public static boolean deleteSectionByName(String name) {
+        return sections.removeIf(line -> Objects.equals(line.getLinkedStationName(), name));
+    }
+
+    public static void deleteAll() {
+        sections.clear();
+    }
+}
