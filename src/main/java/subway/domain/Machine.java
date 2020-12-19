bf64c6bfce9b7974547b089780be43e407cda5ab
jaeseongDev <qkrwotjd1445@naver.com>
2020-12-19 13:54:02 +0900

    fix(Machine) : 메인 화면에서 값을 잘못 입력할 경우, 다시 메인화면을 출력하도록 수정

--- src/main/java/subway/domain/Machine.java
+++ src/main/java/subway/domain/Machine.java
@@ -7,23 +7,23 @@ import subway.view.OutputView;
 
 public class Machine {
     public void start(Scanner scanner) {
-        OutputView.printMainMenu();
-        MainFunction mainFunction = getInputFunction(scanner);
-        if (mainFunction == MainFunction.QUIT) {
-            return;
-        }
-        mainFunction.operate(scanner);
-        start(scanner);
-    }
-
-    private MainFunction getInputFunction(Scanner scanner) {
         try {
-            String functionNumber = InputView.inputFunctionNumber(scanner);
-            return MainFunction.getMainFunctionByNumber(functionNumber);
+            OutputView.printMainMenu();
+            MainFunction mainFunction = getInputFunction(scanner);
+            if (mainFunction == MainFunction.QUIT) {
+                return;
+            }
+            mainFunction.operate(scanner);
+            start(scanner);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             OutputView.printEmptyLine();
-            return getInputFunction(scanner);
+            start(scanner);
         }
+    }
+
+    private MainFunction getInputFunction(Scanner scanner) {
+        String functionNumber = InputView.inputFunctionNumber(scanner);
+        return MainFunction.getMainFunctionByNumber(functionNumber);
     }
 }
