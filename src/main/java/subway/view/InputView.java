4ac070530f9aaf7e94b939fce355d052a19ee997
bepoz <kangsy763@naver.com>
2020-12-19 15:51:01 +0900

    refactor: InputView static 리팩토링

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -8,34 +8,34 @@ public class InputView extends Validator {
     private static final String OPTION_SELECT_MESSAGE = "## 원하는 기능을 선택하세요.";
     private static final String START_STATION_MESSAGE = "## 출발역을 입력하세요.";
     private static final String ARRIVED_STATION_MESSAGE = "## 도착역을 입력하세요.";
-    private Scanner scanner;
+    private static Scanner scanner;
 
     public InputView(Scanner scanner) {
         this.scanner = scanner;
     }
 
-    public String inputMainMenuOption() {
+    public static String inputMainMenuOption() {
         System.out.println(OPTION_SELECT_MESSAGE);
         String option = scanner.nextLine();
         validateMainMenuOption(option);
         return option;
     }
 
-    public String inputPathMenuOption() {
+    public static String inputPathMenuOption() {
         System.out.println(OPTION_SELECT_MESSAGE);
         String option = scanner.nextLine();
         validatePathMenuOption(option);
         return option;
     }
 
-    public String inputStartStation() {
+    public static String inputStartStation() {
         System.out.println(START_STATION_MESSAGE);
         String startStationName = scanner.nextLine();
         validateStartStation(startStationName);
         return startStationName;
     }
 
-    public String inputEndStation(String startStationName) {
+    public static String inputEndStation(String startStationName) {
         System.out.println(ARRIVED_STATION_MESSAGE);
         String endStationName = scanner.nextLine();
         validateEndStation(endStationName, startStationName);
