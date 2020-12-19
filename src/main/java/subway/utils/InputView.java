44dc0d500ad9ea20a5ca8c33e39c7a4d01cc8e7d
sjparkk <66293784+sjparkk@users.noreply.github.com>
2020-12-19 13:35:49 +0900

    feat 입력 담당하는 InputView 생성 및 정의

---
+++ src/main/java/subway/utils/InputView.java
@@ -0,0 +1,17 @@
+package subway.utils;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static Scanner scanner;
+
+    public static void initScanner(Scanner nextScanner) {
+        scanner = nextScanner;
+    }
+
+    public static String inputSelect() {
+        return scanner.nextLine();
+    }
+
+
+}
