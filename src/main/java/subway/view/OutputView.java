725b27e84bef3350600f7fa76cc1310456da1176
KOREAparksh <45682868+KOREAparksh@users.noreply.github.com>
2020-12-19 13:33:54 +0900

    feat : 출력클래스 생성 및 내부함수 명명

---
+++ src/main/java/subway/view/OutputView.java
@@ -0,0 +1,29 @@
+package subway.view;
+
+public class OutputView {
+
+    public static void printMainView(){
+
+    }
+
+    public static void printPathRuleView(){
+
+    }
+
+    public static void printAskingFunction(){
+
+    }
+
+    public static void printAskingStartingStation(){
+
+    }
+
+    public static void printAskingEndingStation(){
+
+    }
+
+    public static void printResult(){
+        
+    }
+
+}
