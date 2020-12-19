06b58e162afea73c5f5b75e8641d46b05d791150
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-19 16:44:44 +0900

    feat: 경로를 관리할 SectionRepository 추가

---
+++ src/main/java/subway/domain/SectionRepository.java
@@ -0,0 +1,11 @@
+package subway.domain;
+
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+
+public class SectionRepository {
+    private static final WeightedMultigraph<Station, DefaultWeightedEdge> distanceGraph
+            = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+    private static final  WeightedMultigraph<Station, DefaultWeightedEdge> timeGraph
+            = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+}
