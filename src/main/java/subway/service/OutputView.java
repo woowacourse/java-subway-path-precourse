dd52c137203affc4896a3e3f17852549f1084d11
doorisopen <audauddl2@gmail.com>
2020-12-19 13:52:36 +0900

    feat: 경로 조회 화면을 출력한다

--- src/main/java/subway/service/OutputView.java
+++ src/main/java/subway/service/OutputView.java
@@ -8,4 +8,11 @@ public class OutputView {
         System.out.println("1. 경로 조회");
         System.out.println("Q. 종료");
     }
+
+    public void printSearchPathMenu() {
+        System.out.println(NEW_LINE+"## 경로 기준");
+        System.out.println("1. 최단 거리");
+        System.out.println("2. 최소 시간");
+        System.out.println("B. 돌아가기");
+    }
 }
