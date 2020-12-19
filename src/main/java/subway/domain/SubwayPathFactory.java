e44a9375ef1d80a66203a74345ac0fc553ce8b79
doorisopen <audauddl2@gmail.com>
2020-12-19 14:10:24 +0900

    feat: 지하철 초기 정보를 저장한다

---
+++ src/main/java/subway/domain/SubwayPathFactory.java
@@ -0,0 +1,18 @@
+package subway.domain;
+
+public class SubwayPathFactory {
+
+    public static void init() {
+        initStation();
+    }
+
+    private static void initStation() {
+        StationRepository.addStation(new Station("교대역"));
+        StationRepository.addStation(new Station("강남역"));
+        StationRepository.addStation(new Station("역삼역"));
+        StationRepository.addStation(new Station("남부터미널역"));
+        StationRepository.addStation(new Station("양재역"));
+        StationRepository.addStation(new Station("양재시민의숲역"));
+        StationRepository.addStation(new Station("매봉역"));
+    }
+}
