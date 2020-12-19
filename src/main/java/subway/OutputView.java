529c630a921912d070dd7c6d867eadb75f74c429
Wannte <T.wannte@gmail.com>
2020-12-19 16:46:12 +0900

    feat(RouteSearch): 경로 조회 출력 로직 구현

--- src/main/java/subway/OutputView.java
+++ src/main/java/subway/OutputView.java
@@ -2,6 +2,8 @@ package subway;
 
 import subway.domain.Station;
 
+import java.util.List;
+
 public class OutputView {
     public static final String MAIN_HEADER = "## 메인 화면";
     public static final String MAIN_OPTIONS = "1. 경로 조회, \nQ.종료";
@@ -11,6 +13,9 @@ public class OutputView {
     public static final String INPUT_START_STATION = "## 출발역을 입력하세요.";
     public static final String INPUT_END_STATION = "## 도착역을 입력하세요.";
     public static final String SEARCH_RESULT_HEADER = "## 조회 결과";
+    public static final String DOTS = "---";
+    public static final String INFO_PREFIX = "[INFO] ";
+    public static final String ERROR_PREFIX = "[ERROR] ";
 
     private OutputView() {
     }
@@ -26,15 +31,26 @@ public class OutputView {
         System.out.println(CHOOSE_OPTIONS);
     }
 
-    public static void printStartStationMessage() {
+    public static void printFromStationMessage() {
         System.out.println(INPUT_START_STATION);
     }
 
-    public static void printEndStationMessage() {
+    public static void printToStationMessage() {
         System.out.println(INPUT_END_STATION);
     }
 
-    public static void printSearchResult(Station start,Station end){
+    public static void printSearchResult(int routeLength,int elapsedTime, List<Station> stations){
         System.out.println(SEARCH_RESULT_HEADER);
+        printInfo(DOTS);
+        printInfo("총 거리: " + routeLength + "km");
+        printInfo("총 소요 시간: " + elapsedTime + "분");
+        printInfo(DOTS);
+        for (Station station : stations) {
+            printInfo(station.getName());
+        }
+    }
+
+    private static void printInfo (String message) {
+        System.out.println(INFO_PREFIX + message);
     }
 }
--- src/main/java/subway/RouteSearchService.java
+++ src/main/java/subway/RouteSearchService.java
@@ -1,28 +1,66 @@
 package subway;
 
+import subway.domain.EdgeRepository;
 import subway.domain.Station;
 import subway.domain.StationRepository;
+import subway.domain.SubwayGraph;
+
+import java.util.List;
 
 public class RouteSearchService {
+    public static final String SHORTEST_DISTANCE_CODE = "1";
+    public static final String SHORTEST_ELAPSED_TIME_CODE = "2";
+    public static final String ERR_SAME_FROM_TO_STATION = "출발역과 도착역이 동일합니다.";
     public static void view() {
         OutputView.printRouteSearchView();
-        String answer = InputView.getAnswer();
-        if (answer.equals("1") || answer.equals("2")) {
-            getRoute(answer);
+        String code = InputView.getAnswer();
+        if (code.equals(SHORTEST_DISTANCE_CODE)) {
+            getShortestDistanceRoute();
             return;
         }
-        if (answer.equals("B")) {
+        if (code.equals(SHORTEST_ELAPSED_TIME_CODE)) {
+            getShortestTimeRoute();
+            return;
+        }
+        if (code.equals("B")) {
             return;
         }
         throw new IllegalArgumentException("잘못된 입력값입니다.");
     }
 
-    private static void getRoute(String code) {
-        OutputView.printStartStationMessage();
-        Station start = StationRepository.getStation(InputView.getStation());
-        OutputView.printEndStationMessage();
-        Station end = StationRepository.getStation(InputView.getStation());
-        OutputView.printSearchResult(start, end);
+    private static void getShortestDistanceRoute() {
+        Station from = getStationFrom();
+        Station to = getStationTo();
+        validateNotSameStation(from,to);
+        printResult(SubwayGraph.getDistanceShortestPath(from, to));
     }
 
+    private static void getShortestTimeRoute() {
+        Station from = getStationFrom();
+        Station to = getStationTo();
+        validateNotSameStation(from,to);
+        printResult(SubwayGraph.getTimeShortestPath(from, to));
+    }
+
+    private static void printResult(List<Station> stations) {
+        int routeLength = EdgeRepository.getRouteLength(stations);
+        int elapsedTime = EdgeRepository.getElapsedTime(stations);
+        OutputView.printSearchResult(routeLength,elapsedTime,stations);
+    }
+
+    private static Station getStationTo() {
+        OutputView.printToStationMessage();
+        return StationRepository.getStation(InputView.getStation());
+    }
+
+    private static Station getStationFrom() {
+        OutputView.printFromStationMessage();
+        return StationRepository.getStation(InputView.getStation());
+    }
+
+    private static void validateNotSameStation(Station from, Station to) {
+        if (from.equals(to)) {
+            throw new IllegalArgumentException(ERR_SAME_FROM_TO_STATION);
+        }
+    }
 }
