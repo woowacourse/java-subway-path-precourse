bc59933f77bf1df2627e648de4ce659ad7b0221f
bbakjae <qkrwogus0724@naver.com>
2020-12-19 13:18:22 +0900

    feat(Line.java): 초기 설정에 필요한 객체 선언 및 함수 작성

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,7 +1,11 @@
 package subway.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Line {
     private String name;
+    private List<Station> stations = new ArrayList<Station>();
 
     public Line(String name) {
         this.name = name;
@@ -12,4 +16,20 @@ public class Line {
     }
 
     // 추가 기능 구현
+    
+    public static Station get_station_by_name(String name) {
+		for (Station station : StationRepository.stations()) {
+			if (name.equals(station.getName())) {
+				return station;
+			}
+		}
+		return null;
+	}
+    
+    public Line(String name, List<String> station_name) {
+    	this.name = name;
+    	for (String station : station_name) {    		
+    		stations.add(get_station_by_name(station));
+    	}
+    }
 }
