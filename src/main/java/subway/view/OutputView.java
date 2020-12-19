286b0e9fc6dfdb84eb4068cbcf6d880e2ba433b6
byungwook <bbwwpark@naver.com>
2020-12-19 17:55:48 +0900

    refactor: 메서드내에서 쓰이는 문자열 상수로 수정

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -7,9 +7,14 @@ import subway.domain.selector.menu.Menu;
 public class OutputView {
 
     private static final String INFORMATION_TAG = "[INFO]";
-    private static final String LINE_STATION_SEPARATOR = "---";
+    private static final String LINE_STATION_SEPARATOR = " ---";
     private static final String SCREEN_MESSAGE_START = "\n## ";
     private static final String ID_SEPARATOR = ". ";
+    private static final String DISTANCE_UNIT = "km";
+    private static final String MINUTE_UNIT = " 분";
+    private static final String TOTAL_DISTANCE_MESSAGE = " 총거리: ";
+    private static final String TOTAL_TIME_MESSAGE = " 총 소요 시간: ";
+
 
     public static void printScreen(Menu menu) {
         System.out.println(SCREEN_MESSAGE_START + menu);
@@ -31,18 +36,18 @@ public class OutputView {
     }
 
     public static void printDistance(double distance) {
-        System.out.println("\n[INFO] ---");
-        System.out.println("[INFO] 총거리: " + Math.round(distance) + "km");
+        System.out.println("\n" + INFORMATION_TAG + LINE_STATION_SEPARATOR);
+        System.out.println(INFORMATION_TAG + TOTAL_DISTANCE_MESSAGE + Math.round(distance) + DISTANCE_UNIT);
     }
 
     public static void printTravelTime(double travelTime) {
-        System.out.println("[INFO] 총 소요 시간: " + Math.round(travelTime) + " 분");
+        System.out.println(INFORMATION_TAG + TOTAL_TIME_MESSAGE + Math.round(travelTime) + MINUTE_UNIT);
     }
 
     public static void printStations(List<Station> shortestPath) {
-        System.out.println("[INFO] ---");
+        System.out.println(INFORMATION_TAG + LINE_STATION_SEPARATOR);
         for (Station station : shortestPath) {
-            System.out.println("[INFO] " + station.getName());
+            System.out.println(INFORMATION_TAG + " " + station.getName());
         }
     }
 
