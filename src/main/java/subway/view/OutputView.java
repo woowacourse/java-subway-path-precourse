3f0167cc42fe47e23d54aee467643310cffeed5c
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-19 16:34:45 +0900

    feat: 메인 화면 출력 추가

---
+++ src/main/java/subway/view/OutputView.java
@@ -0,0 +1,11 @@
+package subway.view;
+
+public class OutputView {
+    private static final String NEW_LINE = "\n";
+
+    public static void printMainScene() {
+        System.out.println(NEW_LINE + "## 메인 화면");
+        System.out.println("1. 경로 조회");
+        System.out.println("Q. 종료");
+    }
+}
