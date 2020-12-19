5e935ae7b3b655e7ae1d56392dbd7e701ff5a3d4
perenok <perenoke@gmail.com>
2020-12-19 17:18:22 +0900

    feat(RoutePage): update RoutePage
    각 경우에 필요한 출력문을 모아둔 클래스이다

---
+++ src/main/java/subway/view/RoutePage.java
@@ -0,0 +1,56 @@
+package subway.view;
+
+import utils.ErrorUtils;
+import utils.InfoUtils;
+
+import java.util.List;
+
+public class RoutePage {
+    public void printRoutePage(){
+        System.out.println("## 경로 기준");
+        System.out.println("1. 최단 거리");
+        System.out.println("1. 최소 시간");
+        System.out.println("Q. 종료");
+        System.out.println("\n## 원하는 기능을 선택하세요.");
+    }
+
+    public void printDepartureStationPage(){
+        System.out.println("\n## 출발역을 입력하세요.");
+    }
+
+    public void printArrivalStationPage(){
+        System.out.println("\n## 도착역을 입력하세요.");
+    }
+
+    public void printSelectResult(int distance, int time, List<String> shortestPath) {
+        System.out.println("\n## 조회 결과");
+        printSelectEdge(distance, time);
+        printShortestPath(shortestPath);
+    }
+
+    public void printSelectEdge(int distance, int time) {
+        InfoUtils.printInfo("---");
+        InfoUtils.printInfo("총 거리: " + distance + "km");
+        InfoUtils.printInfo("총 소요 시간: " + time + "분");
+        InfoUtils.printInfo("---");
+    }
+
+    public void printShortestPath(List<String> shortestPath){
+        for(String one : shortestPath){
+            InfoUtils.printInfo("");
+        }
+        System.out.println();
+    }
+
+    public void printWrongItemError(){
+        ErrorUtils.printError("잘못된 입력입니다.");
+    }
+
+    public void printNullStationError(){
+        ErrorUtils.printError("존재하지 않는 역입니다.");
+    }
+
+    public void printSameStationError(){
+        ErrorUtils.printError("출발역과 도착역이 동일합니다.");
+    }
+}
