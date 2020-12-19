50af50d02b7db698194e5ca609985f192bd9ee57
yejin <jinee0717@daum.net>
2020-12-19 15:42:33 +0900

    feat : 경로 정보를 가지는 Path 클래스 구현
    역 목록, 총 소요시간, 총 거리를 멤버로 가지는 Path 클래스 생성

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -19,26 +19,10 @@ public class Line {
         this.name = name;
         for(int i=0; i< stations.length; i++){
             stationList.add(stations[i]);
-//            path.addVertex(stations[i]);
         }
     }
 
     public String getName() {
         return name;
-    }
-
-    public void getDijkstraShortestPath() {
-        WeightedMultigraph<String, DefaultWeightedEdge> graph
-                = new WeightedMultigraph(DefaultWeightedEdge.class);
-        graph.addVertex("v1");
-        graph.addVertex("v2");
-        graph.addVertex("v3");
-        graph.setEdgeWeight(graph.addEdge("v1", "v2"), 2);
-        graph.setEdgeWeight(graph.addEdge("v2", "v3"), 2);
-        graph.setEdgeWeight(graph.addEdge("v1", "v3"), 100);
-
-        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
-        List<String> shortestPath = dijkstraShortestPath.getPath("v3", "v1").getVertexList();
-//        assertThat(shortestPath.size()).isEqualTo(3);
     }
 }
