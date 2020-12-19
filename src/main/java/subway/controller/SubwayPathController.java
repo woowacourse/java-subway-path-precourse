39068b7a5f61dbe17a7fc98990ce4a12f04e9293
thisisyoungbin <thisisyoungbin@gmail.com>
2020-12-19 14:41:01 +0900

    feat(SubwayPathController): 경로 기준에 따라 역을 입력 받고, 역의 이름이 유효한지 검증하는 기능 구현

--- src/main/java/subway/controller/SubwayPathController.java
+++ src/main/java/subway/controller/SubwayPathController.java
@@ -1,2 +1,66 @@
-package subway.controller;public class SubwayPathController {
+package subway.controller;
+
+import subway.domain.Line;
+import subway.domain.LineRepository;
+import subway.domain.StationRepository;
+import subway.view.InputView;
+import subway.view.OutputView;
+
+public class SubwayPathController {
+    private static SubwayPathController subwayPathController;
+    private String departureStation;
+    private String arrivalStation;
+
+
+    public static SubwayPathController getInstance() {
+        if (subwayPathController==null) {
+            subwayPathController = new SubwayPathController();
+        }
+        return subwayPathController;
+    }
+
+    public Boolean printByMinDistance() {
+        if (!isExistStation(departureStation = InputView.getDepartureStation())) {
+            return false;
+        }
+        if (isValidArrivalStation(arrivalStation = InputView.getArrivalStation())) {
+            return false;
+        }
+
+    }
+
+    public Boolean printByMinTime() {
+        if (!isExistStation(departureStation = InputView.getDepartureStation())) {
+            return false;
+        }
+        if (isValidArrivalStation(arrivalStation = InputView.getArrivalStation())) {
+            return false;
+        }
+
+    }
+
+    private boolean isValidArrivalStation(String s) {
+        return notSameWithDeparture(s) && isExistStation(s);
+    }
+
+    private boolean notSameWithDeparture(String s) {
+        boolean result = departureStation.equals(s);
+        if (!result) {
+            OutputView.sameStationError();
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isExistStation(String stationName){
+        if (!StationRepository.searchByName(stationName)) {
+            OutputView.notExistStationError();
+            return false;
+        }
+        return true;
+    }
+
+    public Boolean back() {
+        return true;
+    }
 }
