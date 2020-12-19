034d595a19c94c383010ee65fc80803df7db10ef
binghe819 <binghe819@naver.com>
2020-12-19 16:23:27 +0900

    feat: 지하철 역 추가 및 조회 기능 구현

--- src/main/java/subway/domain/StationRepository.java
+++ src/main/java/subway/domain/StationRepository.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import subway.error.StationNotExistException;
 
 public class StationRepository {
     private static final List<Station> stations = new ArrayList<>();
@@ -14,6 +15,26 @@ public class StationRepository {
 
     public static void addStation(Station station) {
         stations.add(station);
+    }
+
+    public static void addStations(List<Station> stations) {
+        stations.addAll(stations);
+    }
+
+    public static boolean checkExistsStation(String name) {
+        for (Station station : stations) {
+            if (station.getName().equals(name)) {
+                return true;
+            }
+        }
+        throw new StationNotExistException();
+    }
+
+    public static Station findByName(String name) {
+        return stations.stream()
+            .filter(station -> station.getName().equals(name))
+            .findAny()
+            .orElseThrow(() -> {throw new StationNotExistException();});
     }
 
     public static boolean deleteStation(String name) {
