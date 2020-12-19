0c5ea7d169af8f9cf62f8ca1379f717399d68452
김경록 <goodboy3421@gmail.com>
2020-12-19 17:33:01 +0900

    feat: Manager interface를 구현하여 Controller단 구성
    전체적인 Controller단 구현 (Controller 끼리 의존성을 띄고있어서 커밋단위가 애매했음)

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -7,18 +7,24 @@ import java.util.Scanner;
 public class InputView {
     private static final String INPUT_START_STATION_MESSAGE = "## 출발역을 입력하세요.";
     private static final String INPUT_ARRIVAL_STATION_MESSAGE = "## 도착역을 입력하세요.";
+    private static final String INPUT_WANTED_FUNCTION_MESSAGE = "## 원하는 기능을 선택하세요.";
 
-    public Station inputStartStation(Scanner scanner) {
+    public static String inputValue(Scanner scanner) {
+        System.out.println(INPUT_WANTED_FUNCTION_MESSAGE);
+        return scanner.nextLine();
+    }
+
+    public static Station inputStartStation(Scanner scanner) {
         System.out.println(INPUT_START_STATION_MESSAGE);
         return inputStation(scanner);
     }
 
-    public Station inputArrivalStation(Scanner scanner) {
+    public static Station inputArrivalStation(Scanner scanner) {
         System.out.println(INPUT_ARRIVAL_STATION_MESSAGE);
         return inputStation(scanner);
     }
 
-    private Station inputStation(Scanner scanner) {
+    private static Station inputStation(Scanner scanner) {
         try {
             return new Station(scanner.nextLine());
         } catch (IllegalArgumentException e) {
