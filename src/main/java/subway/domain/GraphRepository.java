0663cda8020de71faa7fa5c003ad36500a54d901
LateNightSeoul <60308568+LateNightSeoul@users.noreply.github.com>
2020-12-19 16:18:54 +0900

    refactor : 생성자 활용을 위한 static 요소 제거

--- src/main/java/subway/domain/GraphRepository.java
+++ src/main/java/subway/domain/GraphRepository.java
@@ -7,22 +7,22 @@ import org.jgrapht.graph.WeightedMultigraph;
 import java.util.List;
 
 public class GraphRepository {
-    private static WeightedMultigraph<String, DefaultWeightedEdge> graph
+    private WeightedMultigraph<String, DefaultWeightedEdge> graph
             = new WeightedMultigraph(DefaultWeightedEdge.class);
 
-    public static WeightedMultigraph<String, DefaultWeightedEdge> getGraph() {
+    public WeightedMultigraph<String, DefaultWeightedEdge> getGraph() {
         return graph;
     }
 
-    public static void addVertex(String stationName) {
+    public void addVertex(String stationName) {
         graph.addVertex(stationName);
     }
 
-    public static void setWeight(String v1, String v2, int weight) {
+    public void setWeight(String v1, String v2, int weight) {
         graph.setEdgeWeight(graph.addEdge(v1, v2), weight);
     }
 
-    public static List<String> calPath(String source, String destination) {
+    public List<String> calPath(String source, String destination) {
         DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
         List<String> shortestPath = dijkstraShortestPath.getPath(source, destination).getVertexList();
         return shortestPath;
