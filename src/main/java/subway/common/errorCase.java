113cef85b451faac4df1a24e7eef209758bc7f34
amy choi <cgc8016@ewhain.net>
2020-12-19 16:02:16 +0900

    feat(ErrorCase): add errorCase

--- src/main/java/subway/common/errorCase.java
+++ src/main/java/subway/common/errorCase.java
@@ -1,8 +1,9 @@
 package subway.common;
 
-public class errorCase {
+public class ErrorCase {
 	public static final String FUNCTION_INPUT_ERROR = "선택할 수 없는 기능입니다. ";
 	public static final String INVALID_INPUT_TYPE_ERROR = "잘못된 입력 타입입니다. ";
+	public static final String NO_SUCH_STATION_ERROR = "존재하지 않는 역입니다. ";
 	public static final String SAME_DEPARTURE_ARRIVAL_ERROR = "출발역과 도착역이 동일합니다. ";
 	public static final String UNCONNECTED_DEPARTURE_ARRIVAL_ERROR = "출발역과 도착역이 연결되어 있지 않습니다. ";
 }
