a90fff36ee8fffb4b9590334fb454650319c572f
xrabcde <joara124@ewhain.net>
2020-12-19 13:47:07 +0900

    feat(InputView): 기능 입력에 사용할 InputView 구현

---
+++ src/main/java/subway/view/InputView.java
@@ -0,0 +1,19 @@
+package subway.view;
+
+import subway.domain.Errors;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+    public static String inputFunction(Scanner kbd, List<String> functions) {
+        String input = "0";
+        boolean check = false;
+        while(!check) {
+            System.out.println("\n## 원하는 기능을 선택하세요.");
+            input = kbd.nextLine();
+            check = Errors.checkInput(input, functions);
+        }
+        return input;
+    }
+}
