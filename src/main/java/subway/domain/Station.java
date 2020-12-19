1d1afabf8a55a1709c4fe277c67e2584b3e64c19
yangfriendship <yangfriendship@naver.com>
2020-12-19 13:20:24 +0900

    feat (Station) : 역 이름,  자신이 속한 라인을 갖고 있는 Station 객체

--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -1,13 +1,41 @@
 package subway.domain;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
 public class Station {
+
     private String name;
+    private List<Line> lines = new LinkedList<>();
 
     public Station(String name) {
         this.name = name;
     }
 
     public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Station station = (Station) o;
+        return getName().equals(((Station) o).getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName());
+    }
+
+    @Override
+    public String toString() {
         return name;
     }
 
