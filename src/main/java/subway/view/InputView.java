9060799e0670ae10fba0ba4210cbf68c6cb4e0b8
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-19 16:41:01 +0900

    feat: 메인 화면 기능 선택 입력 추가

---
+++ src/main/java/subway/view/InputView.java
@@ -0,0 +1,16 @@
+package subway.view;
+
+import subway.domain.SceneState;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final String NEW_LINE = "\n";
+    private static final String MAIN_SCENE = "main_scene_";
+    private static final String PATH_SCENE = "path_scene_";
+
+    public static SceneState inputMainSceneChoice(Scanner scanner) {
+        System.out.println(NEW_LINE + "## 원하는 기능을 선택하세요.");
+        return SceneState.findSceneStateByInput(MAIN_SCENE + scanner.nextLine().toLowerCase());
+    }
+}
