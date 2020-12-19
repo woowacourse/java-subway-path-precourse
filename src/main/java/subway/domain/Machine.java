35b772673b81b9469cf7e31fff677f7aaf16f51d
jaeseongDev <qkrwotjd1445@naver.com>
2020-12-19 13:18:12 +0900

    feat(Machine) : 메인 화면 출력 기능 추가

--- src/main/java/subway/domain/Machine.java
+++ src/main/java/subway/domain/Machine.java
@@ -1,9 +1,10 @@
 package subway.domain;
 
 import java.util.Scanner;
+import subway.view.OutputView;
 
 public class Machine {
     public void start(Scanner scanner) {
-        
+        OutputView.printMain();
     }
 }
