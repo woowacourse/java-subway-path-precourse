51320cd228fd68f0c67ac31964b3e5eeb3705603
Sehwan_Jang <whilter08@gmail.com>
2020-12-19 13:48:25 +0900

    feat: Line 안에 stationlist 추가 및 추가 기능 구현

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,15 +1,27 @@
 package subway.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Line {
     private String name;
+    private List<Station> stationInLine = new ArrayList<>();
 
     public Line(String name) {
         this.name = name;
+    }
+
+    public void addStationInLine(Station station) {
+        stationInLine.add(station);
     }
 
     public String getName() {
         return name;
     }
 
-    // 추가 기능 구현
+    public List<Station> stationsInLine() {
+        return Collections.unmodifiableList(stationInLine);
+    }
+
 }
