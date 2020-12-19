15b6c66572db1bef8f47a74329f116fd35de1716
KJunseo <max9106@naver.com>
2020-12-19 15:18:26 +0900

    feat: 경로 기준 탐색시 옵션 선택 기능

--- src/main/java/subway/path/PathController.java
+++ src/main/java/subway/path/PathController.java
@@ -1,7 +1,12 @@
 package subway.path;
 
+import subway.MainOption;
+import subway.common.SelectOption;
 import subway.view.InputView;
 import subway.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PathController {
     private InputView inputView;
@@ -13,7 +18,20 @@ public class PathController {
     public void run() {
         while (true) {
             OutputView.showSearchPathMethod();
+            List<String> optionList = getSearchOptionList();
+            String method = SelectOption.askOptionChoice(inputView, optionList);
             break;
         }
+    }
+
+    private List<String> getSearchOptionList() {
+        SearchMethod[] methods = SearchMethod.values();
+        List<String> optionList = new ArrayList<>();
+
+        for (SearchMethod method : methods) {
+            optionList.add(method.getValue());
+        }
+
+        return optionList;
     }
 }
