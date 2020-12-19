acc42ac7c57194cdce302f07590b4a73ff0588e3
Jinho <jh8579@gmail.com>
2020-12-19 17:20:02 +0900

    fix(validation/path) : add checking station is exist in repository before get shortest path in graph

--- src/main/java/subway/service/validation/PathValidation.java
+++ src/main/java/subway/service/validation/PathValidation.java
@@ -1,9 +1,12 @@
 package subway.service.validation;
 
+import subway.exception.CustomException;
 import subway.exception.path.PathException;
 import subway.exception.path.SameFromToStationException;
 import subway.exception.path.TwoStationIsNotConnectedException;
+import subway.exception.station.StationIsNotExistException;
 import subway.service.PathService;
+import subway.service.StationService;
 
 public class PathValidation {
 
@@ -13,12 +16,20 @@ public class PathValidation {
                 throw new SameFromToStationException();
             }
 
+            if(StationService.isAlreadyExistStation(fromStationName)) {
+                throw new StationIsNotExistException();
+            }
+
+            if(StationService.isAlreadyExistStation(toStationName)) {
+                throw new StationIsNotExistException();
+            }
+
             if (!PathService.isFromToStationConnected(fromStationName, toStationName)) {
                 throw new TwoStationIsNotConnectedException();
             }
 
             return true;
-        } catch (PathException e) {
+        } catch (CustomException e) {
             e.printError();
             return false;
         }
