ef763b0a283c2c6999ac7639541a1f4ac49982c5
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-12-19 15:46:39 +0900

    feat: StationService Repository에서 Station을 찾아 Stations 객체 생성하는 기능 구현
    - Line을 생성할 때 필요한 Stations를 Repository에서 찾아 생성하는
    기능을 구현한다.

---
+++ src/main/java/subway/service/StationService.java
@@ -0,0 +1,20 @@
+package subway.service;
+
+import subway.domain.entity.Section;
+import subway.domain.entity.Station;
+import subway.domain.entity.Stations;
+import subway.domain.repository.StationRepository;
+
+public class StationService {
+
+    public Stations generateStations(String upwardLastStationName, String nextStationName, Section section) {
+        Station upwardLastStation = findStationByName(upwardLastStationName);
+        Station nextStation = findStationByName(nextStationName);
+        return Stations.of(upwardLastStation, nextStation, section);
+    }
+
+    private Station findStationByName(String name) {
+        return StationRepository.findByName(name)
+                .orElseThrow(IllegalArgumentException::new);
+    }
+}
