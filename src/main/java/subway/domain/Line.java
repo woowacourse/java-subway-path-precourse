84577df0effd490ed10c6a9a06969c917ab17424
choijy1705 <cjy1705@naver.com>
2020-12-19 13:25:48 +0900

    feat(LineRepository) : 노선 초기화 기능 구현

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,6 +1,11 @@
 package subway.domain;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Line {
+    private final List<Station> stations = new LinkedList<>();
+
     private String name;
 
     public Line(String name) {
@@ -11,5 +16,9 @@ public class Line {
         return name;
     }
 
+
     // 추가 기능 구현
+    public void addStation(Station station) {
+        stations.add(station);
+    }
 }
--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -6,7 +6,23 @@ import java.util.List;
 import java.util.Objects;
 
 public class LineRepository {
+    private static final String[] LINES = {"2호선", "3호선", "신분당선"};
+    private static final String[][] LINE_STATIONS = {{"교대역","강남역","역삼역"},{"교대역","남부터미널역","양재역"},
+            {"강남역","양재역","양재시민의숲역"}};
+    private static int INDEX = 0;
+
     private static final List<Line> lines = new ArrayList<>();
+
+    static{
+        for(String lineName : LINES){
+            Line line = new Line(lineName);
+            for(String stationName : LINE_STATIONS[INDEX]){
+                line.addStation(new Station(stationName));
+            }
+            INDEX++;
+        }
+    }
+
 
     public static List<Line> lines() {
         return Collections.unmodifiableList(lines);
