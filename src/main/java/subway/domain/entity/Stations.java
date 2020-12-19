c5bc5cf79e8400a42f2375126c6eb9c28fd01f24
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-12-19 15:32:46 +0900

    refactor: Stations 생성시 역과 구간 사이 정보를 그래프에 저장
    - 객체 사용의 편의성을 위해 그래프 객체는 싱글톤으로 구현한다.

--- src/main/java/subway/domain/entity/Stations.java
+++ src/main/java/subway/domain/entity/Stations.java
@@ -1,10 +1,13 @@
 package subway.domain.entity;
 
+import subway.domain.path.SubwayMapGraph;
+
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Stations {
+    private static final SubwayMapGraph SUBWAY_MAP_GRAPH = SubwayMapGraph.getInstance();
 
     private final List<Station> stations;
 
@@ -12,10 +15,11 @@ public class Stations {
         this.stations = stations;
     }
 
-    public static Stations of(Station upwardLastStation, Station nextStation) {
+    public static Stations of(Station upwardLastStation, Station nextStation, Section section) {
         if (upwardLastStation.equals(nextStation)) {
             throw new SectionDuplicationException();
         }
+        SUBWAY_MAP_GRAPH.addStationToGraph(upwardLastStation, nextStation, section);
         List<Station> stations = Stream.of(upwardLastStation, nextStation)
                 .collect(Collectors.toList());
         return new Stations(stations);
--- src/main/java/subway/domain/path/SubwayMapGraph.java
+++ src/main/java/subway/domain/path/SubwayMapGraph.java
@@ -5,15 +5,29 @@ import org.jgrapht.graph.WeightedMultigraph;
 import subway.domain.entity.Section;
 import subway.domain.entity.Station;
 
-public class SubwayMapGraph {
+import java.util.Objects;
 
+public class SubwayMapGraph {
+    private static SubwayMapGraph subwayMapGraph;
     private final WeightedMultigraph<Station, DefaultWeightedEdge> shortestDistanceGraph;
     private final WeightedMultigraph<Station, DefaultWeightedEdge> minimumTimeGraph;
 
-    public SubwayMapGraph(WeightedMultigraph<Station, DefaultWeightedEdge> shortestDistanceGraph,
-                          WeightedMultigraph<Station, DefaultWeightedEdge> minimumTimeGraph) {
+    private SubwayMapGraph(WeightedMultigraph<Station, DefaultWeightedEdge> shortestDistanceGraph,
+                           WeightedMultigraph<Station, DefaultWeightedEdge> minimumTimeGraph) {
         this.shortestDistanceGraph = shortestDistanceGraph;
         this.minimumTimeGraph = minimumTimeGraph;
+    }
+
+    public static void initiate() {
+        subwayMapGraph = new SubwayMapGraph(new WeightedMultigraph(DefaultWeightedEdge.class),
+                new WeightedMultigraph(DefaultWeightedEdge.class));
+    }
+
+    public static SubwayMapGraph getInstance() {
+        if (Objects.isNull(subwayMapGraph)) {
+            throw new IllegalArgumentException();
+        }
+        return subwayMapGraph;
     }
 
     public void addStationToGraph(Station firstStation, Station lastStation, Section section) {
