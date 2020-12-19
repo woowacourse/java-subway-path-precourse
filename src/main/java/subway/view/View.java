e9799d0a49a67e9221f47570185e4d95b073f543
xrabcde <joara124@ewhain.net>
2020-12-19 13:46:51 +0900

    feat(View): 메인화면 출력과 프로그램 종료문구 출력해주는 View 클래스 구현

---
+++ src/main/java/subway/view/View.java
@@ -0,0 +1,13 @@
+package subway.view;
+
+public class View {
+    public static void showMainMenu() {
+        System.out.println("\n## 메인 화면");
+        System.out.println("1. 경로 조회");
+        System.out.println("Q. 종료");
+    }
+
+    public static void finishProgram() {
+        System.out.println("## 프로그램 종료");
+    }
+}
