3b907df9af0d31ceb2e9aff511db7cde8ded9d93
Hyeon9mak <jinha3507@gmail.com>
2020-12-19 14:26:54 +0900

    feat(Time): add TimeSectionRepository

--- src/main/java/subway/domain/TimeSectionRepository.java
+++ src/main/java/subway/domain/TimeSectionRepository.java
@@ -1,5 +1,7 @@
 package subway.domain;
 
+import java.util.List;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
 
@@ -7,4 +9,22 @@ public class TimeSectionRepository {
 
     private static final WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(
         DefaultWeightedEdge.class);
+
+    public static void addSection(Station source, Station destination, Time time) {
+        graph.addVertex(source.getName());
+        graph.addVertex(destination.getName());
+        graph.setEdgeWeight(graph
+            .addEdge(source.getName(), destination.getName()), time.getMinute());
+    }
+
+    public static List<String> getShortestPath(Station source, Station destination){
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+        return dijkstraShortestPath.getPath(source.getName(), destination.getName()).getVertexList();
+    }
+
+    public static Time getShortestTime(Station source, Station destination){
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+        return Time.newTime((int)dijkstraShortestPath
+            .getPathWeight(source.getName(), destination.getName()));
+    }
 }
