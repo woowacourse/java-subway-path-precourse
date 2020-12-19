02e90b7e630bedc0d7111fd022e1b70491627144
doorisopen <audauddl2@gmail.com>
2020-12-19 14:45:37 +0900

    feat: 선택 가능한 기능인지 검증한다

--- src/main/java/subway/domain/MenuType.java
+++ src/main/java/subway/domain/MenuType.java
@@ -3,7 +3,7 @@ package subway.domain;
 import java.util.Arrays;
 import java.util.List;
 
-public enum  MenuType {
+public enum MenuType {
     MAIN_MENU_RANGE(Arrays.asList("1","2","Q")),
     MAIN_SEARCH_PATH("1"),
     MAIN_EXIT("Q"),
@@ -21,5 +21,12 @@ public enum  MenuType {
 
     private MenuType(List<String> keys) {
         this.keys = keys;
+    }
+
+    public void validateMenuRange(String menu) {
+        boolean isContains = this.keys.contains(menu);
+        if (!isContains) {
+            throw new IllegalArgumentException("[ERROR] 선택할 수 없는 기능입니다.");
+        }
     }
 }
