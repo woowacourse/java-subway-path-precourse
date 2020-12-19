0f42287f10bdeddfbeafd3fd937069fab9c96869
Injun Son <ison@sfu.ca>
2020-12-19 15:07:14 +0900

    feat: 경로 조회 화면 출력

--- src/main/java/subway/controller/PathManager.java
+++ src/main/java/subway/controller/PathManager.java
@@ -1,8 +1,35 @@
 package subway.controller;
 
+import java.util.List;
+import org.jgrapht.traverse.
+import subway.domain.DefaultLines;
+import subway.domain.DefaultStations;
+import subway.domain.Line;
+import subway.domain.Station;
+import subway.view.InputView;
+import subway.view.OutputView;
+
 public class PathManager {
 
+    private static final String GO_BACK_CODE = "B";
+    private final List<Line> defaultLines;
+    private final List<Station> defaultStations;
+    private String userOption;
+
+    public PathManager() {
+        defaultLines = DefaultLines.getDefaultLines();
+        defaultStations = DefaultStations.getDefaultStations();
+        userOption = "";
+    }
+
     public void run() {
-        
+        while (!userWantGoBack()) {
+            OutputView.printPathManagerScreen();
+//            userOption = InputView.inputPathUserOption();
+        }
+    }
+
+    private boolean userWantGoBack() {
+        return userOption.equals(GO_BACK_CODE);
     }
 }
--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -5,10 +5,21 @@ public class OutputView {
     private static final String MAIN_TITLE = "## 메인 화면";
     private static final String MAIN_OPTION_ONE = "1. 경로 조회";
     private static final String MAIN_OPTION_TWO = "Q. 종료";
+    private static final String PATH_TITLE = "## 경로 기준";
+    private static final String PATH_OPTION_ONE = "1. 최단 거리";
+    private static final String PATH_OPTION_TWO = "2. 최소 시간";
+    private static final String PATH_OPTION_BACK = "B. 돌아가기";
 
     public static void printMainScreen() {
         System.out.println(MAIN_TITLE);
         System.out.println(MAIN_OPTION_ONE);
         System.out.println(MAIN_OPTION_TWO);
+    }
+
+    public static void printPathManagerScreen() {
+        System.out.println(PATH_TITLE);
+        System.out.println(PATH_OPTION_ONE);
+        System.out.println(PATH_OPTION_TWO);
+        System.out.println(PATH_OPTION_BACK);
     }
 }
