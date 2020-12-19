b38df9181bddfe78bc6c91143b601968f5cea6e5
mungto <daum7766@naver.com>
2020-12-19 15:37:02 +0900

    refactor(Application): 데이터 초기셋팅 해주는 함수 호출

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -6,6 +6,7 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         Input.init(scanner);
+        DataInitService.init();
         UserInterfaceView userInterfaceView = new UserInterfaceView();
         userInterfaceView.start();
     }
