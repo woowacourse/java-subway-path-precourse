a96cf51212f8428f131045e7b88a347c849b6ae8
LateNightSeoul <60308568+LateNightSeoul@users.noreply.github.com>
2020-12-19 14:30:09 +0900

    feat : 구간 정보 객체 구현

---
+++ src/main/java/subway/domain/Section.java
@@ -0,0 +1,19 @@
+package subway.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Section {
+    private Line line;
+    private List<Station> station = new ArrayList<>();
+
+    public Section(Line line) {
+        this.line = line;
+    }
+
+    public void addStation(Station station) { this.station.add(station); }
+
+    public Line getLine() { return line; }
+
+    public List<Station> getStation() { return this.station; }
+}
---
+++ src/main/java/subway/domain/SectionRepository.java
@@ -0,0 +1,18 @@
+package subway.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class SectionRepository {
+    private static List<Section> sections = new ArrayList<>();
+
+    public static List<Section> sections() {
+        return sections;
+    }
+
+    public static void addSection(Section section) {
+        sections.add(section);
+    }
+
+}
