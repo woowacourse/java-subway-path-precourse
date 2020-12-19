6c8324973faa1be8ea7a4a4c135b0d603f24eb70
Minjeong <mathmjseo@naver.com>
2020-12-19 13:31:04 +0900

    feat: 사용자에게 이용할 기능을 입력받는다.

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -1,4 +1,12 @@
 package subway.view;
 
+import java.util.Scanner;
+
 public class InputView {
+
+    public static String inputFunctionNumber(Scanner scanner) {
+        scanner = new Scanner(System.in);
+        System.out.println("## 원하는 기능을 선택하세요.");
+        return scanner.next();
+    }
 }
