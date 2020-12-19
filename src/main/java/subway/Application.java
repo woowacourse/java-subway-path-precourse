52427195f28411859e009b4f65a01ae1097237d2
pika96 <52442949+pika96@users.noreply.github.com>
2020-12-19 17:53:54 +0900

    feat: SubwayMangement 객체 생성, 메인 로직 추가

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -6,5 +6,8 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
+        User user = new User(scanner);
+        SubwayManagement subwayManagement = new SubwayManagement(user);
+        subwayManagement.start();
     }
 }
