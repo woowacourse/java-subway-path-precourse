e609228c8129347ac719027fc00aedf8f38d52eb
Minsung <sns2831@gmail.com>
2020-12-19 13:36:46 +0900

    feat(subway): 역 생성 및 검증 로직 구현
    Station 생성시 검증 로직을 포함하여 검증한다.

---
+++ src/main/java/subway/domain/station/Station.java
@@ -0,0 +1,37 @@
+package subway.domain.station;
+
+import subway.exception.StationNameFormatException;
+
+public class Station {
+    private static final String STATION = "역";
+    private static final int STATION_NAME_LENGTH = 2;
+
+    private String name;
+
+    public Station(String name) {
+        this.name = name;
+    }
+
+    private void validate(String name) {
+        validateNameFormat(name);
+        validateNameLength(name);
+    }
+
+    private void validateNameFormat(String name) {
+        if (!name.endsWith(STATION)) {
+            throw new StationNameFormatException();
+        }
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length()-STATION.length() < STATION_NAME_LENGTH) {
+            throw new StationNameFormatException();
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // 추가 기능 구현
+}
