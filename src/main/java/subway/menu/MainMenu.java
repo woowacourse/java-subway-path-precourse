cb8cd8dde4687b97809eca460fdd972c8fe3c75a
bepoz <kangsy763@naver.com>
2020-12-19 15:37:05 +0900

    feat: 선택지로 서비스를 찾을 수 있다

--- src/main/java/subway/menu/MainMenu.java
+++ src/main/java/subway/menu/MainMenu.java
@@ -1,5 +1,7 @@
 package subway.menu;
 
+import java.util.Arrays;
+
 public enum MainMenu {
     PATH_SERVICE("1","1. 경로 조회"),
     EXIT("Q", "Q. 종료");
@@ -10,6 +12,12 @@ public enum MainMenu {
     MainMenu(String option, String message) {
         this.option = option;
         this.message = message;
+    }
+
+    public static MainMenu findMainMenyByOption(String option) {
+        return Arrays.stream(values())
+                .filter(value -> value.getOption().equals(option))
+                .findFirst().orElseThrow(IllegalArgumentException::new);
     }
 
     public String getOption() {
