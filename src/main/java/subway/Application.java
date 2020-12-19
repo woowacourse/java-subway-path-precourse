2549e604de62684d0ac2b5f3a31d75fe093f7e56
tributetothemoon <kkm97351@gmail.com>
2020-12-19 14:59:40 +0900

    feat(InputView): 입력과 관련된 로직을 담당할 InputView 생성
    - 입력에 관한 View 로직을 담당할 InputView 클래스를 생성했습니다.
    - Application에서 제공된 Scanner 객체를 세팅할 setScanner(Scanner)
      메소드가 구현되었으며, 프로그램 실행 즉시 해당 스캐너를 사용하도록
      설정합니다.

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,13 +1,16 @@
 package subway;
 
 import subway.domain.DataInitializer;
+import subway.view.InputView;
 
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO: 프로그램 구현
+        InputView.setScanner(scanner);
+
         DataInitializer.initialize();
+
     }
 }
