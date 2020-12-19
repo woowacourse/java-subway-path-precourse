e8685aada7d07b8f1afe1cf0e4579be0b58f0286
박우림 <woorimprog@gmail.com>
2020-12-19 14:03:26 +0900

    feat(message-util) : add initial message

--- src/main/java/subway/util/Message.java
+++ src/main/java/subway/util/Message.java
@@ -1,5 +1,30 @@
 package subway.util;
 
 public class Message {
-
+	/*
+	 * title
+	 */
+	public static final String MAIN_VIEW = "메인 화면";
+	public static final String PATH_CRITERION_VIEW = "경로 기준";
+	
+	/*
+	 * sub-title
+	 */
+	public static final String FUNCTION_CHOICE = "원하는 기능을 선택하세요.";
+	public static final String DEPARTURE_STATION_INPUT = "출발역을 입력하세요.";
+	public static final String ARRIVAL_STATION_INPUT = "도착역을 입력하세요.";
+	public static final String READ_RESULT = "조회 결과";
+	
+	/*
+	 * view
+	 */
+	// main
+	public static final String PATH_RESULT = "1. 경로 조회";
+	public static final String QUITE = "Q. 종료";
+	
+	// path criterion
+	public static final String MIN_DISTANCE = "최단 거리";
+	public static final String MIM_TIME = "최소 시간";
+	
+	public static final String BACK = "B. 돌아가기";
 }
