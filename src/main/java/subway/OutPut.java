5c8c17fa181293bc2478fb6ff6bef385645116dc
mungto <daum7766@naver.com>
2020-12-19 13:39:18 +0900

    feat(OutPut): 메인출력함수 생성

---
+++ src/main/java/subway/OutPut.java
@@ -0,0 +1,15 @@
+package subway;
+
+public class OutPut {
+    private static final String MAIN_VIEW = "## 메인 화면";
+    private static final String ROUTE_SEARCH = "1. 경로 조회";
+    private static final String QUIT = "Q. 종료";
+    private static final String[] MAIN_LIST = {MAIN_VIEW, ROUTE_SEARCH, QUIT};
+
+    public static void printView() {
+        for (int i = 0; i < MAIN_LIST.length; i++) {
+            System.out.println(MAIN_LIST[i]);
+        }
+        System.out.println();
+    }
+}
