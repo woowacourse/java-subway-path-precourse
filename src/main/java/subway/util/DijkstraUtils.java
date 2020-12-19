90c1f5e0fc16269db43f8802b9184ab34cd5775a
Seokchae Park <seokcpark@smilegate.com>
2020-12-19 17:18:26 +0900

    fix(DijkstraUtils): 캐스팅 잘못된 부분 수정 및 리팩토링

--- src/main/java/subway/util/DijkstraUtils.java
+++ src/main/java/subway/util/DijkstraUtils.java
@@ -1,5 +1,6 @@
 package subway.util;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
@@ -12,42 +13,57 @@ import subway.domain.Station;
 import subway.domain.StationRepository;
 
 public class DijkstraUtils {
+	private static final int FIRST = 0;
+	private static final int LAST = 0;
 
 	public static WeightedMultigraph<String, DefaultWeightedEdge> getGraphByDistance() {
-		WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+		WeightedMultigraph<String, DefaultWeightedEdge> graph = 
+				new WeightedMultigraph(DefaultWeightedEdge.class);
 		for (Station station : StationRepository.stations()) {
 			graph.addVertex(station.getName());
 		}
 		for (Path path : PathRepository.paths()) {
-			String[] stations = (String[]) path.getStationNames().toArray();
+			List<String> stations = new ArrayList<>(path.getStationNames());
 			double weight = path.getDistance();
-			graph.setEdgeWeight(graph.addEdge(stations[0], stations[1]), weight);
+			graph.setEdgeWeight(
+					graph.addEdge(stations.get(FIRST), stations.get(LAST)), 
+					weight);
 		}
 		return graph;
 	}
 
 	public static WeightedMultigraph<String, DefaultWeightedEdge> getGraphByMinute() {
-		WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+		WeightedMultigraph<String, DefaultWeightedEdge> graph = 
+				new WeightedMultigraph(DefaultWeightedEdge.class);
 		for (Station station : StationRepository.stations()) {
 			graph.addVertex(station.getName());
 		}
 		for (Path path : PathRepository.paths()) {
-			String[] stations = (String[]) path.getStationNames().toArray();
+			List<String> stations = new ArrayList<>(path.getStationNames());
 			double weight = path.getMinute();
-			graph.setEdgeWeight(graph.addEdge(stations[0], stations[1]), weight);
+			graph.setEdgeWeight(
+					graph.addEdge(stations.get(FIRST), stations.get(LAST)), 
+					weight);
 		}
 		return graph;
 	}
 
 	public static List<String> getPathByMinimumDistance(String startStationName, String endStationName) {
-		DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(getGraphByDistance());
-		List<String> shortestPath = dijkstraShortestPath.getPath(startStationName, endStationName).getVertexList();
+		DijkstraShortestPath dijkstraShortestPath = 
+				new DijkstraShortestPath(getGraphByDistance());
+		List<String> shortestPath = 
+				dijkstraShortestPath
+				.getPath(startStationName, endStationName)
+				.getVertexList();
 		return shortestPath;
 	}
 
 	public static List<String> getPathByMinimumMinute(String startStationName, String endStationName) {
-		DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(getGraphByMinute());
-		List<String> shortestPath = dijkstraShortestPath.getPath(startStationName, endStationName).getVertexList();
+		DijkstraShortestPath dijkstraShortestPath = 
+				new DijkstraShortestPath(getGraphByMinute());
+		List<String> shortestPath = dijkstraShortestPath
+				.getPath(startStationName, endStationName)
+				.getVertexList();
 		return shortestPath;
 	}
 }
