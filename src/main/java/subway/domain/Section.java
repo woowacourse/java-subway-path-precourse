206b3e5970d731fa2fbfd7bdb853f83832088817
TaewanKimmmm <ktww88@gmail.com>
2020-12-19 13:53:14 +0900

    feat: 구간 정보를 위한 Section 클래스 추가

---
+++ src/main/java/subway/domain/Section.java
@@ -0,0 +1,15 @@
+package subway.domain;
+
+public class Section {
+    private final String departureStation;
+    private final String arrivalStation;
+    private final int distance;
+    private final int time;
+
+    Section(String departureStation, String arrivalStation, int distance, int time) {
+        this.departureStation = departureStation;
+        this.arrivalStation = arrivalStation;
+        this.distance = distance;
+        this.time = time;
+    }
+}
