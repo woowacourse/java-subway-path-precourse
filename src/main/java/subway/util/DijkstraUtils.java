61397a67427943770134e4172767c081ebee7a3e
Seokchae Park <seokcpark@smilegate.com>
2020-12-19 16:20:44 +0900

    feat(DijkstraUtils): 최소거리와 최소시간을 기준으로 경로를 반환해주는 utils 클래스 추가

---
+++ src/main/java/subway/util/DijkstraUtils.java
@@ -0,0 +1,55 @@
+package subway.util;
+
+import java.util.List;
+
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+
+import subway.domain.Line;
+import subway.domain.LineRepository;
+import subway.domain.Path;
+import subway.domain.PathRepository;
+import subway.domain.Station;
+import subway.domain.StationRepository;
+
+public class DijkstraUtils {
+
+	public WeightedMultigraph<String, DefaultWeightedEdge> getGraphByDistance() {
+		WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+		for (Station station : StationRepository.stations()) {
+			graph.addVertex(station.getName());
+		}
+		for (Path path : PathRepository.paths()) {
+			String[] stations = (String[]) path.getStationNames().toArray();
+			double weight = path.getDistance();
+			graph.setEdgeWeight(graph.addEdge(stations[0], stations[1]), weight);
+		}
+		return graph;
+	}
+
+	public WeightedMultigraph<String, DefaultWeightedEdge> getGraphByMinute() {
+		WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+		for (Station station : StationRepository.stations()) {
+			graph.addVertex(station.getName());
+		}
+		for (Path path : PathRepository.paths()) {
+			String[] stations = (String[]) path.getStationNames().toArray();
+			double weight = path.getMinute();
+			graph.setEdgeWeight(graph.addEdge(stations[0], stations[1]), weight);
+		}
+		return graph;
+	}
+
+	public List<String> getPathByMinimumDistance(String startStationName, String endStationName) {
+		DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(getGraphByDistance());
+		List<String> shortestPath = dijkstraShortestPath.getPath(startStationName, endStationName).getVertexList();
+		return shortestPath;
+	}
+
+	public List<String> getPathByMinimumMinute(String startStationName, String endStationName) {
+		DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(getGraphByMinute());
+		List<String> shortestPath = dijkstraShortestPath.getPath(startStationName, endStationName).getVertexList();
+		return shortestPath;
+	}
+}
