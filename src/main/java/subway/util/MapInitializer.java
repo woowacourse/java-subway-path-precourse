e94cb6abe2dbff338813e366511edbc654379e6c
LateNightSeoul <60308568+LateNightSeoul@users.noreply.github.com>
2020-12-19 15:39:30 +0900

    feat : 노선 초기화 함수 수정 및 추가 구현

--- src/main/java/subway/util/MapInitializer.java
+++ src/main/java/subway/util/MapInitializer.java
@@ -3,6 +3,14 @@ package subway.util;
 import subway.domain.*;
 
 public class MapInitializer {
+
+    public MapInitializer() {
+        initStations();
+        initLines();
+        initSections();
+        initEdge();
+    }
+
     public void initStations() {
         StationRepository.addStation(new Station("교대역"));
         StationRepository.addStation(new Station("강남역"));
@@ -23,6 +31,7 @@ public class MapInitializer {
     public void initSections() {
         for(Line line : LineRepository.lines()) {
             Section section = new Section(line);
+            SectionRepository.addSection(section);
         }
         updateSection("2호선", "교대역");
         updateSection("2호선", "강남역");
@@ -37,14 +46,29 @@ public class MapInitializer {
     }
 
     public void updateSection(String lineName, String stationName) {
-        Station station = null;
-        Section section = null;
+        for(Section sc : SectionRepository.sections()) {
+            addStation(sc, stationName, lineName);
+        }
+    }
 
-        station = (Station) StationRepository.stations().stream()
-                .filter(st -> st.getName().equals(stationName));
+    public void addStation(Section sc, String stationName, String lineName) {
+        if(sc.getLine().getName().equals(lineName)) {
+            sc.addStation(new Station(stationName));
+        }
+    }
 
-        section = (Section) SectionRepository.sections().stream()
-                .filter(section1 -> section1.getLine().getName().equals(lineName));
-        section.addStation(station);
+    public void initEdge() {
+        updateEdge("교대역", "강남역",2,3);
+        updateEdge("강남역", "역삼역",2,3);
+        updateEdge("교대역", "남부터미널역",3,2);
+        updateEdge("남부터미널역", "양재역",6,5);
+        updateEdge("양재역", "매봉역",1,1);
+        updateEdge("강남역", "양재역",2,8);
+        updateEdge("양재역", "양재시민의숲역",10,3);
+    }
+
+    public void updateEdge(String station1, String station2, int distance, int time) {
+        Edge edge = new Edge(station1, station2, distance, time);
+        EdgeRepository.addEdge(edge);
     }
 }
