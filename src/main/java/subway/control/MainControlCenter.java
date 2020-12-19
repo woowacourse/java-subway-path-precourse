9795ca6741e4c3a13cf59dd2a989599978c075f3
Shion <ehdrhelr@gmail.com>
2020-12-19 13:57:24 +0900

    feat : add func inputting command

--- src/main/java/subway/control/MainControlCenter.java
+++ src/main/java/subway/control/MainControlCenter.java
@@ -6,12 +6,23 @@ import java.util.Scanner;
 
 public class MainControlCenter {
 
+    public static String inputCommand(Scanner scanner) {
+        String command = scanner.nextLine();
+        System.out.println();
+        return command.trim();
+    }
+
     public void startMainControl(Scanner scanner) {
-        showMainMenu();
+        String view = "";
+        while (!view.equalsIgnoreCase("Q")) {
+            showMainMenu();
+            String command = inputCommand(scanner);
+        }
     }
 
     private void showMainMenu() {
         MainView.printMainMenu();
         MainView.askInputMenu();
     }
+
 }
