1c5e0f473d7615ca5dcf87537ea2903e17d7def2
yangfriendship <yangfriendship@naver.com>
2020-12-19 13:21:04 +0900

    feat (Line) : 노선, 자신에게 속한 Station과 이름을 갖고 있다.

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,15 +1,45 @@
 package subway.domain;
 
-public class Line {
-    private String name;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
 
-    public Line(String name) {
+public class Line implements Comparable<Line> {
+
+    private String name;
+    private List<Station> stations = new LinkedList<>();
+
+    private Line(String name) {
         this.name = name;
+    }
+
+    public static Line of(String name) {
+        return new Line(name);
     }
 
     public String getName() {
         return name;
     }
 
-    // 추가 기능 구현
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Line line = (Line) o;
+        return Objects.equals(getName(), line.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName());
+    }
+
+    @Override
+    public int compareTo(Line o) {
+        return name.compareTo(o.name);
+    }
 }
