ce1f7354203c3d0130a01b65e45eaeac2b7a2f5a
perenok <perenoke@gmail.com>
2020-12-19 17:53:32 +0900

    feat(LineRepository): LineRepository
    WeightedMultigraph를 이용해 거리와 시간에 따른 각각의 그래프를 만듬
    이를 이용해 최소 거리와 최소 시간 및 경로를 얻을 수 있음

--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -1,5 +1,9 @@
 package subway.domain;
 
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -7,6 +11,11 @@ import java.util.Objects;
 
 public class LineRepository {
     private static final List<Line> lines = new ArrayList<>();
+    private static final WeightedMultigraph<String, DefaultWeightedEdge> stationsByDistance =
+            new WeightedMultigraph<>(DefaultWeightedEdge.class);
+    private static final WeightedMultigraph<String, DefaultWeightedEdge> stationsByTime =
+            new WeightedMultigraph<>(DefaultWeightedEdge.class);
+
 
     public static List<Line> lines() {
         return Collections.unmodifiableList(lines);
@@ -23,4 +32,38 @@ public class LineRepository {
     public static void deleteAll() {
         lines.clear();
     }
+
+    public static void addVertex(String name) {
+        stationsByDistance.addVertex(name);
+        stationsByTime.addVertex(name);
+    }
+
+    public static void addDistance(String source, String target, int distance) {
+        stationsByDistance.setEdgeWeight(stationsByDistance.addEdge(source, target), distance);
+    }
+
+    public static void addTime(String source, String target, int time) {
+        stationsByTime.setEdgeWeight(stationsByTime.addEdge(source, target), time);
+    }
+
+    public static int getShortDistance(String departure, String arrival) {
+        DijkstraShortestPath<String, DefaultWeightedEdge> dijkstraShortestPath = new DijkstraShortestPath<>(stationsByDistance);
+        return (int) Math.round(dijkstraShortestPath.getPathWeight(departure, arrival));
+    }
+
+    public static List<String> getRouteByDistance(String departure, String arrival) {
+        DijkstraShortestPath<String, DefaultWeightedEdge> dijkstraShortestPath = new DijkstraShortestPath<>(stationsByDistance);
+        return dijkstraShortestPath.getPath(departure, arrival).getVertexList();
+    }
+
+    public static int getShortTime(String departure, String arrival) {
+        DijkstraShortestPath<String, DefaultWeightedEdge> dijkstraShortestPath = new DijkstraShortestPath<>(stationsByTime);
+        return (int) Math.round(dijkstraShortestPath.getPathWeight(departure, arrival));
+    }
+
+    public static List<String> getRouteByTime(String departure, String arrival) {
+        DijkstraShortestPath<String, DefaultWeightedEdge> dijkstraShortestPath = new DijkstraShortestPath<>(stationsByTime);
+        return dijkstraShortestPath.getPath(departure, arrival).getVertexList();
+    }
 }
+
