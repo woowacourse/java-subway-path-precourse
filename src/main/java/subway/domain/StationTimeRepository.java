642cc935444bdca87afb815b3af284b81ec5ebc8
hybeom0720 <hybeom@gmail.com>
2020-12-19 14:40:00 +0900

    feat(StationTimeRepository) : 최단 시간 및 경로까지 구해주는 메서드 추가
    추가 :
    - 최소 소요 시간을 구해주는 메서드 추가
    - 최소 소요 시간이 걸릴 때의 경로를 찾아주는 메서드 추가

--- src/main/java/subway/domain/StationTimeRepository.java
+++ src/main/java/subway/domain/StationTimeRepository.java
@@ -1,27 +1,47 @@
 package subway.domain;
 
+import java.util.List;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
 import subway.domain.constants.DomainErrorMessage;
 
 public class StationTimeRepository {
+
     private static WeightedMultigraph<String, DefaultWeightedEdge> timeGraph =
             new WeightedMultigraph(DefaultWeightedEdge.class);
+    private static DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(timeGraph);
 
-    public void addStationTitle(String stationTitle) {
+    public static void addStationTitle(String stationTitle) {
         timeGraph.addVertex(stationTitle);
     }
 
-    public void addStationIntervalTime(String initialStation, String lastStation, int distance) {
+    public static void addStationIntervalTime(String initialStation,
+            String lastStation, int distance) {
         isSameInitialLastStation(initialStation, lastStation);
         DefaultWeightedEdge stationEdge = timeGraph.addEdge(initialStation, lastStation);
         timeGraph.setEdgeWeight(stationEdge, distance);
     }
 
-    private void isSameInitialLastStation(String initialStation, String lasStation) {
+    public static List<String> getShortestPath(String initialStation, String lastStation) {
+        isSameInitialLastStation(initialStation, lastStation);
+        List<String> shortestPath = dijkstraShortestPath.getPath(initialStation, lastStation)
+                .getVertexList();
+        return shortestPath;
+    }
+
+    public static int getShortestTime(String initialStation, String lastStation) {
+        isSameInitialLastStation(initialStation, lastStation);
+        int shortestTime = (int) dijkstraShortestPath.getPath(initialStation, lastStation)
+                .getWeight();
+        return shortestTime;
+    }
+
+    private static void isSameInitialLastStation(String initialStation, String lasStation) {
         if (initialStation.equals(lasStation)) {
             System.out.println(DomainErrorMessage.SAME_STATION);
             throw new IllegalArgumentException(DomainErrorMessage.SAME_STATION);
         }
     }
+
 }
