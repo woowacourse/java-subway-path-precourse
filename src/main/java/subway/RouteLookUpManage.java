6119638728e4e54464531b1b2ac4f1de33902adb
amy choi <cgc8016@ewhain.net>
2020-12-19 17:34:24 +0900

    style(RouteLookUpManage): duplicate code seperate

--- src/main/java/subway/RouteLookUpManage.java
+++ src/main/java/subway/RouteLookUpManage.java
@@ -41,16 +41,16 @@ public class RouteLookUpManage {
 			return true;
 		}
 		if (input.equals(SHORTEST_DISTANCE)) {
-			return findPathByDistanceControl(scanner);
+			return inputControl(scanner, SHORTEST_DISTANCE);
 		}
 		if (input.equals(MINIMUM_TIME)) {
-			return findPathByTimeControl(scanner);
+			return inputControl(scanner, MINIMUM_TIME);
 		}
 		errorPrint(FUNCTION_INPUT_ERROR);
 		return false;
 	}
 
-	private static boolean findPathByDistanceControl(Scanner scanner) {
+	private static boolean inputControl(Scanner scanner, String findType) {
 		String departureStation = inputDepartureStation(scanner);
 		if (!verifyExistStation(departureStation)) {
 			return false;
@@ -59,28 +59,47 @@ public class RouteLookUpManage {
 		if (!verifyExistStation(arrivalStation)) {
 			return false;
 		}
-		if (verifyDifferentStation(departureStation, arrivalStation)) {
+		if (!verifyDifferentStation(departureStation, arrivalStation)) {
+			return false;
+		}
+		if (findType.equals(SHORTEST_DISTANCE)) {
+			return findPathByDistanceControl(departureStation, arrivalStation);
+		}
+		if (findType.equals(MINIMUM_TIME)) {
+			return findPathByTimeControl(departureStation, arrivalStation);
+		}
+		errorPrint(FUNCTION_INPUT_ERROR);
+		return false;
+	}
+
+	private static boolean findPathByDistanceControl(String departureStation,
+		String arrivalStation) {
+		GraphPath<String, DefaultWeightedEdge> path = findPathByDistance(departureStation,
+			arrivalStation);
+		if (!verifyPathConnected(path)) {
 			return false;
 		}
 
-		findPathByDistance(departureStation, arrivalStation);
+		List<String> pathStations = path.getVertexList();
+		int totalDistance = (int) path.getWeight();
+		int totalTime = getTimeByVertexList(pathStations);
+		printLookUpResult(totalDistance, totalTime, pathStations);
+
 		return true;
 	}
 
-	private static boolean findPathByTimeControl(Scanner scanner) {
-		String departureStation = inputDepartureStation(scanner);
-		if (!verifyExistStation(departureStation)) {
-			return false;
-		}
-		String arrivalStation = inputArrivalStation(scanner);
-		if (!verifyExistStation(arrivalStation)) {
-			return false;
-		}
-		if (verifyDifferentStation(departureStation, arrivalStation)) {
+	private static boolean findPathByTimeControl(String departureStation,
+		String arrivalStation) {
+		GraphPath<String, DefaultWeightedEdge> path = findPathByTime(departureStation,
+			arrivalStation);
+		if (!verifyPathConnected(path)) {
 			return false;
 		}
 
-		findPathByTime(departureStation, arrivalStation);
+		List<String> pathStations = path.getVertexList();
+		int totalTime = (int) path.getWeight();
+		int totalDistance = getDistanceByVertexList(pathStations);
+		printLookUpResult(totalDistance, totalTime, pathStations);
 		return true;
 	}
 
@@ -96,38 +115,30 @@ public class RouteLookUpManage {
 		String arrivalStation) {
 		if (departureStation.equals(arrivalStation)) {
 			errorPrint(SAME_DEPARTURE_ARRIVAL_ERROR);
-			return true;
+			return false;
 		}
-		return false;
+		return true;
 	}
 
-	private static void findPathByDistance(String departureStation, String arrivalStation) {
+	private static boolean verifyPathConnected(GraphPath<String, DefaultWeightedEdge> path) {
+		if (path == null) {
+			errorPrint(UNCONNECTED_DEPARTURE_ARRIVAL_ERROR);
+			return false;
+		}
+		return true;
+	}
+
+	private static GraphPath<String, DefaultWeightedEdge> findPathByDistance(
+		String departureStation, String arrivalStation) {
 		DijkstraShortestPath<String, DefaultWeightedEdge> shortestDistancePath = new DijkstraShortestPath<>(
 			DISTANCE_GRAPH);
-		GraphPath<String, DefaultWeightedEdge> path = shortestDistancePath
-			.getPath(departureStation, arrivalStation);
-		if (path == null) {
-			errorPrint(UNCONNECTED_DEPARTURE_ARRIVAL_ERROR);
-		}
-		List<String> pathStations = path.getVertexList();
-		int totalDistance = (int) path.getWeight();
-		int totalTime = getTimeByVertexList(pathStations);
-
-		printLookUpResult(totalDistance, totalTime, pathStations);
+		return shortestDistancePath.getPath(departureStation, arrivalStation);
 	}
 
-	private static void findPathByTime(String departureStation, String arrivalStation) {
+	private static GraphPath<String, DefaultWeightedEdge> findPathByTime(String departureStation,
+		String arrivalStation) {
 		DijkstraShortestPath<String, DefaultWeightedEdge> shortestDistancePath = new DijkstraShortestPath<>(
 			TIME_GRAPH);
-		GraphPath<String, DefaultWeightedEdge> path = shortestDistancePath
-			.getPath(departureStation, arrivalStation);
-		if (path == null) {
-			errorPrint(UNCONNECTED_DEPARTURE_ARRIVAL_ERROR);
-		}
-		List<String> pathStations = path.getVertexList();
-		int totalTime = (int) path.getWeight();
-		int totalDistance = getDistanceByVertexList(pathStations);
-
-		printLookUpResult(totalDistance, totalTime, pathStations);
+		return shortestDistancePath.getPath(departureStation, arrivalStation);
 	}
 }
