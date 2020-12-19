22b2d8e8a59cbe10333f93ec36b073809444993e
jaeseongDev <qkrwotjd1445@naver.com>
2020-12-19 13:17:21 +0900

    feat(Machine) : 지하철 노선도 경로 조회하는 프로그램에 대한 Machine 클래스 생성

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,12 @@
 package subway;
 
 import java.util.Scanner;
+import subway.domain.Machine;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO: 프로그램 구현
+        Machine machine = new Machine();
+        machine.start(scanner);
     }
 }
---
+++ src/main/java/subway/domain/Machine.java
@@ -0,0 +1,9 @@
+package subway.domain;
+
+import java.util.Scanner;
+
+public class Machine {
+    public void start(Scanner scanner) {
+        
+    }
+}
