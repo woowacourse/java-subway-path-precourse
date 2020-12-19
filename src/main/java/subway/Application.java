8053d57aa70d7bc77635bc533cb7ae86abff9b31
sjpark-dev <sjpark.dev@gmail.com>
2020-12-19 14:20:34 +0900

    feat(subway): add initialize repositories feature

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,60 @@
 package subway;
 
+import subway.domain.Line;
+import subway.domain.LineRepository;
+import subway.domain.Station;
+import subway.domain.StationRepository;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
+        initializeRepositories();
+    }
+
+    private static void initializeRepositories() {
+        initializeStationRepository();
+        initializeLineRepository();
+    }
+
+    private static void initializeLineRepository() {
+        Line line2 = new Line("2호선");
+        initializeLine2EdgesAndWeights(line2);
+        Line line3 = new Line("3호선");
+        initializeLine3EdgesAndWeights(line3);
+        Line sinbundangLine = new Line("신분당선");
+        initializeSinbundangLineEdgesAndWeights(sinbundangLine);
+    }
+
+    private static void initializeSinbundangLineEdgesAndWeights(Line sinbundangLine) {
+        List<String[]> edgesAndWeights = new ArrayList<>();
+        edgesAndWeights.add(new String[]{"강남역", "양재역", "2km", "8분"});
+        edgesAndWeights.add(new String[]{"양재역", "양재시민의숲역", "10km", "3분"});
+        sinbundangLine.addEdgesAndWeights(edgesAndWeights);
+    }
+
+    private static void initializeLine3EdgesAndWeights(Line line3) {
+        List<String[]> edgesAndWeights = new ArrayList<>();
+        edgesAndWeights.add(new String[]{"교대역", "남부터미널역", "3km", "2분"});
+        edgesAndWeights.add(new String[]{"남부터미널역", "양재역", "6km", "5분"});
+        edgesAndWeights.add(new String[]{"양재역", "매봉역", "1km", "1분"});
+        line3.addEdgesAndWeights(edgesAndWeights);
+    }
+
+    private static void initializeLine2EdgesAndWeights(Line line2) {
+        List<String[]> edgesAndWeights = new ArrayList<>();
+        edgesAndWeights.add(new String[]{"교대역", "강남역", "2km", "3분"});
+        edgesAndWeights.add(new String[]{"강남역", "역삼역", "2km", "3분"});
+        line2.addEdgesAndWeights(edgesAndWeights);
+    }
+
+    private static void initializeStationRepository() {
+        List<String> stationNames = Arrays.asList("교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역");
+        stationNames.forEach(stationName -> StationRepository.addStation(new Station(stationName)));
     }
 }
--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,7 +1,10 @@
 package subway.domain;
 
+import java.util.List;
+
 public class Line {
     private String name;
+    private List<String[]> edgesAndWeights;
 
     public Line(String name) {
         this.name = name;
@@ -9,6 +12,10 @@ public class Line {
 
     public String getName() {
         return name;
+    }
+
+    public void addEdgesAndWeights(List<String[]> edgesAndWeights) {
+        this.edgesAndWeights = edgesAndWeights;
     }
 
     // 추가 기능 구현
