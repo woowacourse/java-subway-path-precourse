e1b8d0baabb7a76b3ddaffe0e3f69b2e5dbcf2e5
LateNightSeoul <60308568+LateNightSeoul@users.noreply.github.com>
2020-12-19 16:56:44 +0900

    feat : input 함수 분리 구현

--- src/main/java/subway/view/Input.java
+++ src/main/java/subway/view/Input.java
@@ -1,12 +1,26 @@
 package subway.view;
 
+import subway.util.GraphInitializer;
+
 import java.util.Scanner;
 
 public class Input {
     public static final String ASK_COMMAND = "## 원하는 기능을 선택하세요.";
 
-    public static String getInput(Scanner scanner) {
+    public static String getCommand(Scanner scanner) {
         System.out.println(ASK_COMMAND);
         return scanner.nextLine().trim();
+    }
+
+    public static String getSourceStation(Scanner scanner) {
+        System.out.println(GraphInitializer.ASK_SOURCE);
+        String station = scanner.nextLine().trim();
+        return station;
+    }
+
+    public static String getDestinationStation(Scanner scanner) {
+        System.out.println(GraphInitializer.ASK_DESTINATION);
+        String station = scanner.nextLine().trim();
+        return station;
     }
 }
