3c98113b93161029851bf28df957a32a8d46c584
amy choi <cgc8016@ewhain.net>
2020-12-19 13:54:56 +0900

    feat(outputView): add printPathTypes, printLookUpResult

--- src/main/java/subway/view/outputView.java
+++ src/main/java/subway/view/outputView.java
@@ -1,12 +1,40 @@
 package subway.view;
 
 import static subway.common.logger.guidePrint;
+import static subway.common.logger.infoPrint;
+
+import java.util.List;
+import subway.domain.Station;
 
 public class outputView {
 
 	public static void printMain() {
+		System.out.println();
 		guidePrint("메인 화면");
 		System.out.println("1. 경로 조회");
 		System.out.println("Q. 종료");
+		System.out.println();
+	}
+
+	public static void printPathTypes() {
+		System.out.println();
+		guidePrint("경로 기준");
+		System.out.println("1. 최단 거리");
+		System.out.println("2. 최소 시간");
+		System.out.println("B. 돌아가기");
+		System.out.println();
+	}
+
+	public static void printLookUpResult(int totalDistance, int totalTime,
+		List<Station> totalRoute) {
+		guidePrint("조회 결과");
+		infoPrint("---");
+		infoPrint("총 거리: " + totalDistance + "km");
+		infoPrint("총 소요 시간: " + totalTime + "분");
+		infoPrint("---");
+		for (Station station : totalRoute) {
+			infoPrint(station.getName());
+		}
+		System.out.println();
 	}
 }
