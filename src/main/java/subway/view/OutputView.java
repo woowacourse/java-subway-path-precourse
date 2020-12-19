39ea66f1b44136e87cacc9afd99276701a01e16b
Minjeong <mathmjseo@naver.com>
2020-12-19 14:22:01 +0900

    docs: README에 경로 조회 관련 기능 목록 추가

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -5,6 +5,7 @@ public class OutputView {
         System.out.println("## 메인 화면");
         System.out.println("1. 경로 조회");
         System.out.println("Q. 종료");
+        System.out.println();
     }
 
     public static void printRouteMainMenu() {
@@ -12,5 +13,6 @@ public class OutputView {
         System.out.println("1. 최단 거리");
         System.out.println("2. 최소 시간");
         System.out.println("B. 돌아가기");
+        System.out.println();
     }
 }
