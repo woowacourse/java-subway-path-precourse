385274c8a9345dab8fe0a12ed8ea844681243457
Noh Hyeon Jin <hjforj@naver.com>
2020-12-19 14:06:41 +0900

    feat: 원하는 기능 입력받는 기능 구현

--- src/main/java/subway/screen/Screen.java
+++ src/main/java/subway/screen/Screen.java
@@ -5,6 +5,12 @@ public interface Screen {
     public static final String QUIT = "Q. 종료";
     public static final String BACK = "B. 돌아가기";
     public static final String DOT = ". ";
-
+	
+    public static final String ASK_CHOOSE_FUNCTION = "원하는 기능을 선택하세요.";
+	
     public void printScreen();
+	
+    public static void chooseFunction() {
+        System.out.println(SHARP + ASK_CHOOSE_FUNCTION);
+    }
 }
