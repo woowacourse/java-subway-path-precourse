05ee468bdcd34c5f592b3e4460dbaa6d25b7c596
choijy1705 <cjy1705@naver.com>
2020-12-19 13:51:51 +0900

    feat(LineRepository,StationRepository) : 역간 시간 및 거리 초기화 구현

--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -1,5 +1,8 @@
 package subway.domain;
 
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -7,8 +10,12 @@ import java.util.Objects;
 
 public class LineRepository {
     private static final String[] LINES = {"2호선", "3호선", "신분당선"};
-    private static final String[][] LINE_STATIONS = {{"교대역","강남역","역삼역"},{"교대역","남부터미널역","양재역"},
+    private static final String[][] LINE_STATIONS = {{"교대역","강남역","역삼역"},{"교대역","남부터미널역","양재역","매봉역"},
             {"강남역","양재역","양재시민의숲역"}};
+    private static final WeightedMultigraph<String, DefaultWeightedEdge> timeWeight
+            = new WeightedMultigraph(DefaultWeightedEdge.class);
+    private static final WeightedMultigraph<String, DefaultWeightedEdge> lengthWeight
+            = new WeightedMultigraph(DefaultWeightedEdge.class);
     private static int INDEX = 0;
 
     private static final List<Line> lines = new ArrayList<>();
@@ -22,6 +29,41 @@ public class LineRepository {
             lines.add(line);
             INDEX++;
         }
+    }
+
+    static{
+        lengthWeight.addVertex("교대역");
+        lengthWeight.addVertex("강남역");
+        lengthWeight.addVertex("역삼역");
+        lengthWeight.addVertex("남부터미널역");
+        lengthWeight.addVertex("양재역");
+        lengthWeight.addVertex("매봉역");
+        lengthWeight.addVertex("양재시민의숲역");
+
+        lengthWeight.setEdgeWeight(timeWeight.addEdge("교대역","강남역"), 2);
+        lengthWeight.setEdgeWeight(timeWeight.addEdge("강남역","역삼역"), 2);
+        lengthWeight.setEdgeWeight(timeWeight.addEdge("교대역","남부터미널역"), 3);
+        lengthWeight.setEdgeWeight(timeWeight.addEdge("남부터미널역","양재역"), 6);
+        lengthWeight.setEdgeWeight(timeWeight.addEdge("양재역","매봉역"), 1);
+        lengthWeight.setEdgeWeight(timeWeight.addEdge("강남역","양재역"), 2);
+        lengthWeight.setEdgeWeight(timeWeight.addEdge("양재역","양재시민의숲역"), 10);
+    }
+    static{
+        timeWeight.addVertex("교대역");
+        timeWeight.addVertex("강남역");
+        timeWeight.addVertex("역삼역");
+        timeWeight.addVertex("남부터미널역");
+        timeWeight.addVertex("양재역");
+        timeWeight.addVertex("매봉역");
+        timeWeight.addVertex("양재시민의숲역");
+
+        timeWeight.setEdgeWeight(timeWeight.addEdge("교대역","강남역"), 3);
+        timeWeight.setEdgeWeight(timeWeight.addEdge("강남역","역삼역"), 3);
+        timeWeight.setEdgeWeight(timeWeight.addEdge("교대역","남부터미널역"), 2);
+        timeWeight.setEdgeWeight(timeWeight.addEdge("남부터미널역","양재역"), 5);
+        timeWeight.setEdgeWeight(timeWeight.addEdge("양재역","매봉역"), 1);
+        timeWeight.setEdgeWeight(timeWeight.addEdge("강남역","양재역"), 8);
+        timeWeight.setEdgeWeight(timeWeight.addEdge("양재역","양재시민의숲역"), 3);
     }
 
 
