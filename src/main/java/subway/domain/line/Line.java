c655cfb1988739de74ff446bf17d633bc881483a
opdshe <opdshe@naver.com>
2020-12-19 13:58:43 +0900

    feat: 노선에 경로를 추가할 수 있어야 한다

--- src/main/java/subway/domain/line/Line.java
+++ src/main/java/subway/domain/line/Line.java
@@ -1,15 +1,23 @@
 package subway.domain.line;
 
+import subway.domain.path.Path;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Line {
+    private List<Path> paths = new ArrayList<>();
     private String name;
 
     public Line(String name) {
         this.name = name;
     }
 
+    public void addPath(Path path) {
+        paths.add(path);
+    }
+
     public String getName() {
         return name;
     }
-
-    // 추가 기능 구현
 }
