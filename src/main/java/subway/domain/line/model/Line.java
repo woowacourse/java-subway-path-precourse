4a3548df99bb42763dbe87cdcaa28b6293135aea
김경록 <goodboy3421@gmail.com>
2020-12-19 16:28:17 +0900

    feat: 사전 등록 정보를 등록하는 기능
    Line의 상태값으로 stations 생성

--- src/main/java/subway/domain/line/model/Line.java
+++ src/main/java/subway/domain/line/model/Line.java
@@ -1,10 +1,17 @@
 package subway.domain.line.model;
 
+import subway.station.model.Station;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Line {
     private String name;
+    private final List<Station> stations;
 
-    public Line(String name) {
+    public Line(String name, List<Station> stations) {
         this.name = name;
+        this.stations = new ArrayList<>(stations);
     }
 
     public String getName() {
