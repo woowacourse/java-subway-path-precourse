f1c70cc8327f7845feed92090eafff21fb4058a1
RinSabbit <celena1004@naver.com>
2020-12-19 16:16:22 +0900

    refactor: OutputView의 문자열 매핑

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -5,13 +5,25 @@ import subway.exception.SubwayException;
 public class OutputView {
 
     public static final String DIVIDER = "---";
+    public static final String INFO_PREFIX = "[INFO] ";
+    public static final String MENU_PREFIX = "## ";
+    public static final String MAIN_SCREEN_MESSAGE = "메인 화면";
+    public static final String PATH_SCREEN_MESSAGE = "경로 기준";
+    public static final String RESULT_MESSAGE = "조회 결과";
+    public static final String CHOOSE_CATEGORY_MESSAGE = "원하는 기능을 선택하세요.";
+    public static final String INPUT_START_MESSAGE = "출발역을 입력하세요.";
+    public static final String INPUT_END_MESSAGE = "도착역을 입력하세요.";
+    public static final String TOTAL_DISTANCE = "총 거리: ";
+    public static final String TOTAL_TIME = "총 소요 시간: ";
+    public static final String KILOMETER = "km";
+    public static final String MINUTE = "분";
 
     public static void printMainScreen() {
-        System.out.println("## 메인 화면");
+        showMenuMessage(MAIN_SCREEN_MESSAGE);
     }
 
     public static void chooseCategory() {
-        System.out.println("## 원하는 기능을 선택하세요.");
+        showMenuMessage(CHOOSE_CATEGORY_MESSAGE);
     }
 
     public static void showErrorMessage(SubwayException exception) {
@@ -19,27 +31,31 @@ public class OutputView {
     }
 
     public static void printStartStation() {
-        System.out.println("## 출발역을 입력하세요.");
+        showMenuMessage(INPUT_START_MESSAGE);
     }
 
     public static void printEndStation() {
-        System.out.println("## 도착역을 입력하세요.");
+        showMenuMessage(INPUT_END_MESSAGE);
     }
 
     public static void printResult(int distance, int time){
-        System.out.println("## 조회 결과");
+        showMenuMessage(RESULT_MESSAGE);
         showInfoMessage(DIVIDER);
-        showInfoMessage("총 거리: " + distance +"km");
-        showInfoMessage("총 소요 시간: " + time + "분");
+        showInfoMessage(TOTAL_DISTANCE + distance + KILOMETER);
+        showInfoMessage(TOTAL_TIME + time + MINUTE);
         showInfoMessage(DIVIDER);
     }
 
     public static void printPathScreen() {
-        System.out.println("## 경로 기준");
+        showMenuMessage(PATH_SCREEN_MESSAGE);
     }
 
     public static void showInfoMessage(String message){
-        System.out.println("[INFO] " + message);
+        System.out.println(INFO_PREFIX + message);
+    }
+
+    public static void showMenuMessage(String message){
+        System.out.println(MENU_PREFIX + message);
     }
 
 }
