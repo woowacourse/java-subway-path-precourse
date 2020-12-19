4830812ea894fccf256b03c3bdcea0694efaac17
NewWisdom <wlgp2500@gmail.com>
2020-12-19 17:43:37 +0900

    feat(initial-setter): 초기 경로 셋팅하기 기능 추가

--- src/main/java/subway/InitialSetter.java
+++ src/main/java/subway/InitialSetter.java
@@ -1,17 +1,33 @@
 package subway;
 
-import subway.domain.Line;
-import subway.domain.LineRepository;
-import subway.domain.Station;
-import subway.domain.StationRepository;
+import subway.domain.*;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class InitialSetter {
     private static final List<String> STATION_NAME_LIST = Collections.unmodifiableList(
-        Arrays.asList("교대역", "강남역", "역삼역", "남부터미널역", "양재역", "매봉역"));
+            Arrays.asList("교대역", "강남역", "역삼역", "남부터미널역", "양재역", "매봉역"));
     private static final List<String> LINE_NAME_LIST =
             Collections.unmodifiableList(Arrays.asList("2호선", "3호선", "신분당선"));
+    private static final List<List<String>> PATH_STATION_LIST = Arrays.asList(
+            Arrays.asList("교대역", "강남역", "역삼역"),
+            Arrays.asList("교대역", "남부터미널역", "양재역", "매봉역"),
+            Arrays.asList("강남역", "양재역", "양재시민숲역")
+    );
+    private static final List<List<Integer>> PATH_DISTANCE_LIST = Arrays.asList(
+            Arrays.asList(2, 2),
+            Arrays.asList(3, 6, 1),
+            Arrays.asList(2, 10)
+    );
+    private static final List<List<Integer>> PATH_TIME_LIST = Arrays.asList(
+            Arrays.asList(3, 3),
+            Arrays.asList(2, 5, 1),
+            Arrays.asList(8, 3)
+    );
+    private static final int ADD_COUNT = 3;
+
 
     private InitialSetter() {
     }
@@ -36,8 +52,17 @@ public class InitialSetter {
         }
     }
 
-    private static void setUpPath(){
-
+    private static void setUpPath() {
+        for (int i = 0; i < ADD_COUNT; i++) {
+            Path path = new Path(
+                    LineRepository.selectLineByName(LINE_NAME_LIST.get(i)),
+                    StationRepository.selectStationListByNames(PATH_STATION_LIST.get(i)),
+                    PATH_DISTANCE_LIST.get(i),
+                    PATH_TIME_LIST.get(i)
+            );
+            PathRepository.addPath(path);
+        }
     }
+
 
 }
