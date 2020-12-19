8df1dfcc8ce85e0313bbd07113b1a87ac4cc826f
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-12-19 15:04:03 +0900

    feat: 기능선택 로직 구현

--- src/main/java/subway/Controller/SubwayController.java
+++ src/main/java/subway/Controller/SubwayController.java
@@ -17,9 +17,33 @@ public class SubwayController {
         while (true) {
             OutputView.mainView();
             String status = InputView.inputFunction(scanner);
+            if (status.equals("1")) {
+                routeLookup();
+            }
             if (status.equals("Q")) {
                 break;
             }
         }
+    }
+
+    public void routeLookup() {
+        OutputView.path_standardView();
+        String status = InputView.inputFunction(scanner);
+        if (status.equals("1")) {
+            shortestDistance();
+        }
+        if (status.equals("2")) {
+            shortestTime();
+        }
+    }
+
+    public void shortestDistance() {
+        String startStation = InputView.inputStartStation(scanner);
+        String arrivalStation = InputView.inputArriveStation(scanner);
+    }
+
+    private void shortestTime() {
+        String startStation = InputView.inputStartStation(scanner);
+        String arrivalStation = InputView.inputArriveStation(scanner);
     }
 }
