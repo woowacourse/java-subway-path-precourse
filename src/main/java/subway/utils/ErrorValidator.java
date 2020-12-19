2f41ac82ffb2b3e3198c153c738eb40742793932
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2020-12-19 17:57:45 +0900

    refactor: 에러 검사 기능 변경

--- src/main/java/subway/utils/ErrorValidator.java
+++ src/main/java/subway/utils/ErrorValidator.java
@@ -5,12 +5,13 @@ import java.util.List;
 import subway.domain.StationRepository;
 
 public class ErrorValidator {
+
     private final static String ERRORMESSAGE = "[ERROR] ";
     private final static String WRONGUSERINPUT = "잘못된 입력입니다.";
     private final static String WRONGSTATIONNAME = "존재하지 않는 역 이름입니다.";
     private final static String SAMESTATIONNAME = "출발역과 도착역이 동일합니다.";
 
-    public static void checkMainSelection(List<String> MAINSELECTLIST, String userSelection){
+    public static void checkMainSelection(List<String> MAINSELECTLIST, String userSelection) {
         if (!MAINSELECTLIST.contains(userSelection)) {
             throw new InputMismatchException(ERRORMESSAGE + WRONGUSERINPUT);
         }
