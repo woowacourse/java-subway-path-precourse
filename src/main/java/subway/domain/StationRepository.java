abba14116175e7ae6bd70f410f1593e2a933963a
bbakjae <qkrwogus0724@naver.com>
2020-12-19 14:27:21 +0900

    feat(StationRepository.java): 다음 역 정보 생성 삭제

--- src/main/java/subway/domain/StationRepository.java
+++ src/main/java/subway/domain/StationRepository.java
@@ -7,9 +7,9 @@ import java.util.List;
 import java.util.Objects;
 
 public class StationRepository {
-	private static final List<Station> stations = new ArrayList<>(Arrays.asList(new Station("교대역"),new Station("강남역"),new Station("역삼역"),new Station("남부터미널역"),new Station("양재역"),
-    		new Station("양재시민의숲역"),new Station("매봉역")));
-
+	 private static final List<Station> stations = new ArrayList<>(Arrays.asList(new Station("교대역"),new Station("강남역"),new Station("역삼역"),new Station("남부터미널역"),new Station("양재역"),
+	    		new Station("양재시민의숲역"),new Station("매봉역")));
+	 
     public static List<Station> stations() {
         return Collections.unmodifiableList(stations);
     }
