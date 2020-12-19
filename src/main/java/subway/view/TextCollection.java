67ba08c1e542be5853eb45b13eff2723bb48bca6
operasystem6 <operasystem6@naver.com>
2020-12-19 17:41:41 +0900

    feat: 텍스트 모음

---
+++ src/main/java/subway/view/TextCollection.java
@@ -0,0 +1,31 @@
+package subway.view;
+
+public class TextCollection {
+    public static final String PREFIX = "##";
+    public static final String SEPARATOR = "---";
+    public static final String MAIN_SCREEN_MESSAGE = "메인 화면";
+    public static final String SEARCH_MENU_MESSAGE = "경로 조회";
+    public static final String EXIT_MENU_MESSAGE = "종료";
+    public static final String SEARCH_MANAGEMENT_MESSAGE = "경로 기준";
+    public static final String MINIMUM_DISTANCE_MESSAGE = "최단 거리";
+    public static final String RETURN_MENU_MESSAGE = "돌아가기";
+    public static final String MINIMUM_TIME_MESSAGE = "최소 시간";
+    public static final String SEARCH_RESULT_MESSAGE = "조회 결과";
+
+    public static final String INFO_TAG = "[INFO] ";
+    public static final String ERROR_TAG = "[ERROR] ";
+
+    public static final String SELECT_FUNCTION_MESSAGE = "원하는 기능을 선택하세요.";
+    public static final String REGISTER_SOURCE_STATION_MESSAGE = "출발역을 입력하세요.";
+    public static final String REGISTER_TARGET_STATION_MESSAGE = "도착역을 입력하세요.";
+
+    public static final String SAME_STATION_MESSAGE = "출발역과 도착역의 이름이 같습니다";
+    public static final String NOT_NUMBER_MESSAGE = "숫자가 아닌 값을 입력하였습니다.";
+    public static final String WRONG_MENU_INPUT_MESSAGE = "선택할 수 없는 기능입니다.";
+    public static final String NOT_EXIST_STATION_MESSAGE = "존재하지 않는 역입니다.";
+    public static final String NOT_EXIST_LINE_MESSAGE = "존재하지 않는 노선입니다.";
+    public static final String NOT_EXIST_SECTION_MESSAGE = "존재하지 않는 구간입니다.";
+
+    public static final String TOTAL_DISTANCE_MESSAGE = "총 거리";
+    public static final String TOTAL_TIME_MESSAGE = "총 시간";
+}
