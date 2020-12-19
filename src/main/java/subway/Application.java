b368c2f27611530f793f6c8539a07d3d0c537927
Livenow14 <xowjd41@naver.com>
2020-12-19 15:41:00 +0900

    feat: - 초기 저장 (시간(분) 미포함)
    시간을 어떻게 저장해야할지 감이 안잡혀서 현재는 구간많 있는 것으로 저장함.

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,5 +1,8 @@
 package subway;
 
+import subway.domain.section.SectionRepository;
+import subway.domain.section.SectionService;
+import subway.domain.station.StationService;
 import subway.view.InputView;
 import subway.view.OutputView;
 
@@ -9,6 +12,9 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
+        DataInitService dataInitService = new DataInitService(new StationService(), new SectionService(new SectionRepository()));
+        dataInitService.init();
+
         SubwayManageApp subwayManageApp = new SubwayManageApp(new InputView(scanner), new OutputView(new StringBuilder()));
         subwayManageApp.startApp();
     }
--- src/main/java/subway/domain/distanceTime/Distance.java
+++ src/main/java/subway/domain/distanceTime/Distance.java
@@ -1,4 +1,4 @@
-package subway.domain;
+package subway.domain.distanceTime;
 
 import java.math.BigDecimal;
 
