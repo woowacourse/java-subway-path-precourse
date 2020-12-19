a0f465a53bb975fedcac800ebaa9b05d75675868
xrabcde <joara124@ewhain.net>
2020-12-19 13:52:30 +0900

    feat(View): 경로 조회 목록을 출력해주는 showPathMenu 구현

--- src/main/java/subway/view/View.java
+++ src/main/java/subway/view/View.java
@@ -7,6 +7,13 @@ public class View {
         System.out.println("Q. 종료");
     }
 
+    public static void showPathMenu() {
+        System.out.println("## 경로 기준");
+        System.out.println("1. 최단 거리");
+        System.out.println("2. 최소 시간");
+        System.out.println("B. 돌아가기");
+    }
+
     public static void finishProgram() {
         System.out.println("## 프로그램 종료");
     }
