1f6a5bd682d97d13bbc4b9fbb7f9cf1e2ee5ffd6
DH Lim <dlarkfl2@gamil.com>
2020-12-19 17:24:23 +0900

    feat: 노선에서 구간 저장 기능 추가

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,5 +1,9 @@
 package subway.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Line {
     private String name;
 
@@ -12,4 +16,14 @@ public class Line {
     }
 
     // 추가 기능 구현
+    private final List<Section> sections = new ArrayList<>();
+
+    public void addSection(Section section) {
+        sections.add(section);
+        SectionRepository.addSection(section);
+    }
+
+    public List<Section> sections() {
+        return Collections.unmodifiableList(sections);
+    }
 }
