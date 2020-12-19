5bbb9b534913a2d3cd2d7ead198e634b2a34f2d9
amy choi <cgc8016@ewhain.net>
2020-12-19 16:57:45 +0900

    feat(Graph): add getTimeByVertextList, getDistanceByVertexList

--- src/main/java/subway/domain/Graph.java
+++ src/main/java/subway/domain/Graph.java
@@ -1,5 +1,6 @@
 package subway.domain;
 
+import java.util.List;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
 
@@ -20,5 +21,25 @@ public class Graph {
 
 	public static void addDistanceEdge(String firstStationName, String secondStationName, int distanceWeight) {
 		DISTANCE_GRAPH.setEdgeWeight(DISTANCE_GRAPH.addEdge(firstStationName, secondStationName), distanceWeight);
+	}
+
+	public static int getTimeByVertexList(List<String> stringVertexList) {
+		int totalTime = 0;
+		int end_index = stringVertexList.size()-1;
+		for (int index = 0; index < end_index; index++) {
+			totalTime += (int) TIME_GRAPH.getEdgeWeight(
+				TIME_GRAPH.getEdge(stringVertexList.get(index), stringVertexList.get(index+1)));
+		}
+		return totalTime;
+	}
+
+	public static int getDistanceByVertexList(List<String> stringVertexList) {
+		int totalDistance = 0;
+		int end_index = stringVertexList.size()-1;
+		for (int index = 0; index < end_index; index++) {
+			totalDistance += (int) DISTANCE_GRAPH.getEdgeWeight(
+				DISTANCE_GRAPH.getEdge(stringVertexList.get(index), stringVertexList.get(index+1)));
+		}
+		return totalDistance;
 	}
 }
