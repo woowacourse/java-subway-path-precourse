030f04bda9e6e8997e3aaf9e8d43c74b74bedcf5
pika96 <52442949+pika96@users.noreply.github.com>
2020-12-19 17:05:24 +0900

    feat: NodeData에서 데이터를 가져오는 메소드 추가

--- src/main/java/subway/domain/NodeData.java
+++ src/main/java/subway/domain/NodeData.java
@@ -1,13 +1,31 @@
 package subway.domain;
 
 public class NodeData {
+    private Station beforeStation;
     private Station nextStation;
     private int timeCost;
     private int distanceCost;
 
-    public NodeData(Station nextStation, int timeCost, int distanceCost){
+    public NodeData(Station beforeStation, Station nextStation, int timeCost, int distanceCost) {
+        this.beforeStation = beforeStation;
         this.nextStation = nextStation;
         this.timeCost = timeCost;
         this.distanceCost = distanceCost;
+    }
+
+    public String getBeforeStation() {
+        return beforeStation.getName();
+    }
+
+    public String getNextStation() {
+        return nextStation.getName();
+    }
+
+    public int getTimeCost() {
+        return timeCost;
+    }
+
+    public int getDistanceCost() {
+        return distanceCost;
     }
 }
