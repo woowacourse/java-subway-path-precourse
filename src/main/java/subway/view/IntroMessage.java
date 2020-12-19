9f7ba454dae1d959a8562fa5a6a55a894316bf39
yunjung <cairofaith@gmail.com>
2020-12-19 17:45:41 +0900

    fix(IntroMessage): 오타 수정

--- src/main/java/subway/view/IntroMessage.java
+++ src/main/java/subway/view/IntroMessage.java
@@ -6,7 +6,7 @@ public enum IntroMessage {
 	MAIN_MENU("\n## 메인 화면"),
 	PATH_MENU("\n## 경로 기준"),
 	FROM_STATION("\n## 출발역을 입력하세요."),
-	TO_STATION("## 도착역을 입력하세요.");
+	TO_STATION("\n## 도착역을 입력하세요.");
 	
 	final private String message;
 	
