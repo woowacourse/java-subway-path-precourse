17619eaeba3bd7481dc61fafc2893975e8105ba8
Gomding <backchida@gmail.com>
2020-12-19 15:40:47 +0900

    feat : 메인화면 view를 담당하는 MainMenu 생성

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,14 @@
 package subway;
 
+import subway.view.MainMenu;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
+
+        new MainMenu(scanner).startMainMENU();
     }
 }
---
+++ src/main/java/subway/view/MainMenu.java
@@ -0,0 +1,19 @@
+package subway.view;
+
+import java.util.Scanner;
+
+public class MainMenu {
+    private final String MAIN_TITLE = "## 메인 화면";
+    private final String MAINMENU_1 = "1. 경로 조회";
+    private final String QUIT_APP = "Q. 종료";
+
+    private Scanner scanner;
+
+    public MainMenu(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void startMainMENU() {
+
+    }
+}
