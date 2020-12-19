2e38b613349fe8c4264e10934f096702a6194925
Taehwa Lee <xoghk0321@gmail.com>
2020-12-19 15:35:38 +0900

    feat: 지하철 역 기능을 구현한다.

---
+++ src/test/java/subway/domain/station/StationTest.java
@@ -0,0 +1,30 @@
+package subway.domain.station;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import subway.domain.station.domain.Station;
+import subway.domain.station.exception.ShorterThanMinStationNameException;
+
+@DisplayName("지하철 역 기능에 대한 테스트")
+class StationTest {
+
+    @DisplayName("지하철 역 이름은 2글자 이상이어야 한다.")
+    @Test
+    void shorterThanMinStationNameException() {
+        final String name = "a";
+
+        assertThrows(ShorterThanMinStationNameException.class, () -> Station.from(name));
+    }
+
+    @DisplayName("지하철 역을 생성할 수 있다.")
+    @Test
+    void create() {
+        final String name = "test";
+        final Station station = Station.from(name);
+
+        assertNotNull(station);
+    }
+}
