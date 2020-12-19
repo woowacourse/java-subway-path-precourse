ecdb44332d11b38ef313212fbbc417d4c4009bd5
Minjeong <mathmjseo@naver.com>
2020-12-19 14:28:22 +0900

    feat: 조회 결과 화면 일부 구현

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -1,6 +1,13 @@
 package subway.view;
 
+import subway.domain.Station;
+
+import java.util.List;
+
 public class OutputView {
+    private static final String INFO_MARK = "[INFO] ";
+    private static final String SEPARATER = "---";
+
     public static void printMainMenu() {
         System.out.println("## 메인 화면");
         System.out.println("1. 경로 조회");
@@ -14,5 +21,26 @@ public class OutputView {
         System.out.println("2. 최소 시간");
         System.out.println("B. 돌아가기");
         System.out.println();
+    }
+
+    public static void printResults(){
+        System.out.println("## 조회 결과");
+        
+        /*
+        ## 조회 결과
+[INFO] ---
+[INFO] 총 거리: 6km
+[INFO] 총 소요 시간: 14분
+[INFO] ---
+[INFO] 교대역
+[INFO] 강남역
+[INFO] 양재역
+         */
+    }
+
+    private static void printResultsByStationList(List<Station> stations){
+        for(Station station: stations){
+            System.out.println(INFO_MARK + station.getName());
+        }
     }
 }
