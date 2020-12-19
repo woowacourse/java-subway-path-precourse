92922b402ee5b57af82237e33a45664d0a571e98
LeeJuHyuk <dlwngur1535@gmail.com>
2020-12-19 16:36:52 +0900

    feat: 출발지와 도착지가 같은 역인지 검증 로직 추가

---
+++ src/main/java/subway/exception/SectionStationDuplicateException.java
@@ -0,0 +1,9 @@
+package subway.exception;
+
+public class SectionStationDuplicateException extends SubwayMapException{
+    private static final String message = "[ERROR] 출발역과 도착역은 같을 수 없습니다.";
+
+    public SectionStationDuplicateException() {
+        super(message);
+    }
+}
--- src/main/java/subway/service/PathService.java
+++ src/main/java/subway/service/PathService.java
@@ -2,6 +2,7 @@ package subway.service;
 
 import subway.domain.Station;
 import subway.domain.StationRepository;
+import subway.exception.SectionStationDuplicateException;
 import subway.exception.StationNotFoundException;
 import subway.view.InputView;
 
@@ -12,11 +13,19 @@ public class PathService {
     public static void calculateShortDistance() {
         Station startStation = initStation(InputView.PLEASE_INPUT_START_STATION);
         Station endStation = initStation(InputView.PLEASE_INPUT_END_STATION);
+
+        validateStationDuplicated(startStation, endStation);
     }
 
     private static Station initStation(String message) {
         return StationRepository.findByName(InputView.print(message))
             .orElseThrow(StationNotFoundException::new);
+    }
+
+    private static void validateStationDuplicated(Station startStation, Station endStation) {
+        if(startStation.equals(endStation)){
+            throw new SectionStationDuplicateException();
+        }
     }
 
 }
