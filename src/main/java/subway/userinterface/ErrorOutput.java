6a54e4ce21f9704347d16fd44189db4b9f934ad4
Sehwan_Jang <whilter08@gmail.com>
2020-12-19 17:41:07 +0900

    refactor: 출력문 상수로 추출

--- src/main/java/subway/userinterface/ErrorOutput.java
+++ src/main/java/subway/userinterface/ErrorOutput.java
@@ -5,10 +5,14 @@ import subway.domain.StationRepository;
 
 public class ErrorOutput {
     private static final String ERROR = "\n[ERROR] ";
+    private static final String NOT_EXISTING_STATION = "존재하지 않는 역 이름입니다.";
+    private static final String SAME_START_END_INPUT = "출발역과 도착역이 동일합니다.";
+    private static final String START_END_NOT_CONNECTED = "출발역과 도착역이 서로 연결되어있지 않습니다.";
+    private static final String INPUT_ERROR = "선택할 수 없는 기능입니다.";
 
     public static boolean returnStatus(boolean status) {
         if (status) {
-            System.out.println(ERROR + "선택할 수 없는 기능입니다.");
+            System.out.println(ERROR + INPUT_ERROR);
         }
         return status;
     }
@@ -25,7 +29,7 @@ public class ErrorOutput {
 
     public static boolean printWrongStationInput(String input) {
         if (!StationRepository.isStationPresent(input)) {
-            System.out.println(ERROR+"존재하지 않는 역 이름입니다.");
+            System.out.println(ERROR + NOT_EXISTING_STATION);
             return true;
         }
         return false;
@@ -33,14 +37,14 @@ public class ErrorOutput {
 
     public static boolean printSameStationNameError(String start, String end) {
         if (start.equals(end)) {
-            System.out.println(ERROR +"출발역과 도착역이 동일합니다.");
+            System.out.println(ERROR + SAME_START_END_INPUT);
             return true;
         }
         return false;
     }
 
     public static void printNotConnectedError() {
-        System.out.println(ERROR+"출발역과 도착역이 서로 연결되어있지 않습니다.");
+        System.out.println(ERROR + START_END_NOT_CONNECTED);
     }
 
 
--- src/main/java/subway/userinterface/Input.java
+++ src/main/java/subway/userinterface/Input.java
@@ -6,7 +6,13 @@ import java.util.Scanner;
 
 public class Input {
     private static Scanner scanner = new Scanner(System.in);
+
     private static final String PREFIX = "\n## ";
+    private static final String RESULT_NOTIFICATION = "조회 결과";
+    private static final String MAIN_MENU = "메인 화면";
+    private static final String SEARCH_MENU = "경로 기준";
+    private static final String START_STATION_INPUT = "출발역을 입력하세요";
+    private static final String END_STATION_INPUT = "도착역을 입력하세요";
     private static final String CHOOSE_MENU = PREFIX + "원하는 기능을 선택하세요.";
 
     public static String newInput() {
@@ -14,11 +20,11 @@ public class Input {
     }
 
     public static void printResultNotification() {
-        System.out.println(PREFIX+"조회 결과");
+        System.out.println(PREFIX + RESULT_NOTIFICATION);
     }
 
     public static void printMainMenu() {
-        System.out.println(PREFIX + "메인 화면");
+        System.out.println(PREFIX + MAIN_MENU);
         for (int i = 0; i < MenuRepository.mainMenuButtons.size(); i++) {
             System.out.println(MenuRepository.mainMenuButtons.get(i) + ". " + MenuRepository.mainMenuNames.get(i));
         }
@@ -26,7 +32,7 @@ public class Input {
     }
 
     public static void printSearchMenu() {
-        System.out.println(PREFIX + "경로 기준");
+        System.out.println(PREFIX + SEARCH_MENU);
         for (int i = 0; i < MenuRepository.searchMenuButtons.size(); i++) {
             System.out.println(MenuRepository.searchMenuButtons.get(i) + ". " + MenuRepository.searchMenuNames.get(i));
         }
@@ -34,10 +40,10 @@ public class Input {
     }
 
     public static void printStartStation() {
-        System.out.println(PREFIX + "출발역을 입력하세요");
+        System.out.println(PREFIX + START_STATION_INPUT);
     }
 
     public static void printEndStation() {
-        System.out.println(PREFIX + "도착역을 입력하세요");
+        System.out.println(PREFIX + END_STATION_INPUT);
     }
 }
