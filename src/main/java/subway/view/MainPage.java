7bef2095f3c72239570172c8a54471b013d2ddd7
perenok <perenoke@gmail.com>
2020-12-19 17:22:59 +0900

    fix(MainPage): add code
    출력문 형식에 맞게 "원하는 기능을 선택하세요" 구문 추가

--- src/main/java/subway/view/MainPage.java
+++ src/main/java/subway/view/MainPage.java
@@ -7,6 +7,7 @@ public class MainPage {
         System.out.println("## 메인 화면");
         System.out.println("1. 경로 조회");
         System.out.println("Q. 종료");
+        System.out.println("\n## 원하는 기능을 선택하세요.");
     }
 
     public void printWrongItemError(){
