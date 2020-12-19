0853edf2874a82e6e4a8b10038fecab852d011c3
이동건 <tnpdlzi@naver.com>
2020-12-19 13:56:16 +0900

    feat(Route): 역 입력받기

--- src/main/java/subway/domain/Route.java
+++ src/main/java/subway/domain/Route.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Route {
     private Scanner scanner;
+    private Station startStation;
+    private Station endStation;
 
     public Route(Scanner scanner) {
         this.scanner = scanner;
@@ -15,6 +17,14 @@ public class Route {
 
     public void findDistanceByTime() {
 
+    }
+
+    public void getStationName() {
+        System.out.println("## 출발역을 입력하세요.");
+        this.startStation = StationRepository.getStationByName(scanner.next());
+
+        System.out.println("## 도착역을 입력하세요.");
+        this.endStation = StationRepository.getStationByName(scanner.next());
     }
 
 }
