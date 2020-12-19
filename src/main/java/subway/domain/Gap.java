a5d39e01f64fbe01c0bf43052e96ecffe20c57da
Hansaem Woo <feliscatus@hanyang.ac.kr>
2020-12-19 14:45:30 +0900

    refactor(Gap): rearrange arguments of constructor

--- src/main/java/subway/domain/Gap.java
+++ src/main/java/subway/domain/Gap.java
@@ -2,12 +2,12 @@ package subway.domain;
 
 public class Gap {
 
-    private final int time;
     private final int distance;
+    private final int time;
 
-    public Gap(final int time, final int distance) {
-        this.time = time;
+    public Gap(final int distance, final int time) {
         this.distance = distance;
+        this.time = time;
     }
 
     public int getTime() {
