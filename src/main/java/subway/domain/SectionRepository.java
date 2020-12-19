395e88baac91db129f3bcba92369cdce57fb2919
Hansaem Woo <feliscatus@hanyang.ac.kr>
2020-12-19 17:06:30 +0900

    feat(SectionRepository): add addSectionByName and corresponding methods
    노선 이름, 역 이름 배열, 구간 간격 리스트를 이용하여 구간을 추가하는 메소드 추가
    노선 저장소로부터 노선을 가져오는 메소드 추가
    역 저장소로부터 역을 가져오는 메소드 추가

--- src/main/java/subway/domain/SectionRepository.java
+++ src/main/java/subway/domain/SectionRepository.java
@@ -1,9 +1,12 @@
 package subway.domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 public class SectionRepository {
 
@@ -23,6 +26,32 @@ public class SectionRepository {
     public static void addSection(Section section) throws IllegalArgumentException {
         validateSectionLineNameDuplicate(section.getLine().getName());
         sections.add(section);
+    }
+
+    private static Line getLineFromOptional(Optional<Line> optionalLine)
+        throws IllegalArgumentException {
+        if (optionalLine.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        return optionalLine.get();
+    }
+
+    private static Station getStationFromOptional(Optional<Station> optionalStation)
+        throws IllegalArgumentException {
+        if (optionalStation.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        return optionalStation.get();
+    }
+
+    public static void addSectionByNames(String lineName, String[] stationNames, List<Gap> gaps)
+        throws IllegalArgumentException {
+        final Line sectionLine = getLineFromOptional(LineRepository.getLineByName(lineName));
+        final List<Station> stations = new LinkedList<>();
+        Arrays.stream(stationNames).forEach(stationName ->
+            stations.add(getStationFromOptional(StationRepository.getStationByName(stationName)))
+        );
+        addSection(new Section(sectionLine, stations, gaps));
     }
 
     public static boolean deleteSectionByLineName(String lineName) {
