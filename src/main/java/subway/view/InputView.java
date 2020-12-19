2b393a0961cf3432b42357122279bd54d06d5c68
taelee <taelee@student.42seoul.kr>
2020-12-19 15:11:09 +0900

    fix(InputView): 각 화면에서 입력된 메뉴 값에서 에러 발생시 상위 클래스로 넘겨주기

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class InputView {
 
-    public static String askMenu(Scanner scanner, String regex){
+    public static String askMenu(Scanner scanner, String regex) throws CustomException{
         System.out.println("\n## 원하는 기능을 선택하세요.");
         String userInput = scanner.nextLine().trim();
         Validate.matchWithRegex(regex, userInput);
