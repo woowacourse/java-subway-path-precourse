de030efd3276a56d9095408266f8bc2cecfc757e
kmj990929 <kmj990929@dgist.ac.kr>
2020-12-19 13:26:27 +0900

    feat: 메인 화면 출력 기능 구현

---
+++ src/main/java/subway/view/OutputView.java
@@ -0,0 +1,11 @@
+package subway.view;
+
+public class OutputView {
+
+    private static final String MAIN_SCREEN = "\n## 메인 화면\n1. 경로 조회\nQ. 종료";
+
+    public static void printMainScreen() {
+        System.out.println(MAIN_SCREEN);
+    }
+
+}
