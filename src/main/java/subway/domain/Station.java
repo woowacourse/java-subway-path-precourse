5b72cf939b19b4a95535c4b2492f4c178885d53b
TaewanKimmmm <ktww88@gmail.com>
2020-12-19 13:41:02 +0900

    feat: 이름이 같으면 같은 역으로 취급하기 위해 equals와 hashcode override

--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -1,5 +1,7 @@
 package subway.domain;
 
+import java.util.Objects;
+
 public class Station {
     private String name;
 
@@ -12,4 +14,17 @@ public class Station {
     }
 
     // 추가 기능 구현
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Station station = (Station) o;
+        return Objects.equals(name, station.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
