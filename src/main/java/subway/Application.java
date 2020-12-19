df2ef4be42d4e0491d17928e4e1d1f8f5f5a842d
mungto <daum7766@naver.com>
2020-12-19 13:53:15 +0900

    refactor(Application): 입력클래스 초기화 및 유저인터페이스클래스 호출방식 변경

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        UserInterfaceView userInterfaceView = new UserInterfaceView(scanner);
+        Input.init(scanner);
+        UserInterfaceView userInterfaceView = new UserInterfaceView();
         userInterfaceView.start();
     }
 }
