ee9d3aefeaa06ccad3e9e72dad61fa697438ba70
tributetothemoon <kkm97351@gmail.com>
2020-12-19 14:30:50 +0900

    feat(SectionRepository): 모든 구간들의 정보를 저장할 Section Repository 생성

--- src/main/java/subway/domain/StationRepository.java
+++ src/main/java/subway/domain/StationRepository.java
@@ -10,8 +10,6 @@ import java.util.Objects;
 public class StationRepository {
     private static final List<Station> stations = new ArrayList<>();
 
-
-
     public static List<Station> stations() {
         return Collections.unmodifiableList(stations);
     }
