7dc2e76ec0b09f9b9f214dd9559d273e3121e9de
mungto <daum7766@naver.com>
2020-12-19 15:47:54 +0900

    feat(Station): 역에 자신과 연결된 정보를 추가하는 함수 추가

--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -1,7 +1,13 @@
 package subway.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import subway.ConnectData;
+
 public class Station {
+
     private String name;
+    private List<ConnectData> ConnectDataList = new ArrayList<>();
 
     public Station(String name) {
         this.name = name;
@@ -13,5 +19,11 @@ public class Station {
 
     public boolean isEqualName(String name) {
         return name.equals(this.name);
+    }
+
+    public void addConnectData(String stationName, int distance, int time) {
+        Station station = StationRepository.getStation(stationName);
+        ConnectData connectData = new ConnectData(station, distance, time);
+        ConnectDataList.add(connectData);
     }
 }
