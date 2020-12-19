1dcf46ca816571dad5a7f28c2bbf9ea8b25e97da
KJunseo <max9106@naver.com>
2020-12-19 14:24:14 +0900

    feat: 거리와 시간 비용을 나타내는 클래스 생성

---
+++ src/main/java/subway/cost/Cost.java
@@ -0,0 +1,11 @@
+package subway.cost;
+
+public class Cost {
+    private int distance;
+    private int time;
+
+    public Cost(int distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
+}
