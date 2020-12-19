5350a3df87b34422e3445f5e828f87b1198a18c8
Eugine Park <pkeugine@gmail.com>
2020-12-19 13:53:25 +0900

    feat(MenuControllerValidator): validate numeric option decision

--- src/main/java/subway/utils/MenuControllerValidator.java
+++ src/main/java/subway/utils/MenuControllerValidator.java
@@ -1,8 +1,20 @@
 package subway.utils;
 
+import subway.view.ErrorMessage;
+
 public class MenuControllerValidator {
 
-    public static void validateDecision(String decision, int maxDecisionValue) {
+    private static final int MINIMUM_DECISION_VALUE = 1;
 
+    public static void validateDecision(String decision, int maxDecisionValue) {
+        validateNumeric(decision);
+    }
+
+    private static void validateNumeric(String decision) {
+        try {
+            Integer.parseInt(decision);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.DECISION_WRONG);
+        }
     }
 }
---
+++ src/main/java/subway/view/ErrorMessage.java
@@ -0,0 +1,8 @@
+package subway.view;
+
+public class ErrorMessage {
+
+    private static final String LABEL = "[ERROR] ";
+
+    public static final String DECISION_WRONG = LABEL + "선택할 수 없는 기능입니다.";
+}
