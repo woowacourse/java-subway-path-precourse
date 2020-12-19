b046bd6c156062434f61fd4e59ca71a2f16fe836
JinseongHwang <eddy5360@naver.com>
2020-12-19 17:59:04 +0900

    refactor: final 키워드 추가

--- src/main/java/subway/util/Validator.java
+++ src/main/java/subway/util/Validator.java
@@ -7,25 +7,30 @@ import java.util.List;
 
 public class Validator {
 
-    public static void functionSelect(List<String> choices, String command) {
+    public static void functionSelect(final List<String> choices, final String command) {
         if (!choices.contains(command)) {
             OutputView.printError(OutputView.ERROR_INVALID_SELECT);
             throw new IllegalArgumentException();
         }
     }
 
-    public static void existStation(String stationName) {
+    public static void existStation(final String stationName) {
         if (!StationRepository.isExist(stationName)){
             OutputView.printError(OutputView.ERROR_INVALID_STATION);
             throw new IllegalArgumentException();
         }
     }
 
-    public static boolean sameStation(String source, String destination) {
+    public static boolean sameStation(final String source, final String destination) {
         if (source.equals(destination)) {
             OutputView.printError(OutputView.ERROR_SAME_STATION);
             return true;
         }
+        return false;
+    }
+
+    public static boolean unconnected(final String source, final String destination) {
+        // 구현 못했습니다.
         return false;
     }
 
