559a004438751ade22b626ec7b522c5e88408012
KSB <woltaryksb1@gmail.com>
2020-12-19 14:48:55 +0900

    feat: 경로 표시 기능 추가

--- src/main/java/subway/domain/Graph.java
+++ src/main/java/subway/domain/Graph.java
@@ -4,6 +4,8 @@ import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
 
+import java.util.List;
+
 public class Graph {
 
     private final WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
@@ -12,8 +14,12 @@ public class Graph {
     public Graph() {
     }
 
-    public double shortestPath(Station v1, Station v2) {
+    public double shortestPathWeight(Station v1, Station v2) {
         return dijkstraShortestPath.getPath(v1, v2).getWeight();
+    }
+
+    public List<Station> shortestPath(Station v1, Station v2) {
+        return dijkstraShortestPath.getPath(v1, v2).getVertexList();
     }
 
     public void makeConnection(Station v1, Station v2, double weight) {
