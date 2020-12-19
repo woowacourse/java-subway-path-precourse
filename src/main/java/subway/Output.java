0cfab8534badad3c1e87634b9114935ebfca9c1b
mungto <daum7766@naver.com>
2020-12-19 14:17:14 +0900

    feat(Output): 경로화면 출력함수 생성

--- src/main/java/subway/Output.java
+++ src/main/java/subway/Output.java
@@ -8,6 +8,11 @@ public class Output {
     private static final String[] MAIN_LIST = {MAIN_VIEW, ROUTE_SEARCH, QUIT};
     private static final String ERROR = "[ERROR] ";
     private static final String BAD_CHOICE = "목록에 없는것을 선택하셨습니다.";
+    private static final String ROUTE_VIEW = "## 경로 기준";
+    private static final String MIN_DISTANCE = "1. 최단 거리";
+    private static final String SHORT_TIME = "2. 최소 시간";
+    private static final String BACK = "B. 돌아가기";
+    private static final String[] ROUTE_LIST = {ROUTE_VIEW, MIN_DISTANCE, SHORT_TIME, BACK};
 
     public static void printMainView() {
         for (String str : MAIN_LIST) {
@@ -18,5 +23,12 @@ public class Output {
 
     public static void printChoiceErrorMessage() {
         System.out.println(ERROR + BAD_CHOICE);
+    }
+
+    public static void printRouteSearchView() {
+        for (String str : ROUTE_LIST) {
+            System.out.println(str);
+        }
+        System.out.println();
     }
 }
