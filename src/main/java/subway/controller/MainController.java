ea0766282c0303e2d90472d0fc3b03e56d23374b
perenok <perenoke@gmail.com>
2020-12-19 17:20:52 +0900

    feat(MainController): add startProgram
    startProgram을 통해 초기 설정을 한 후 메인 화면을 출력함
    한번 호출한 후 재반복시 startMainPage메소드를 사용하여 계속된 초기설정 방지

--- src/main/java/subway/controller/MainController.java
+++ src/main/java/subway/controller/MainController.java
@@ -1,5 +1,7 @@
 package subway.controller;
 
+import subway.Initial;
+import subway.domain.LineRepository;
 import subway.view.MainPage;
 import utils.InputUtils;
 
@@ -9,27 +11,27 @@ public class MainController {
     private static final String SELECT_ROUTE = "1";
     private static final String QUIT = "Q";
     private static final MainPage MAIN_PAGE = new MainPage();
-    private Scanner scanner;
+    private static final RouteController ROUTE_CONTROLLER = new RouteController();
 
-    public MainController(Scanner scanner) {
-        this.scanner = scanner;
+    public static void startProgram(Scanner scanner){
+        Initial.setInitial();
+        startMainPage(scanner);
     }
 
-    public void startMainPage() {
+    public static void startMainPage(Scanner scanner) {
         MAIN_PAGE.printMainPage();
-        forkPath(InputUtils.inputString(scanner));
+        forkPath(InputUtils.inputString(scanner), scanner);
     }
 
-    public void forkPath(String item){
+    public static void forkPath(String item, Scanner scanner){
         if(SELECT_ROUTE.equals(item)){
-
+            ROUTE_CONTROLLER.startRoutePage(scanner);
             return;
         }
         if(QUIT.equals(item)){
-
             return;
         }
         MAIN_PAGE.printWrongItemError();
-        startMainPage();
+        startMainPage(scanner);
     }
 }
