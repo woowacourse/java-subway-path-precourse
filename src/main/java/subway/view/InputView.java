98ada81c64de06200b09640d65c74c069b09f445
taelee <taelee@student.42seoul.kr>
2020-12-19 15:10:32 +0900

    feat(InputView): 각 화면에서 입력된 메뉴 값 좌우 공백 trim처리

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -7,9 +7,9 @@ import java.util.Scanner;
 
 public class InputView {
 
-    public static String askMenu(Scanner scanner, String regex) {
+    public static String askMenu(Scanner scanner, String regex){
         System.out.println("\n## 원하는 기능을 선택하세요.");
-        String userInput = scanner.nextLine();
+        String userInput = scanner.nextLine().trim();
         Validate.matchWithRegex(regex, userInput);
         return userInput;
     }
