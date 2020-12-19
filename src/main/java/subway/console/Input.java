7bf8793f84574544e23b4d32752fe8949509f9d7
sounmind <sounmind777@gmail.com>
2020-12-19 17:29:29 +0900

    feat(Input): 출발역과 도착역 선택화면을 출력한다

--- src/main/java/subway/console/Input.java
+++ src/main/java/subway/console/Input.java
@@ -1,9 +1,14 @@
 package subway.console;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import subway.domain.Station;
+import subway.domain.StationRepository;
 
 public class Input {
     private static final String MESSAGE_SELECT_MENU = "\n## 원하는 기능을 선택하세요.";
+    private static final String ADD_START_STATION = "\n## 출발역을 입력하세요.";
+    private static final String ADD_END_STATION = "\n## 도착역을 입력하세요.";
     static Scanner scanner;
 
     public Input(Scanner scanner) {
@@ -18,5 +23,14 @@ public class Input {
     public static String getPathCriteriaScreenInput() {
         System.out.println(MESSAGE_SELECT_MENU);
         return scanner.nextLine();
+    }
+
+    public static ArrayList<Station> getStationsToFindPath() {
+        ArrayList<Station> stations = new ArrayList<>();
+        System.out.println(ADD_START_STATION);
+        stations.add(StationRepository.getStation(scanner.nextLine()));
+        System.out.println(ADD_END_STATION);
+        stations.add(StationRepository.getStation(scanner.nextLine()));
+        return stations;
     }
 }
