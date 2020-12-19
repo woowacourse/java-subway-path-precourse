41ecb0e3190550a2291abb3755ce6f8b0e124afe
park <msmn1729@gmail.com>
2020-12-19 13:47:18 +0900

    feat: 메인 클래스에서 초기화와 프로그램 실행하는 기능 구현

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,13 @@
 package subway;
 
+import controller.SubwayController;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO: 프로그램 구현
+        new MakeSubway().initSubway();
+        new SubwayController(scanner);
     }
 }
