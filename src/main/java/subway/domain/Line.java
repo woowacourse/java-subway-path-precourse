bc7d8f317d64150ab5406479d1edb658b4a46caa
Gomding <backchida@gmail.com>
2020-12-19 14:11:01 +0900

    feat : 노선 안에 구간을 추가하는 기능 추가

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,5 +1,10 @@
 package subway.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
 public class Line {
     private String name;
 
@@ -12,4 +17,18 @@ public class Line {
     }
 
     // 추가 기능 구현
+
+    private List<Station> stationList = new ArrayList<>();
+
+    public List<Station> stations() {
+        return Collections.unmodifiableList(stationList);
+    }
+
+    public void addStation(Station station) {
+        stationList.add(station);
+    }
+
+    public boolean deleteStation(String name) {
+        return stationList.removeIf(station -> Objects.equals(station.getName(), name));
+    }
 }
