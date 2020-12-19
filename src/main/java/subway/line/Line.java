d4fe318e0893f5af485d916e884eb17d48c9a2ec
KJunseo <max9106@naver.com>
2020-12-19 17:22:50 +0900

    refactor: 사용하지 않는 생성자 삭제

--- src/main/java/subway/line/Line.java
+++ src/main/java/subway/line/Line.java
@@ -6,11 +6,6 @@ public class Line {
     private String name;
     private Sections sections;
 
-//    public Line(String name) {
-//        this.name = name;
-//        this.sections = new Sections();
-//    }
-
     public Line(String name, Sections sections) {
         this.name = name;
         this.sections = sections;
