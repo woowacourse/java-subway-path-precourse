d92414a2ff6974dae77e38fbdd8d512f668dfcad
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-12-19 17:30:58 +0900

    feat: InputView 유저의 경로 조회 요청 입력 기능 구현

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -1,5 +1,6 @@
 package subway.view;
 
+import subway.dto.PathRequestDto;
 import subway.type.FunctionType;
 import subway.type.ManagementType;
 
@@ -7,6 +8,8 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String INPUT_MANAGEMENT_OR_FUNCTION_TYPE = "\n## 원하는 기능을 선택하세요.";
+    private static final String INPUT_FIRST_STATION_NAME = "\n## 출발역을 입력하세요.";
+    private static final String INPUT_LAST_STATION_NAME = "\n## 도착역을 입력하세요.";
 
     private final Scanner scanner;
 
@@ -34,5 +37,13 @@ public class InputView {
             OutputView.printErrorMessage(runtimeException);
             return inputFunctionType(managementType);
         }
+    }
+
+    public PathRequestDto inputPathRequest() {
+        System.out.println(INPUT_FIRST_STATION_NAME);
+        String firstStationName = scanner.nextLine();
+        System.out.println(INPUT_LAST_STATION_NAME);
+        String lastStationName = scanner.nextLine();
+        return new PathRequestDto(firstStationName, lastStationName);
     }
 }
