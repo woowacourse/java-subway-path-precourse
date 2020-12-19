cf5d8780420a89d82e28e9af103e72e1d4824e3b
pika96 <52442949+pika96@users.noreply.github.com>
2020-12-19 17:31:39 +0900

    feat: 에러 코드 상수 추가

--- src/main/java/subway/Constants.java
+++ src/main/java/subway/Constants.java
@@ -5,4 +5,7 @@ public class Constants {
     public static final String FUNCTION_TWO = "2";
     public static final String FUNCTION_Q = "Q";
     public static final String FUNCTION_B = "B";
+    public static final String TIME_COST = "TIME";
+    public static final String DISTANCE_COST = "DISTANCE";
+    public static final int ERROR_CODE = -1;
 }
