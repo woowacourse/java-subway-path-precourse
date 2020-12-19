30719d0f22d9a01201176ed9d6b58374c6410785
Seonho Kim <zxzx235@naver.com>
2020-12-19 13:53:05 +0900

    추가: 상수 문자열 추가

---
+++ src/main/java/subway/domain/Constant.java
@@ -0,0 +1,13 @@
+package subway.domain;
+
+public class Constant {
+    static final String HEAD_HASH = "## ";
+    static final String HEAD_INFO = "[INFO] ";
+    static final String HEAD_ERROR = "[ERROR] ";
+    static final String DIVIDER = "---";
+
+    public static final String CHOOSE_FUNCTION = "원하는 기능을 선택하세요.";
+    static final String MAIN_MENU_TITLE = "메인 화면";
+
+    static final String IS_IN_MENU = "존재하지 않는 항목입니다.";
+}
