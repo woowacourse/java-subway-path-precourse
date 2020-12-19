e2a749c026ac728dc24723d3d65df666f5bc70f6
amy choi <cgc8016@ewhain.net>
2020-12-19 15:05:53 +0900

    feat(Line): add another generator and addNextStation method

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,15 +1,43 @@
 package subway.domain;
 
+import static subway.domain.StationRepository.getStationByName;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+
 public class Line {
-    private String name;
+	private final List<Station> stations = new ArrayList<>();
+	private final WeightedMultigraph<String, DefaultWeightedEdge> timeGraph = new WeightedMultigraph(
+		DefaultWeightedEdge.class);
+	private final WeightedMultigraph<String, DefaultWeightedEdge> distanceGraph = new WeightedMultigraph(
+		DefaultWeightedEdge.class);
+	private final String name;
 
-    public Line(String name) {
-        this.name = name;
-    }
+	public Line(String name) {
+		this.name = name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public Line(String name, String upwardTerminalName) {
+		this.name = name;
+		stations.add(getStationByName(upwardTerminalName));
+		timeGraph.addVertex(upwardTerminalName);
+		distanceGraph.addVertex(upwardTerminalName);
+	}
 
-    // 추가 기능 구현
+	public String getName() {
+		return name;
+	}
+
+	public void addNextStation(String beforeStationName, String newStationName, int distanceWeight,
+		int timeWeight) {
+		stations.add(getStationByName(newStationName));
+
+		distanceGraph.addVertex(newStationName);
+		distanceGraph.setEdgeWeight(distanceGraph.addEdge(beforeStationName, newStationName),
+			distanceWeight);
+		timeGraph.addVertex(newStationName);
+		timeGraph.setEdgeWeight(timeGraph.addEdge(beforeStationName, newStationName), timeWeight);
+	}
 }
