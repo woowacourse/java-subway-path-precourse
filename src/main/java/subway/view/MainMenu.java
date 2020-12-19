fd4e7224a61e036b9cfc508d999545531f7e6030
Gomding <backchida@gmail.com>
2020-12-19 15:48:47 +0900

    feat : 메인메뉴 화면을 출력하는 기능과 사용자의 입력을 받는 기능 추가

--- src/main/java/subway/view/MainMenu.java
+++ src/main/java/subway/view/MainMenu.java
@@ -6,6 +6,9 @@ public class MainMenu {
     private final String MAIN_TITLE = "## 메인 화면";
     private final String MAINMENU_1 = "1. 경로 조회";
     private final String QUIT_APP = "Q. 종료";
+    private final String ENTER = "\n";
+    private final String CHOICE_MENU = "## 원하는 기능을 선택하세요.";
+    private final String QUIT_MESSAGE = "프로그램을 종료합니다.";
 
     private Scanner scanner;
 
@@ -14,6 +17,31 @@ public class MainMenu {
     }
 
     public void startMainMENU() {
+        outputMainMenu();
+        String input = scanner.nextLine();
+        System.out.println();
+        inputMenu(input);
+    }
 
+    private void outputMainMenu() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(MAIN_TITLE).append(ENTER)
+                .append(MAINMENU_1).append(ENTER)
+                .append(QUIT_APP).append(ENTER)
+                .append(ENTER)
+                .append(CHOICE_MENU);
+    }
+
+    private void inputMenu(String input) {
+        while (true) {
+            if (input.equals("1")) {
+
+            }
+            if (input.equals("Q")) {
+                System.out.println();
+                System.out.println(QUIT_MESSAGE);
+                return;
+            }
+        }
     }
 }
