aba969068adafe44daaf02e7c461b502f5fadb20
kmj990929 <kmj990929@dgist.ac.kr>
2020-12-19 15:43:32 +0900

    feat: 초기 노선 정보로 역 연결 기능 구현

--- src/main/java/subway/controller/Initializer.java
+++ src/main/java/subway/controller/Initializer.java
@@ -5,10 +5,13 @@ import subway.domain.StationRepository;
 import subway.domain.Line;
 import subway.domain.LineRepository;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class Initializer {
+    private static final int ORDER_CONSTANT = 1;
 
     private static final String station1 = "교대역";
     private static final String station2 = "강남역";
@@ -24,28 +27,47 @@ public class Initializer {
 
     private static final List<String> initStations = Arrays
         .asList(station1, station2, station3, station4, station5, station6, station7);
-    private static final List<String> initLines = Arrays
-        .asList(line1, line2, line3);
+    private static final List<String> line1Stations = Arrays.asList(station1, station2, station3);
+    private static final List<String> line2Stations = Arrays.asList(station1, station4, station5, station7);
+    private static final List<String> line3Stations = Arrays.asList(station2, station5, station6);
+
+    private static final HashMap<String, List<String>> initLines = new HashMap<String, List<String>>() {{
+        put(line1, line1Stations);
+        put(line2, line2Stations);
+        put(line3, line3Stations);
+    }};
 
     private Initializer() {
     }
 
     public static void set() {
-        makeStations();
-        makeLines();
+        HashMap<String,Station> stations = makeStations();
+        List<String> lineNameList = new ArrayList<String>(initLines.keySet());
+        for (int i = 0; i < lineNameList.size(); i++) {
+            String lineName = lineNameList.get(i);
+            makeLine(lineName);
+        }
     }
 
-    private static void makeStations() {
-        for (String stationName : initStations) {
+    private static HashMap<String, Station> makeStations() {
+        HashMap<String, Station> stations = new HashMap<String, Station>();
+        for (int i = 0; i < initStations.size(); i++) {
+            String stationName = initStations.get(i);
             Station station = new Station(stationName);
             StationRepository.addStation(station);
+            stations.put(stationName, station);
         }
+        return stations;
     }
 
-    private static void makeLines() {
-        for (String lineName : initLines) {
-            Line line = new Line(lineName);
-            LineRepository.addLine(line);
+    private static void makeLine(String lineName) {
+        Line line = new Line(lineName);
+        List<String> stationList = initLines.get(lineName);
+        for (int i = 0; i < stationList.size(); i++) {
+            String stationName = stationList.get(i);
+            int order = i + ORDER_CONSTANT;
+            line.addStationByName(stationName, order);
         }
+        LineRepository.addLine(line);
     }
 }
--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,7 +1,13 @@
 package subway.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Line {
+    private static final int ORDER_CONSTANT = 1;
+
     private String name;
+    private List<Station> stations = new ArrayList<>();
 
     public Line(String name) {
         this.name = name;
@@ -11,5 +17,17 @@ public class Line {
         return name;
     }
 
-    // 추가 기능 구현
+    private void addStation(Station station, int order) {
+        int indexOrder = order - ORDER_CONSTANT;
+        stations.add(indexOrder,station);
+    }
+
+    public void addStationByName(String stationName, int order) {
+        for (int i = 0; i < StationRepository.stations().size(); i++) {
+            Station station = StationRepository.stations().get(i);
+            if (station.getName().equals(stationName)) {
+                addStation(station, order);
+            }
+        }
+    }
 }
