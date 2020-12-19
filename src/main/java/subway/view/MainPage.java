cce10efa742e8df18bd60fcf20453b8142fe5469
perenok <perenoke@gmail.com>
2020-12-19 13:49:04 +0900

    feat(MainPage): add printMainPage
    메인 화면 출력 담당 클래스 생성
    printMainPage 메소드를 통해 메인화면 출력

---
+++ src/main/java/subway/view/MainPage.java
@@ -0,0 +1,9 @@
+package subway.view;
+
+public class MainPage {
+    public void printMainPage(){
+        System.out.println("## 메인 화면");
+        System.out.println("1. 경로 조회");
+        System.out.println("Q. 종료");
+    }
+}
