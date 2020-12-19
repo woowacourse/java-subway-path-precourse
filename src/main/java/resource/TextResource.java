089a5c8a7b38d96242f08bdbbb34a274fda0659a
DWL5 <ssop6403@gmail.com>
2020-12-19 14:33:03 +0900

    feat: 지하철 역 등록 기능 추가

---
+++ src/main/java/resource/TextResource.java
@@ -0,0 +1,9 @@
+package resource;
+
+public class TextResource {
+    public static final String PREFIX_ERROR = "\n[ERROR]";
+
+    public static final String ERROR_STATION_NAME_DUPLICATED = PREFIX_ERROR
+            + "이미 해당 지하철역이 존재 합니다. 등록 할 수 없습니다.";
+
+}
--- src/main/java/subway/domain/StationRepository.java
+++ src/main/java/subway/domain/StationRepository.java
@@ -5,6 +5,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
+import static resource.TextResource.ERROR_STATION_NAME_DUPLICATED;
+
 public class StationRepository {
     private static final List<Station> stations = new ArrayList<>();
 
@@ -13,6 +15,9 @@ public class StationRepository {
     }
 
     public static void addStation(Station station) {
+        if (hasStation(station.getName())) {
+            throw new IllegalArgumentException(ERROR_STATION_NAME_DUPLICATED);
+        }
         stations.add(station);
     }
 
@@ -22,5 +27,23 @@ public class StationRepository {
 
     public static void deleteAll() {
         stations.clear();
+    }
+
+    public static Station getStationByName(String stationName) {
+        for (Station station : stations) {
+            if (station.getName().equals(stationName)) {
+                return station;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public static Boolean hasStation(String stationName) {
+        for (Station station : stations) {
+            if (station.getName().equals(stationName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
