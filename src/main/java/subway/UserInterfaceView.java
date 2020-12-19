293691d85eb161462ddbe43b997cd21ec8ea6536
mungto <daum7766@naver.com>
2020-12-19 13:23:44 +0900

    feat(UserInterfaceView): 유저화면클래스 생성

---
+++ src/main/java/subway/UserInterfaceView.java
@@ -0,0 +1,11 @@
+package subway;
+
+import java.util.Scanner;
+
+public class UserInterfaceView {
+    private static Scanner scanner;
+
+    public UserInterfaceView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+}
