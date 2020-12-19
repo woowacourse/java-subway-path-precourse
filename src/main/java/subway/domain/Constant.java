6abd890f4dfaee080ed19f597c0565d652e4320b
Seonho Kim <zxzx235@naver.com>
2020-12-19 14:07:04 +0900

    추가: 경로 기준 메뉴 항목 리스트 추가

--- src/main/java/subway/domain/Constant.java
+++ src/main/java/subway/domain/Constant.java
@@ -6,10 +6,17 @@ import java.util.List;
 
 public class Constant {
     static final String[] mainMenuItems = {"1. 경로 조회", "Q. 종료"};
+    static final String[] routeSelectItems = {"1. 최단 거리", "2. 최소 시간", "Q. 종료"};
+
     private static final List<String> mainMenuItemList = Arrays.asList(mainMenuItems);
+    private static final List<String> routeSelectItemList = Arrays.asList(routeSelectItems);
 
     public static List<String> mainMenuItemList() {
         return Collections.unmodifiableList(mainMenuItemList);
+    }
+
+    public static List<String> routeSelectItemList() {
+        return Collections.unmodifiableList(routeSelectItemList);
     }
 
     static final String HEAD_HASH = "## ";
