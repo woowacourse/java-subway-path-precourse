7310a1240d17ac040d90122afce66dbdb0c50da8
hybeom0720 <hybeom@gmail.com>
2020-12-19 13:41:59 +0900

    feat(Line) : 노선 생성시 유효성 체크 메서드들 추가
    추가 :
    - 노선 생성시에 필요한 이름 유효성 체크
    - 노선에 역 추가시에 필요한 중복 유효성 체크

--- src/main/java/subway/domain/constants/DomainErrorMessage.java
+++ src/main/java/subway/domain/constants/DomainErrorMessage.java
@@ -1,6 +1,7 @@
 package subway.domain.constants;
 
 public class DomainErrorMessage {
+    public static final String LINE_FORMAT = "[ERROR] 노선의 이름은 선으로 끝나야 합니다.";
     public static final String STATION_FORMAT = "[ERROR] 역의 이름은 역으로 끝나야합니다.";
     public static final String OVERLAP_STATION = "[ERROR] 이미 등록된 역입니다.";
 }
