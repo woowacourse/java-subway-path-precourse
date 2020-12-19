59f22a9c0dc150f621f34709eec8232964c61231
mungto <daum7766@naver.com>
2020-12-19 14:08:53 +0900

    refactor(UserInterfaceView): printMainViewAndReturnIsEnd 함수 내용 수정
    잘못된 선택시 에러문구 출력기능 추가, Q선택시 종료기능 추가

--- src/main/java/subway/UserInterfaceView.java
+++ src/main/java/subway/UserInterfaceView.java
@@ -1,6 +1,15 @@
 package subway;
 
+import java.util.Arrays;
+
 public class UserInterfaceView {
+
+    private static final String ONE = "1";
+    private static final String TWO = "2";
+    private static final String Q = "Q";
+    private static final String B = "B";
+    private static final String[] MAIN_CHOICE_LIST = {ONE, Q};
+    private static final String[] ROUTE_SEARCH_CHOICE_LIST = {ONE, TWO, B};
 
     public UserInterfaceView() {
     }
@@ -15,6 +24,15 @@ public class UserInterfaceView {
 
     private boolean printMainViewAndReturnIsEnd() {
         Output.printMainView();
+        String userInput = Input.getUserInput();
+        if (!Arrays.asList(MAIN_CHOICE_LIST).contains((userInput))) {
+            Output.printChoiceErrorMessage();
+            return false;
+        }
+        if (userInput == Q) {
+            return true;
+        }
+        // TODO - 다음 실행할 함수 호출하기
         return false;
     }
 
