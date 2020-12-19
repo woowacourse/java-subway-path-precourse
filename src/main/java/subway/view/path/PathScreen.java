66216d92bfe874f52e538641ca2a488023b7d0a6
operasystem6 <operasystem6@naver.com>
2020-12-19 17:39:16 +0900

    refactor: 경로 view key에 맞는 메뉴 불러오기 추가

--- src/main/java/subway/view/path/PathScreen.java
+++ src/main/java/subway/view/path/PathScreen.java
@@ -6,20 +6,20 @@ import subway.view.OutputView;
 import subway.view.TextCollection;
 
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class PathScreen {
 
-    public static String selectMenu() {
+    public static Boolean selectMenu() {
         showMenu();
         String input = InputView.inputFunction();
         if (!PathMenu.isValidInput(input)) {
             throw new SubwayException(TextCollection.WRONG_MENU_INPUT_MESSAGE);
         }
-        return input;
+        PathMenu.findMenuByKey(input).request();
+        return true;
     }
 
-    public static void showMenu() {
+    private static void showMenu() {
         OutputView.printQuestion(TextCollection.SEARCH_MANAGEMENT_MESSAGE);
         Arrays.stream(PathMenu.values()).forEach(menu -> {
             System.out.println(menu.getKey() + ". " + menu.getTitle());
