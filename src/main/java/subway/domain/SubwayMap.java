26c476102ce216f17552ec9cd0dcfa068167997f
yunjung <cairofaith@gmail.com>
2020-12-19 15:54:48 +0900

    feat(SubwayMap): 입력된 역과 경로 정보를 담은 graph 객체 구현
    - 최단 거리 및 최소 시간 검색을 위한 경로 그래프 추가
    - 역을 vertex로 연결정보를 edge로 시간이나 거리를 weight로 추가
    - 거리와 시간 그래프를 따로 생성함

---
+++ src/main/java/subway/domain/SubwayMap.java
@@ -0,0 +1,23 @@
+package subway.domain;
+
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+
+public class SubwayMap {
+	static WeightedMultigraph<Station, DefaultWeightedEdge> pathWithDistance
+		= new WeightedMultigraph(DefaultWeightedEdge.class);
+	static WeightedMultigraph<Station, DefaultWeightedEdge> pathWithTime
+		= new WeightedMultigraph(DefaultWeightedEdge.class);
+	
+	public static void makePathWithDistance(Station fromStation, Station toStation, int distance) {
+		pathWithDistance.addVertex(fromStation);
+		pathWithDistance.addVertex(toStation);
+		pathWithDistance.setEdgeWeight(pathWithDistance.addEdge(fromStation, toStation), distance);
+	}
+	
+	public static void makePathWithTime(Station fromStation, Station toStation, int time) {
+		pathWithTime.addVertex(fromStation);
+		pathWithTime.addVertex(toStation);
+		pathWithTime.setEdgeWeight(pathWithTime.addEdge(fromStation, toStation), time);
+	}
+}
