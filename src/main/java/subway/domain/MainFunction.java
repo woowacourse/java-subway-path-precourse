fccecdc5bc0d3f9adec699ce65b623f04fe54e6b
jaeseongDev <qkrwotjd1445@naver.com>
2020-12-19 13:24:26 +0900

    feat(MainFunction) : 메인 화면에서 조회되는 기능들을 관리하는 MainFunction 클래스 추가

---
+++ src/main/java/subway/domain/MainFunction.java
@@ -0,0 +1,31 @@
+package subway.domain;
+
+import java.util.Arrays;
+
+public enum MainFunction {
+    ORDER("1") {
+        @Override
+        public void operate() {
+        }
+    },
+    QUIT("Q") {
+        @Override
+        public void operate() {
+        }
+    };
+
+    private String number;
+
+    MainFunction(String number) {
+        this.number = number;
+    }
+
+    public static MainFunction getMainFunctionByNumber(String inputNumber) {
+        return Arrays.stream(MainFunction.values())
+            .filter(function -> function.number.equals(inputNumber))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("[ERROR] 선택할 수 없는 기능입니다."));
+    }
+    
+    public abstract void operate();
+}
