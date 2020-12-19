f79355f60a43944787c8e44460512768a85fb2b2
mungto <daum7766@naver.com>
2020-12-19 16:47:31 +0900

    refactor(StationService): 정보추가시 역방향정보 삭제

--- src/main/java/subway/StationService.java
+++ src/main/java/subway/StationService.java
@@ -14,6 +14,5 @@ public class StationService {
         Station station = StationRepository.getStation(stationName);
         Station connectStation = StationRepository.getStation(connectStationName);
         station.addConnectData(connectStation, distance, time);
-        connectStation.addConnectData(station, distance, time);
     }
 }
