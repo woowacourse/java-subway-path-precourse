2c8485b48a9b95b52c5b564da83d6fa741d33270
opdshe <opdshe@naver.com>
2020-12-19 15:19:58 +0900

    feat: 출발역을 기준으로 해당 출발역에서 갈 수 있는 경로를 조회할 수 있어야 한다

--- src/main/java/subway/domain/path/Path.java
+++ src/main/java/subway/domain/path/Path.java
@@ -4,17 +4,21 @@ import subway.domain.line.Line;
 import subway.domain.station.Station;
 
 public class Path {
-    private final Station prevStation;
-    private final Station nextStation;
+    private final Station source;
+    private final Station dest;
     private final Line line;
     private final int distance;
     private final int time;
 
-    public Path(Station prevStation, Station nextStation, Line line, int distance, int time) {
-        this.prevStation = prevStation;
-        this.nextStation = nextStation;
+    public Path(Station source, Station dest, Line line, int distance, int time) {
+        this.source = source;
+        this.dest = dest;
         this.line = line;
         this.distance = distance;
         this.time = time;
+    }
+
+    public Station getSource() {
+        return source;
     }
 }
--- src/main/java/subway/domain/path/PathRepository.java
+++ src/main/java/subway/domain/path/PathRepository.java
@@ -1,12 +1,21 @@
 package subway.domain.path;
 
+import subway.domain.station.Station;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PathRepository {
     private static List<Path> paths = new ArrayList<>();
 
     public static void addPath(Path path) {
         paths.add(path);
+    }
+
+    public static List<Path> findBySource(Station source) {
+        return paths.stream()
+                .filter(path -> path.getSource().equals(source))
+                .collect(Collectors.toList());
     }
 }
