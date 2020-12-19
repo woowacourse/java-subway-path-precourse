46397a3ed738da5d1972aff94e88dbfeecb33c6c
Minjeong <mathmjseo@naver.com>
2020-12-19 16:23:53 +0900

    feat: jgrapht를 이용해 시간과 거리로 나눠 초기화

--- src/main/java/subway/SubwayInitializer.java
+++ src/main/java/subway/SubwayInitializer.java
@@ -12,30 +12,19 @@ public class SubwayInitializer {
     private static final String SLASH = "/";
     private static final int LINE_NAME = 0;
     private static final int FIRST = 1;
+    private static final String DISTANCE = "거리";
+    private static List<String> stations;
 
     public static void init() {
-        List<String> stations = Arrays.asList("교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역");
+        stations = Arrays.asList("교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역");
         List<String> lineAndSection = Arrays.asList(
                 "2호선/교대역/강남역/역삼역",
                 "3호선/교대역/남부터미널역/양재역/매봉역",
                 "신분당선/강남역/양재역/양재시민의숲역"
         );
 
-
-        /**
-         *    - 2호선: 교대역 - ( 2km / 3분 ) - 강남역 - ( 2km / 3분 ) - 역삼역
-         *    - 3호선: 교대역 - ( 3km / 2분 ) - 남부터미널역 - ( 6km / 5분 ) - 양재역 - ( 1km / 1분 ) - 매봉역
-         *    - 신분당선: 강남역 - ( 2km / 8분 ) - 양재역 - ( 10km / 3분 ) - 양재시민의숲역
-         */
         makeStations(stations);
-
-//        graph.setEdgeWeight(graph.addEdge("v1", "v2"), 2);
-//        graph.setEdgeWeight(graph.addEdge("v2", "v3"), 2);
-//        graph.setEdgeWeight(graph.addEdge("v1", "v3"), 100);
-
-//        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
-//        List<String> shortestPath = dijkstraShortestPath.getPath("v2", "v1").getVertexList();
-
+        separateLineBySlash(lineAndSection);
     }
 
     private static void makeStations(List<String> stations) {
@@ -59,9 +48,40 @@ public class SubwayInitializer {
         ));
     }
 
-    private static void makeWeightedGraph(List<String> stations, List<String> sectionInfo) {
-        WeightedMultigraph<String, DefaultWeightedEdge> subwayWeightedGraph = new WeightedMultigraph(DefaultWeightedEdge.class);
-        stations.stream().forEach(subwayWeightedGraph::addVertex);
+    public static WeightedMultigraph<String, DefaultWeightedEdge> initGraph(String type) {
+        if (type.equals(DISTANCE)) {
+            return makeDistanceWeightedGraph();
+        }
+        return makeTimeWeightedGraph();
     }
 
+    private static WeightedMultigraph<String, DefaultWeightedEdge> makeDistanceWeightedGraph() {
+        WeightedMultigraph<String, DefaultWeightedEdge> distanceWeightedGraph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        stations.stream().forEach(distanceWeightedGraph::addVertex);
+
+        distanceWeightedGraph.setEdgeWeight(distanceWeightedGraph.addEdge("교대역", "강남역"), 2);
+        distanceWeightedGraph.setEdgeWeight(distanceWeightedGraph.addEdge("강남역", "역삼역"), 2);
+        distanceWeightedGraph.setEdgeWeight(distanceWeightedGraph.addEdge("교대역", "남부터미널역"), 3);
+        distanceWeightedGraph.setEdgeWeight(distanceWeightedGraph.addEdge("남부터미널역", "양재역"), 6);
+        distanceWeightedGraph.setEdgeWeight(distanceWeightedGraph.addEdge("양재역", "매봉역"), 1);
+        distanceWeightedGraph.setEdgeWeight(distanceWeightedGraph.addEdge("강남역", "양재역"), 2);
+        distanceWeightedGraph.setEdgeWeight(distanceWeightedGraph.addEdge("양재역", "양재시민의숲역"), 10);
+
+        return distanceWeightedGraph;
+    }
+
+    private static WeightedMultigraph<String, DefaultWeightedEdge> makeTimeWeightedGraph() {
+        WeightedMultigraph<String, DefaultWeightedEdge> timeWeightedGraph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        stations.stream().forEach(timeWeightedGraph::addVertex);
+
+        timeWeightedGraph.setEdgeWeight(timeWeightedGraph.addEdge("교대역", "강남역"), 3);
+        timeWeightedGraph.setEdgeWeight(timeWeightedGraph.addEdge("강남역", "역삼역"), 3);
+        timeWeightedGraph.setEdgeWeight(timeWeightedGraph.addEdge("교대역", "남부터미널역"), 2);
+        timeWeightedGraph.setEdgeWeight(timeWeightedGraph.addEdge("남부터미널역", "양재역"), 5);
+        timeWeightedGraph.setEdgeWeight(timeWeightedGraph.addEdge("양재역", "매봉역"), 1);
+        timeWeightedGraph.setEdgeWeight(timeWeightedGraph.addEdge("강남역", "양재역"), 8);
+        timeWeightedGraph.setEdgeWeight(timeWeightedGraph.addEdge("양재역", "양재시민의숲역"), 3);
+
+        return timeWeightedGraph;
+    }
 }
