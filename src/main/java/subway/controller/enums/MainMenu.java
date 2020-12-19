346b49bac922a2286124937cc33aa8713936b51d
Beenie93 <seongbeen93@naver.com>
2020-12-19 16:07:29 +0900

    feat(controller-enums): 메인 화면의 메뉴 선택 enum 구현

---
+++ src/main/java/subway/controller/enums/MainMenu.java
@@ -0,0 +1,36 @@
+package subway.controller.enums;
+
+import subway.controller.PathController;
+import subway.view.OutputView;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public enum MainMenu {
+    FIND_PATH("1", PathController.getInstance(new Scanner(System.in))::run),
+    EXIT("Q", OutputView.getInstance()::printExit);
+
+    private String type;
+    private Runnable expression;
+    private static final String ERROR_INVALID_MENU = "[ERROR] 선택할 수 없는 기능입니다.";
+
+    MainMenu(String type, Runnable expression) {
+        this.type = type;
+        this.expression = expression;
+    }
+
+    public void run() {
+        this.expression.run();
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public static MainMenu findMainMenu(String selectedMenu) {
+        return Arrays.stream(MainMenu.values())
+                .filter(mainMenu -> mainMenu.type.equals(selectedMenu))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(ERROR_INVALID_MENU));
+    }
+}
