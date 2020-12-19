fc73a53d593fa4a0f6eff0310043ed49b55b7822
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2020-12-19 17:56:16 +0900

    feat: 출력 화면 구현

--- src/main/java/subway/Views/OutputView.java
+++ src/main/java/subway/Views/OutputView.java
@@ -1,15 +1,23 @@
 package subway.Views;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class OutputView {
 
     private final static String MAINSCREEN = "## 메인 화면";
     private final static String ROUTESEARCHSELECTION = "1. 경로 조회";
     private final static String PROGRAMEXIT = "Q. 종료";
-
     private final static String ROUTESCREEN = "## 경로 기준";
     private final static String MINDISTANCEROUTE = "1. 최단 거리";
     private final static String MINTIMEROUTE = "2. 최소 시간";
     private final static String BACKOPTION = "B. 돌아가기";
+    private final static String RESULTSCREEN = "## 조회 결과";
+    private final static String INFORMATION = "[INFO] ";
+    private final static String SPLITLINE = "---";
+    private final static String TOTALDISTANCE = "총 거리: %dkm";
+    private final static String TOTALTIME = "총 소요 시간: %d분";
+
 
     protected static void printMainSelection() {
         System.out.println(MAINSCREEN);
@@ -24,5 +32,16 @@ public class OutputView {
         System.out.println(MINTIMEROUTE);
         System.out.println(BACKOPTION);
         System.out.println();
+    }
+
+    protected static void printResultRoute(List<String> resultList) {
+        System.out.println(RESULTSCREEN);
+        System.out.println(INFORMATION + SPLITLINE);
+//        System.out.println(String.format(INFORMATION + TOTALDISTANCE, totalDistance));
+//        System.out.println(String.format(INFORMATION + TOTALTIME, totalTime));
+//        System.out.println(INFORMATION + SPLITLINE);
+        for (String result : resultList) {
+            System.out.println(INFORMATION + result);
+        }
     }
 }
