37f4127e4d95f792d072a425cf3645257e8b2e44
mungto <daum7766@naver.com>
2020-12-19 14:06:32 +0900

    feat(Output): 잘못선택시 에러출력해주는 함수 생성

--- src/main/java/subway/Output.java
+++ src/main/java/subway/Output.java
@@ -5,11 +5,17 @@ public class Output {
     private static final String ROUTE_SEARCH = "1. 경로 조회";
     private static final String QUIT = "Q. 종료";
     private static final String[] MAIN_LIST = {MAIN_VIEW, ROUTE_SEARCH, QUIT};
+    private static final String ERROR = "[ERROR] ";
+    private static final String BAD_CHOICE = "목록에 없는것을 선택하셨습니다.";
 
     public static void printMainView() {
         for (int i = 0; i < MAIN_LIST.length; i++) {
             System.out.println(MAIN_LIST[i]);
         }
         System.out.println();
+    }
+
+    public static void printChoiceErrorMessage() {
+        System.out.println(ERROR + BAD_CHOICE);
     }
 }
