c628b5a56ab099c163c6cd1f9a04fcabe61d529c
jaeseongDev <qkrwotjd1445@naver.com>
2020-12-19 17:48:47 +0900

    feat : Section에 대한 유효성 검증 추가

--- src/main/java/subway/domain/section/Section.java
+++ src/main/java/subway/domain/section/Section.java
@@ -5,12 +5,15 @@ import java.util.List;
 import subway.domain.station.Station;
 
 public class Section {
-
-    List<Station> stations;
+    private static final int MIN_DISTANCE = 1;
+    private static final int MIN_TIME = 1;
+    private List<Station> stations;
     private int distance;
     private int time; // 단위 : 분
 
     public Section(int distance, int time, List<Station> stations) {
+        validateDistance(distance);
+        validateTime(time);
         this.distance = distance;
         this.time = time;
         this.stations = new ArrayList<>(stations);
@@ -26,5 +29,17 @@ public class Section {
 
     public boolean contains(Station station, Station otherStation) {
         return stations.contains(station) && stations.contains(otherStation);
+    }
+
+    public void validateDistance(int distance) {
+        if (distance < MIN_DISTANCE) {
+            throw new IllegalArgumentException("[ERROR] 거리는 양의 정수여야 합니다.");
+        }
+    }
+
+    public void validateTime(int time) {
+        if (time < MIN_TIME) {
+            throw new IllegalArgumentException("[ERROR] 시간은 양의 정수여야 합니다.");
+        }
     }
 }
