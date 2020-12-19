16e1199d0c40517f054ce52dc81dae44588ce725
sounmind <sounmind777@gmail.com>
2020-12-19 16:53:54 +0900

    feat(DataInitialization): 주어진 노선, 역 데이터, 그래프 초기화

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -3,11 +3,13 @@ package subway;
 import java.util.Scanner;
 import subway.console.Input;
 import subway.console.MainScreen;
+import subway.utils.DataInitialization;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
+        new DataInitialization();
         new Input(scanner);
         MainScreen.startProcess();
     }
