ac9432e8a1a0e6f49462f416a09b3f13971eb91e
Beenie93 <seongbeen93@naver.com>
2020-12-19 14:17:57 +0900

    refactor(domain-Line): 초기 설정에 필요한 메소드 추가 및 경로에 관한 정보 데이터 추가

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,15 +1,38 @@
 package subway.domain;
 
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+
 public class Line {
     private String name;
+    private WeightedMultigraph<Station, DefaultWeightedEdge> distancePath;
+    private WeightedMultigraph<Station, DefaultWeightedEdge> timePath;
 
     public Line(String name) {
         this.name = name;
+        this.distancePath = new WeightedMultigraph(DefaultWeightedEdge.class);
+        this.timePath = new WeightedMultigraph(DefaultWeightedEdge.class);
     }
 
     public String getName() {
         return name;
     }
 
-    // 추가 기능 구현
+    public void addStation(Station station) {
+        this.distancePath.addVertex(station);
+        this.timePath.addVertex(station);
+    }
+
+    public void addPathInformation(Station start, Station end, int distance, int time) {
+        this.distancePath.setEdgeWeight(this.distancePath.addEdge(start, end), distance);
+        this.timePath.setEdgeWeight(this.timePath.addEdge(start, end), time);
+    }
+
+    public WeightedMultigraph<Station, DefaultWeightedEdge> getDistancePath() {
+        return distancePath;
+    }
+
+    public WeightedMultigraph<Station, DefaultWeightedEdge> getTimePath() {
+        return timePath;
+    }
 }
