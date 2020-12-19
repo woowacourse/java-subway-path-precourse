cef4160e7623b97cf49c3e300e87d7fa11ee6136
Minjeong <mathmjseo@naver.com>
2020-12-19 14:25:18 +0900

    feat: 경로 조회 기능 중 사용자에게 입력받는 기능 구현

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -5,8 +5,16 @@ import java.util.Scanner;
 public class InputView {
 
     public static String inputFunctionNumber(Scanner scanner) {
-        scanner = new Scanner(System.in);
         System.out.println("## 원하는 기능을 선택하세요.");
+        return scanner.next();
+    }
+    
+    public static String inputStation(Scanner scanner, String type){
+        return inputStationByType(scanner, type);
+    }
+
+    private static String inputStationByType(Scanner scanner, String type){
+        System.out.printf("## %s역을 입력하세요", type);
         return scanner.next();
     }
 }
