8497e1b50f5a0f88118710cd5b152589c22ac247
thisisyoungbin <thisisyoungbin@gmail.com>
2020-12-19 15:29:09 +0900

    feat(Line): 노선에 역 추가 하는 기능 구현

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,15 +1,23 @@
 package subway.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Line {
     private String name;
+    private List<Station> stations;
 
     public Line(String name) {
         this.name = name;
+        this.stations = new ArrayList<>();
     }
 
     public String getName() {
         return name;
     }
-
     // 추가 기능 구현
+    public void addStations(String... names) {
+        Arrays.stream(names).forEach(name -> stations.add(new Station(name)));
+    }
 }
