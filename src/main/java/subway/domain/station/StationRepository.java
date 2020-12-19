a7cd6bc9c40ec1cfe360b5084aa8f96df3a6662a
jaeseongDev <qkrwotjd1445@naver.com>
2020-12-19 14:10:54 +0900

    feat(StationRepository) : 존재하는 역인지 체크하는 메소드 추가

--- src/main/java/subway/domain/station/StationRepository.java
+++ src/main/java/subway/domain/station/StationRepository.java
@@ -23,4 +23,8 @@ public class StationRepository {
     public static void deleteAll() {
         stations.clear();
     }
+    
+    public static boolean contains(Station station) {
+        return stations.contains(station);
+    }
 }
