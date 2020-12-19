bacc0fd6b97c881381b2768d074ca71d59bea49b
bepoz <kangsy763@naver.com>
2020-12-19 14:43:19 +0900

    feat: 노선에 역을 등록한다

--- src/main/java/subway/util/Initializer.java
+++ src/main/java/subway/util/Initializer.java
@@ -1,7 +1,9 @@
 package subway.util;
 
 import subway.domain.Line;
+import subway.domain.LineRepository;
 import subway.domain.Station;
+import subway.domain.StationRepository;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,10 +12,14 @@ public class Initializer {
     private static final List<String> stations =
             Arrays.asList("교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역");
     private static final List<String> lines = Arrays.asList("2호선", "3호선", "신분당선");
+    private static final List<String> secondLineStations = Arrays.asList("교대역", "강남역", "역삼역");
+    private static final List<String> thirdLineStations = Arrays.asList("교대역", "남부터미널역", "양재역", "매봉역");
+    private static final List<String> shinbundangLineStations = Arrays.asList("강남역", "양재역", "양재시민의숲역");
 
     public void init() {
         createStation();
         createLine();
+        enrollStationOnLine();
     }
 
     private void createStation() {
@@ -24,9 +30,19 @@ public class Initializer {
         lines.forEach(Line::from);
     }
 
-    private void enrolllStationOnLine() {
-        //findLineByName
-        //findLine.addStation(station);
-    }
+    private void enrollStationOnLine() {
+        Line secondLine = LineRepository.findLineByName("2호선");
+        Line thirdLine = LineRepository.findLineByName("3호선");
+        Line shinbundangLine = LineRepository.findLineByName("신분당선");
 
+        secondLineStations.stream()
+                .map(StationRepository::findStationByName)
+                .forEach(secondLine::addStation);
+        thirdLineStations.stream()
+                .map(StationRepository::findStationByName)
+                .forEach(thirdLine::addStation);
+        shinbundangLineStations.stream()
+                .map(StationRepository::findStationByName)
+                .forEach(shinbundangLine::addStation);
+    }
 }
