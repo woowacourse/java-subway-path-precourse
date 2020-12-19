14da704c91c655fb6c24cd3da1d85da85d6a6eb7
Hansaem Woo <feliscatus@hanyang.ac.kr>
2020-12-19 15:37:51 +0900

    refactor(SectionRepositoryTest): fix implementation
    Constants 내의 상수를 쓰도록 코드 수정

--- src/test/java/subway/domain/SectionRepositoryTest.java
+++ src/test/java/subway/domain/SectionRepositoryTest.java
@@ -10,33 +10,22 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import subway.util.Constants;
 
 class SectionRepositoryTest {
-
-    private static final String[] LINE_NAMES = {"2호선", "3호선", "신분당선"};
-    private static final String[][] STATION_NAMES_IN_LINES = {
-        {"교대역", "강남역", "역삼역"},
-        {"교대역", "남부터미널역", "양재역", "매봉역"},
-        {"강남역", "양재역", "양재시민의숲역"}
-    };
-    private static final int[][][] GAPS_IN_LINES = {
-        {{2, 3}, {2, 3}},
-        {{3, 2}, {6, 5}, {1, 1}},
-        {{2, 8}, {10, 3}}
-    };
 
     private static final List<Section> sections = new ArrayList<>();
 
     @BeforeAll
     static void setUpBeforeAll() {
-        for (int i = 0; i < LINE_NAMES.length; i++) {
-            final Line line = new Line(LINE_NAMES[i]);
+        for (int i = 0; i < Constants.INITIAL_LINE_NAMES.length; i++) {
+            final Line line = new Line(Constants.INITIAL_LINE_NAMES[i]);
             final List<Station> stations = new LinkedList<>();
             final List<Gap> gaps = new LinkedList<>();
-            for (String stationName : STATION_NAMES_IN_LINES[i]) {
+            for (String stationName : Constants.INITIAL_STATION_NAMES_IN_LINES[i]) {
                 stations.add(new Station(stationName));
             }
-            for (int[] gap : GAPS_IN_LINES[i]) {
+            for (int[] gap : Constants.INITIAL_GAPS_BETWEEN_STATIONS_OF_SECTIONS[i]) {
                 gaps.add(new Gap(gap[0], gap[1]));
             }
             sections.add(new Section(line, stations, gaps));
@@ -68,12 +57,16 @@ class SectionRepositoryTest {
 
     @Test
     void deleteSectionByLineName_LineNotExist_False() {
-        assertThat(SectionRepository.deleteSectionByLineName(LINE_NAMES[sections.size() - 1]))
-            .isFalse();
+        assertThat(
+            SectionRepository
+                .deleteSectionByLineName(Constants.INITIAL_LINE_NAMES[sections.size() - 1])
+        ).isFalse();
     }
 
     @Test
     void deleteSectionByLineName_ValidName_True() {
-        assertThat(SectionRepository.deleteSectionByLineName(LINE_NAMES[0])).isTrue();
+        assertThat(
+            SectionRepository.deleteSectionByLineName(Constants.INITIAL_LINE_NAMES[0])
+        ).isTrue();
     }
 }
