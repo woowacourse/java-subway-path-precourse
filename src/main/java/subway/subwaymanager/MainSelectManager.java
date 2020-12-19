fa9fd46d43b6e826155434d84010bac39b48f3a1
sjparkk <66293784+sjparkk@users.noreply.github.com>
2020-12-19 16:02:21 +0900

    feat 메인 기능 선택 관리자 생성 MainManger 및 출력 담당 OutputView 생성

---
+++ src/main/java/subway/subwaymanager/MainSelectManager.java
@@ -0,0 +1,12 @@
+package subway.subwaymanager;
+
+import subway.utils.InputView;
+import subway.utils.OutputView;
+
+public class MainSelectManager {
+
+    public static void mainSelectManager() {
+        OutputView.printMainContents();
+        InputView.inputSelect();
+    }
+}
---
+++ src/main/java/subway/utils/OutputView.java
@@ -0,0 +1,12 @@
+package subway.utils;
+
+public class OutputView {
+
+    public static void printMainContents() {
+        System.out.println("메인 화면");
+        System.out.println("1. 경로 조회");
+        System.out.println("Q. 종료");
+        System.out.println();
+        System.out.println("원하는 기능을 선택하세요.");
+    }
+}
