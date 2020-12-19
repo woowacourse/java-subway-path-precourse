1ac27363f6538bb8ac7de1596b01b6fe2c54baa3
eundonglee <cr3306@naver.com>
2020-12-19 17:39:22 +0900

    feat: 존재하지 않는 경로 입력 시 에러 메시지 출력

--- src/main/java/subway/domain/DistanceGraph.java
+++ src/main/java/subway/domain/DistanceGraph.java
@@ -1,6 +1,8 @@
 package subway.domain;
 
 import java.util.List;
+
+import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
@@ -18,10 +20,18 @@ public class DistanceGraph {
     }
     
     public static List<Station> getShortestPath(Station leftEndStation, Station rightEndStation) {
-        DijkstraShortestPath<Station, DefaultWeightedEdge> dijikstraShortestPath = new DijkstraShortestPath<>(
-                distanceGraph);
-        List<Station> shortestPath = dijikstraShortestPath.getPath(leftEndStation, rightEndStation).getVertexList();
+        DijkstraShortestPath<Station, DefaultWeightedEdge> dijikstraShortestPath;
+        GraphPath<Station, DefaultWeightedEdge> shortestPath;
         
-        return shortestPath;
+        if (leftEndStation == rightEndStation) {
+            throw new IllegalArgumentException(DomainConstants.ORIGIN_DEST_EQUAL_ERROR_MESSAGE);
+        }
+        dijikstraShortestPath = new DijkstraShortestPath<>(distanceGraph);
+        shortestPath = dijikstraShortestPath.getPath(leftEndStation, rightEndStation);
+        if (shortestPath == null) {
+            throw new IllegalArgumentException(DomainConstants.NO_PATH_ERROR_MESSAGE);
+        }
+        
+        return shortestPath.getVertexList();
     }
 }
