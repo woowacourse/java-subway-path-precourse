76503bb68c8988c94873f5ae36281f5247fccc83
Junroot <junroot0909@gmail.com>
2020-12-19 15:36:13 +0900

    feat(subway): 구간 모델 구현
    Line에 stations, distances, times 정보 추가

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,7 +1,13 @@
 package subway.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Line {
     private String name;
+    private List<Station> stations = new ArrayList<Station>();
+    private List<Integer> distances = new ArrayList<Integer>(); // 0번째와 1번째 station들의 관계는 0번째 인덱스에 존재한다.
+    private List<Integer> times = new ArrayList<Integer>();
 
     public Line(String name) {
         this.name = name;
@@ -11,5 +17,9 @@ public class Line {
         return name;
     }
 
-    // 추가 기능 구현
+    public void pushSections(Station station, int distance, int time) {
+        stations.add(station);
+        distances.add(distance);
+        times.add(time);
+    }
 }
