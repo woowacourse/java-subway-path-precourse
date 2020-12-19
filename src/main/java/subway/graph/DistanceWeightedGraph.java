222a747281060b1004a580a7d153abed9f908ffd
JinseongHwang <eddy5360@naver.com>
2020-12-19 17:19:50 +0900

    feat: 거리를 가중치로 하는 그래프를 일급 컬렉션 형태로 저장

---
+++ src/main/java/subway/graph/DistanceWeightedGraph.java
@@ -0,0 +1,22 @@
+package subway.graph;
+
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+
+public class DistanceWeightedGraph {
+
+    private static final WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+
+    public static void addVertex(String vertex) {
+        graph.addVertex(vertex);
+    }
+
+    public static DefaultWeightedEdge addEdge(String source, String destination) {
+        return graph.addEdge(source, destination);
+    }
+
+    public static void setEdgeWeight(DefaultWeightedEdge e, double weight) {
+        graph.setEdgeWeight(e, weight);
+    }
+
+}
