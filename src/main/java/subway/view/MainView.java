03b27da994686df14fd6ceac5ba05f8b282c30a1
PaperCrafter <leejonghee342@gmail.com>
2020-12-19 14:16:11 +0900

    feat: Q를 누르면 종료되는 로직 추가

--- src/main/java/subway/view/MainView.java
+++ src/main/java/subway/view/MainView.java
@@ -1,5 +1,6 @@
 package subway.view;
 
+import subway.util.MainViewUtil;
 import subway.view.input.MainInputView;
 import subway.view.output.CommonOutputView;
 import subway.view.output.MainOutputView;
@@ -19,13 +20,28 @@ public class MainView {
 
     public void render() {
         boolean isContinuing = true;
-        while(isContinuing) {
+        while (isContinuing) {
             try {
                 mainOutputView.printMenu();
                 String menuFeature = mainInputView.getMenuTargetFeature();
+                checkAndRunSwitchViewToRouteLookupView(menuFeature);
+                isContinuing = checkAndQuitApplication(menuFeature);
             } catch (IllegalArgumentException illegalArgumentException) {
                 commonOutputView.printExceptionMessage(illegalArgumentException);
             }
         }
+    }
+
+    public void checkAndRunSwitchViewToRouteLookupView(String menuFeature) {
+        if (!menuFeature.equals(MainViewUtil.getBtnSelectPath())) {
+            return;
+        }
+    }
+
+    public boolean checkAndQuitApplication(String menuFeature) {
+        if (!menuFeature.equals(MainViewUtil.getBtnExit())) {
+            return true;
+        }
+        return false;
     }
 }
