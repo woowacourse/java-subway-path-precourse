b126e16e82996d1c864b5048c500552d1fee3eac
(학생) 이서윤 (전기전자컴퓨터공학부) <lynn312@unist.ac.kr>
2020-12-19 16:12:19 +0900

    [수정] 오류 메세지 수정

--- src/main/java/subway/domain/Dijkstra/IntervalEdges.java
+++ src/main/java/subway/domain/Dijkstra/IntervalEdges.java
@@ -1,17 +1,17 @@
 package subway.domain.Dijkstra;
 
 import exception.SubwayMapException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class IntervalEdges {
-    private List<IntervalEdge> intervalEdges;
-    private static final String NOT_EXIST_ROUTE = "두 역 사이에 경로가 존재하지 않습니다.";
-    public IntervalEdges(List<IntervalEdge> intervalEdges) {
-
+    private List<IntervalEdge> intervalEdges = new ArrayList<>();
+    private static final String NOT_EXIST_ROUTE = "두 역 사이에 구간이 존재하지 않습니다.";
+    public IntervalEdges() {
+        this.intervalEdges = intervalEdges;
     }
 
-    public IntervalEdge findByStations(Station source, Station Destination)
-        throws SubwayMapException {
+    public IntervalEdge findByStations(Station source, Station Destination) throws SubwayMapException {
         return intervalEdges.stream()
                             .filter(intervalEdge -> intervalEdge.getStart() == source && intervalEdge.getEnd() == Destination)
                             .findFirst()
