e1faef7626acb7a2fdd6722bdfe9be8dabd7700c
bbakjae <qkrwogus0724@naver.com>
2020-12-19 14:25:07 +0900

    feat(Line.java): 간선의 거리 및 시간을 포함한 객체 생성 메서스 작성

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class Line {
     private String name;
     private List<Station> stations = new ArrayList<Station>();
+    private List<Integer> distances = new ArrayList<>();
+    private List<Integer> times = new ArrayList<>();
 
     public Line(String name) {
         this.name = name;
@@ -17,6 +19,18 @@ public class Line {
 
     // 추가 기능 구현
     
+    public Line(String name, List<String> station_name, int[] distances, int[] times) {
+    	this.name = name;
+    	int i=0;
+    	for (String station : station_name) {    		
+    		stations.add(get_station_by_name(station));
+    		this.distances.add(distances[i]);
+    		this.times.add(times[i]);
+    		i++;
+    	}
+    	
+    }
+    
     public static Station get_station_by_name(String name) {
 		for (Station station : StationRepository.stations()) {
 			if (name.equals(station.getName())) {
@@ -25,11 +39,4 @@ public class Line {
 		}
 		return null;
 	}
-    
-    public Line(String name, List<String> station_name) {
-    	this.name = name;
-    	for (String station : station_name) {    		
-    		stations.add(get_station_by_name(station));
-    	}
-    }
 }
