f7fcf5a8b47d65e2ff982c76094cde80e03e3c9c
eoc940 <hgk0000@naver.com>
2020-12-19 15:16:31 +0900

    feat: 출발역과 도착역의 경로를 리스트로 저장하는 기능 구현

---
+++ src/main/java/subway/domain/PathController.java
@@ -0,0 +1,73 @@
+package subway.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PathController {
+
+    public Station findStation(String stationName) {
+        Station searchedStation = StationRepository.stations()
+                .stream().parallel()
+                .filter(station -> station.getName().equals(stationName))
+                .findAny().orElseThrow(IllegalArgumentException::new);
+        return searchedStation;
+    }
+
+    public List<Line> findLine(Station station) {
+        List<Line> lineList = new ArrayList<>();
+        for (Line line : LineRepository.lines()) {
+            if (line.getStation().contains(station)) {
+                lineList.add(line);
+            }
+        }
+        return lineList;
+    }
+
+    public boolean isExistCrossLine(Station startStation, Station endStation) {
+        List<Line> startLine = findLine(startStation);
+        List<Line> endLine = findLine(endStation);
+        for (Line line : startLine) {
+            if (endLine.contains(line)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Line findCrossLine(Station startStation, Station endStation) {
+        List<Line> startLine = findLine(startStation);
+        List<Line> endLine = findLine(endStation);
+        for (Line line : startLine) {
+            if (endLine.contains(line)) {
+                return line;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+
+
+    public void findPath(Station startStation, Station endStation) {
+        List<Station> path = new ArrayList<>();
+        if (isExistCrossLine(startStation, endStation)) {
+            Line crossLine = findCrossLine(startStation, endStation);
+            int startIndex = crossLine.getStation().indexOf(startStation);
+            int endIndex = crossLine.getStation().indexOf(endStation);
+            if (startIndex >= endIndex) {
+                path.add(crossLine.getStation().get(startIndex));
+                startIndex--;
+            }
+            if (endIndex >= startIndex) {
+                path.add(crossLine.getStation().get(endIndex));
+                endIndex--;
+            }
+        }
+    }
+
+
+
+
+
+
+
+}
