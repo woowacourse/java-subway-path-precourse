5eaf89de70b1ead805d778e79ae420cc2d3d6c1c
JinseongHwang <eddy5360@naver.com>
2020-12-19 14:06:42 +0900

    feat: 초기화할 노선들의 이름을 enum에서 정의

---
+++ src/main/java/subway/Lines.java
@@ -0,0 +1,17 @@
+package subway;
+
+public enum Lines {
+
+    LINE_2("2호선"),
+    LINE_3("3호선"),
+    LINE_SHINBUNDANG("신분당선");
+
+    private String name;
+    Lines(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
