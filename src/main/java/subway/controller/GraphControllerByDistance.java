1c84cd18ff9ba2184a6fbafecbb358464def46b7
thisisyoungbin <thisisyoungbin@gmail.com>
2020-12-19 16:15:30 +0900

    feat(GraphControllers): 거리와 시간 구간 정보를 관리하는 각 클래스 구현 및 시간과 거리 별로 초기 구간 정보 설정

--- src/main/java/subway/controller/GraphControllerByDistance.java
+++ src/main/java/subway/controller/GraphControllerByDistance.java
@@ -6,7 +6,7 @@ import org.jgrapht.graph.WeightedMultigraph;
 import java.util.Arrays;
 import java.util.List;
 
-public class GraphController {
+public class GraphControllerByDistance {
     private static final String KYODAE = "교대역";
     private static final String GANGNAM = "강남역";
     private static final String YUKSAM = "역삼역";
@@ -15,30 +15,38 @@ public class GraphController {
     private static final String YANGJAEFOREST = "양재시민의숲역";
     private static final String MAEBONG = "매봉역";
 
-    private List<String> stations = Arrays.asList(KYODAE, GANGNAM, YUKSAM, NAMBU,
-            YANGJAE, YANGJAEFOREST, MAEBONG);
-    private List<String> line2 = Arrays.asList(KYODAE, GANGNAM, YUKSAM);
-    private List<String> line3 = Arrays.asList(KYODAE, NAMBU, YANGJAE, MAEBONG);
-    private List<String> lineShin = Arrays.asList(GANGNAM, YANGJAE, YANGJAEFOREST);
+    private List<String> stations = Arrays.asList(KYODAE, GANGNAM, YUKSAM, NAMBU, YANGJAE, YANGJAEFOREST, MAEBONG);
+
+    public GraphControllerByDistance() {
+        init();
+    }
 
     public void init() {
         initByDistance();
-        initByTime();
     }
 
     private void initByDistance() {
         WeightedMultigraph<String, DefaultWeightedEdge> graph
                 = new WeightedMultigraph(DefaultWeightedEdge.class);
         stations.forEach(station -> graph.addVertex(station));
-        for (int i = 0; i < line2.size()-1; i++) {
-            line2[i]
-        }
-
+        initLineTwoByDistance(graph);
+        initLineThreeByDistance(graph);
+        initLineShinByDistance(graph);
     }
 
-    private void initByTime() {
-        WeightedMultigraph<String, DefaultWeightedEdge> graph
-                = new WeightedMultigraph(DefaultWeightedEdge.class);
-        stations.forEach(station -> graph.addVertex(station));
+    private void initLineTwoByDistance(WeightedMultigraph<String, DefaultWeightedEdge> graph) {
+        graph.setEdgeWeight(graph.addEdge(KYODAE, GANGNAM),2);
+        graph.setEdgeWeight(graph.addEdge(GANGNAM, YUKSAM),2);
+    }
+
+    private void initLineThreeByDistance(WeightedMultigraph<String, DefaultWeightedEdge> graph) {
+        graph.setEdgeWeight(graph.addEdge(KYODAE, NAMBU), 3);
+        graph.setEdgeWeight(graph.addEdge(NAMBU, YANGJAE),6);
+        graph.setEdgeWeight(graph.addEdge(YANGJAE, MAEBONG),1);
+    }
+
+    private void initLineShinByDistance(WeightedMultigraph<String, DefaultWeightedEdge> graph) {
+        graph.setEdgeWeight(graph.addEdge(GANGNAM, YANGJAE),2);
+        graph.setEdgeWeight(graph.addEdge(YANGJAE, YANGJAEFOREST),10);
     }
 }
