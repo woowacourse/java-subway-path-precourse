eb6057cbbb7061804ebd833a42991c9737b5ae78
Injun Son <ison@sfu.ca>
2020-12-19 15:34:55 +0900

    feat: 출발역과 도착역이 다른지 검사

--- src/main/java/subway/controller/MinCostCalculator.java
+++ src/main/java/subway/controller/MinCostCalculator.java
@@ -10,8 +10,12 @@ import org.jgrapht.traverse.
 
 public class MinCostCalculator {
 
+    private static final String SAME_STATION_ERROR = "출발역과 도착역은 달라야합니다";
+    private static final String ERROR_PREFIX = "[ERROR]: ";
     private final List<Line> lines;
     private final List<Station> stations;
+    private String departureStation;
+    private String arrivalStation;
 
     public MinCostCalculator() {
         lines = DefaultLines.getDefaultLines();
@@ -19,11 +23,25 @@ public class MinCostCalculator {
     }
 
     public void runToGetMinDistance() {
-        String departureStation = InputView.inputStation();
-        String arrivalStation = InputView.inputStation();
+        getStationNames();
+
+
     }
 
     public void runToGetMinTime() {
+    }
+
+    private void getStationNames() {
+        try {
+            String departureStation = InputView.inputStation();
+            String arrivalStation = InputView.inputStation();
+            if (departureStation.equals(arrivalStation)) {
+                throw new IllegalArgumentException(SAME_STATION_ERROR);
+            }
+        } catch (Exception e) {
+            System.out.println(ERROR_PREFIX + e.getMessage());
+            getStationNames();
+        }
     }
 
     public void calculateMinDistance() {
