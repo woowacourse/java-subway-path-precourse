2a230678d2625c008f2f8aa0aa4a3c6e5ab8da52
Hansaem Woo <feliscatus@hanyang.ac.kr>
2020-12-19 16:26:10 +0900

    feat(Initializer): add station repository initializer method
    역 저장소를 초기화하는 메소드 추가

--- src/main/java/subway/util/Initializer.java
+++ src/main/java/subway/util/Initializer.java
@@ -1,8 +1,22 @@
 package subway.util;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import subway.domain.Station;
+import subway.domain.StationRepository;
+
 public class Initializer {
 
     private Initializer() {
     }
 
+    private static void InitializeStationRepository() {
+        final Set<String> stationNameSet = new HashSet<>();
+        for (String[] stationNames : Constants.INITIAL_STATION_NAMES_IN_LINES) {
+            stationNameSet.addAll(Arrays.asList(stationNames));
+        }
+        stationNameSet
+            .forEach(stationName -> StationRepository.addStation(new Station(stationName)));
+    }
 }
