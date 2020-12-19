2adcb4ad6118bc47bf4dbeda715479491fbe6f2c
BaekSeungChan <baekchan1024@DESKTOP-THJVK48.localdomain>
2020-12-19 14:54:10 +0900

    feat (RouteManagement) : 메인화면과 구간 기준을 연결

---
+++ src/main/java/controller/RouteManagement.java
@@ -0,0 +1,43 @@
+package controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import view.InputView;
+import view.OutputView;
+
+enum RouteButton {
+	SHORTEST_DISTANCE("1"), SHORTEST_TIME("2"), BACK_TO_MAIN("B");
+
+	private final String button;
+
+	RouteButton(String button) {
+		this.button = button;
+	}
+
+	public String getButton() {
+		return button;
+	}
+}
+
+public class RouteManagement {
+	private static final List<String> menu = Arrays.asList(RouteButton.SHORTEST_DISTANCE.getButton(),
+			RouteButton.SHORTEST_TIME.getButton(), RouteButton.BACK_TO_MAIN.getButton());
+
+	public static void execute() {
+		OutputView.printRoutestandard();
+		String fuction = InputView.selectFunction();
+		proceduresExecute(fuction);
+	}
+
+	public static void proceduresExecute(String input) {
+		if (input.equals(RouteButton.BACK_TO_MAIN.getButton())) {
+			MainManagement.execute();
+			return;
+		} else if (input.equals(RouteButton.SHORTEST_DISTANCE.getButton())) {
+
+		} else if (input.equals(RouteButton.SHORTEST_TIME.getButton())) {
+
+		}
+	}
+}
