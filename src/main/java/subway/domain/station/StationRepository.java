4c55c7581c9807dd36c377a5ce4700b38c76f119
Gayoung <gyyoon4u@naver.com>
2020-12-19 16:33:12 +0900

    feat: staion두개 입력 시 각각의 index 뽑아내기

--- src/main/java/subway/domain/station/StationRepository.java
+++ src/main/java/subway/domain/station/StationRepository.java
@@ -1,5 +1,6 @@
 package subway.domain.station;
 
+import subway.domain.line.Line;
 import subway.exception.station.DuplicateStationNameException;
 
 import java.util.ArrayList;
@@ -9,6 +10,7 @@ import java.util.Objects;
 
 public class StationRepository {
     private static final List<Station> stations = new ArrayList<>();
+    private static final List<Line> lines = new ArrayList<>();
 
     public static List<Station> stations() {
         return Collections.unmodifiableList(stations);
@@ -25,8 +27,21 @@ public class StationRepository {
         stations.forEach(StationRepository::save);
     }
 
+
     public static void addStation(Station station) {
         stations.add(station);
+    }
+
+    public static List<Integer> findIndexByStationName(Station firstStation, Station secondStation) {
+        int firstStationIndex = lines.indexOf(firstStation);
+        int secondStationIndex = lines.indexOf(secondStation);
+
+        List<Integer> result = new ArrayList<>();
+        if (firstStationIndex != -1 && secondStationIndex != -1) {
+            result.add(firstStationIndex);
+            result.add(secondStationIndex);
+        }
+        return result;
     }
 
     public static boolean deleteStation(String name) {
