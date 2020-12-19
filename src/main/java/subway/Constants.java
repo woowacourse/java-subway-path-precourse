c3d9d0dbd8a974bc01a1450a79d232d0ed699a5c
pika96 <52442949+pika96@users.noreply.github.com>
2020-12-19 13:42:12 +0900

    feat: Constants 클래스 추가

---
+++ src/main/java/subway/Constants.java
@@ -0,0 +1,8 @@
+package subway;
+
+public class Constants {
+    public static final String FUNCTION_ONE = "1";
+    public static final String FUNCTION_TWO = "2";
+    public static final String FUNCTION_Q = "Q";
+    public static final String FUNCTION_B = "B";
+}
