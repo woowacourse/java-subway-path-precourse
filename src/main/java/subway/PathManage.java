f147b79663068282451a3a8bd73f0dfdb2a9c6ee
xrabcde <joara124@ewhain.net>
2020-12-19 17:32:42 +0900

    feat(PathManage): 최단 경로 출력 메소드 구현

--- src/main/java/subway/PathManage.java
+++ src/main/java/subway/PathManage.java
@@ -1,12 +1,13 @@
 package subway;
 
 import subway.domain.Constants;
-import subway.domain.Errors;
-import subway.domain.Station;
-import subway.domain.StationRepository;
+import subway.domain.Init;
+import subway.domain.Line;
+import subway.domain.LineRepository;
 import subway.view.InputView;
 import subway.view.View;
 
+import java.util.List;
 import java.util.Scanner;
 
 import static subway.Application.startProgram;
@@ -25,7 +26,9 @@ public class PathManage {
 
     public static void findMinDistance(Scanner kbd) {
         try {
-            InputView.inputSrcDest(kbd);
+            String[] stations = InputView.inputSrcDest(kbd);
+            List<String> shortestPath = Init.dijkstraDistance.getPath(stations[0], stations[1]).getVertexList();
+            getMinPath(Constants.PATH_DISTANCE, shortestPath, getMinValue(stations));
             startProgram(kbd);
         } catch (Exception e) {
             startProgram(kbd);
@@ -34,10 +37,28 @@ public class PathManage {
 
     public static void findMinTime(Scanner kbd) {
         try {
-            InputView.inputSrcDest(kbd);
+            String[] stations = InputView.inputSrcDest(kbd);
+            List<String> shortestPath = Init.dijkstraTime.getPath(stations[0], stations[1]).getVertexList();
+            getMinPath(Constants.PATH_TIME, shortestPath, getMinValue(stations));
             startProgram(kbd);
         } catch (Exception e) {
             startProgram(kbd);
         }
+    }
+
+    public static void getMinPath(String name, List<String> path, int[] values) {
+        Line minPath = new Line(name);
+        LineRepository.addLine(minPath, path);
+        View.displayPath(minPath, values[0], values[1]);
+        LineRepository.deleteLineByName(name);
+    }
+
+    public static int[] getMinValue(String[] stations) {
+        double time = Init.dijkstraTime.getPath(stations[0], stations[1]).getWeight();
+        double distance = Init.dijkstraDistance.getPath(stations[0], stations[1]).getWeight();
+        int[] values = new int[2];
+        values[0] = (int) Math.round(time);
+        values[1] = (int) Math.round(distance);
+        return values;
     }
 }
