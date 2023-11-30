cbc0fbab1611ccf8629e8c6dbff67e3f3535a279
zangsu <zangsu_@naver.com>
2023-11-30 17:59:24 +0900

    fix : 총 소요 시간, 총 거리를 구하는 로직 수정

--- src/main/java/subway/domain/repository/PathRepository.java
+++ src/main/java/subway/domain/repository/PathRepository.java
@@ -6,7 +6,7 @@ import subway.domain.Station;
 import subway.domain.UnitPath;
 
 public class PathRepository {
-    private static HashSet<UnitPath> paths;
+    private static HashSet<UnitPath> paths = new HashSet<>();
 
     public static void addPath(UnitPath path){
         paths.add(path);
@@ -16,11 +16,12 @@ public class PathRepository {
         int time = 0;
         for (int i = 0; i < totalPath.size() - 2; i++) {
             int finalI = i;
-            UnitPath unitPath = paths.stream()
+            UnitPath unitPath =
+                    paths.stream()
                     .filter(path -> path.isPathOf(totalPath.get(finalI), totalPath.get(finalI + 1)))
                     .findFirst()
                     .orElseThrow(IllegalArgumentException::new);
-            //time += unitPath.getTime();
+            time += unitPath.getTime();
         }
         return time;
     }
@@ -29,11 +30,11 @@ public class PathRepository {
         int distance = 0;
         for (int i = 0; i < totalPath.size() - 2; i++) {
             int finalI = i;
-            /*UnitPath unitPath = paths.stream()
+            UnitPath unitPath = paths.stream()
                     .filter(path -> path.isPathOf(totalPath.get(finalI), totalPath.get(finalI + 1)))
                     .findFirst()
                     .orElseThrow(IllegalArgumentException::new);
-            distance += unitPath.getDistance();*/
+            distance += unitPath.getDistance();
         }
         return distance;
     }
